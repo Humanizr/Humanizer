65972edb0085a2fa24bea6a40b7a1c8b74537052
Mehdi Khalili <me@mehdi-khalili.com>
2012-05-03 23:00:50 +1000

    File -> New - Created Humanizer, Humanizer.Mvc and Humanizer.Tests and implemented string extensions

---
+++ src/Humanizer.Tests/StringExtensionTests.cs
@@ -0,0 +1,60 @@
+﻿using Humanize.Extensions;
+using Xunit;
+
+namespace Humanizer.Tests
+{
+    public class StringExtensionTests
+    {
+        [Fact]
+        public void PascalCaseInputStringIsTurnedIntoSentence()
+        {
+            Assert.Equal(
+                "Pascal case input string is turned into sentence",
+                "PascalCaseInputStringIsTurnedIntoSentence".Humanize());
+        }
+
+        [Fact]
+        public void WhenInputStringContainsConsequtiveCaptialLetters_ThenTheyAreTurnedIntoOneLetterWords()
+        {
+            Assert.Equal(
+                "When I use an input a here",
+                "WhenIUseAnInputAHere".Humanize());
+        }
+
+        [Fact]
+        public void WhenInputStringStartsWithANumber_ThenNumberIsDealtWithLikeAWord()
+        {
+            Assert.Equal("10 is in the begining", "10IsInTheBegining".Humanize());
+        }
+
+        [Fact]
+        public void WhenInputStringEndWithANumber_ThenNumberIsDealtWithLikeAWord()
+        {
+            Assert.Equal("Number is at the end 100", "NumberIsAtTheEnd100".Humanize());
+        }
+
+        [Fact]
+        public void UnderscoredInputStringIsTurnedIntoSentence()
+        {
+            Assert.Equal(
+                "Underscored input string is turned into sentence",
+                "Underscored_input_string_is_turned_into_sentence".Humanize());
+        }
+
+        [Fact]
+        public void UnderscoredInputStringPreservesCasing()
+        {
+            Assert.Equal(
+                "Underscored input String is turned INTO sentence",
+                "Underscored_input_String_is_turned_INTO_sentence".Humanize());
+        }
+
+        [Fact]
+        public void OneLetterWordInTheBeginningOfStringIsTurnedIntoAWord()
+        {
+            Assert.Equal(
+                "X is first word in the sentence",
+                "XIsFirstWordInTheSentence".Humanize());
+        }
+    }
+}
---
+++ src/Humanizer/Extensions/StringExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Humanize.Extensions
+{
+    public static class StringExtensions
+    {
+        static readonly Func<string, string> FromUnderscoreSeparatedWords = methodName => string.Join(" ", methodName.Split(new[] { '_' }));
+        static string FromPascalCase(string name)
+        {
+            var chars = name.Aggregate(
+                new List<char>(),
+                (list, currentChar) =>
+                {
+                    if (currentChar == ' ')
+                    {
+                        list.Add(currentChar);
+                        return list;
+                    }
+
+                    if (list.Count == 0)
+                    {
+                        list.Add(currentChar);
+                        return list;
+                    }
+
+                    var lastCharacterInTheList = list[list.Count - 1];
+                    if (lastCharacterInTheList != ' ')
+                    {
+                        if (char.IsDigit(lastCharacterInTheList))
+                        {
+                            if (char.IsLetter(currentChar))
+                                list.Add(' ');
+                        }
+                        else if (!char.IsLower(currentChar))
+                            list.Add(' ');
+                    }
+
+                    list.Add(char.ToLower(currentChar));
+
+                    return list;
+                });
+
+            var result = new string(chars.ToArray());
+            return result.Replace(" i ", " I "); // I is an exception
+        }
+
+        public static string Humanize(this string input) 
+        {
+            if (input.Contains("_"))
+                return FromUnderscoreSeparatedWords(input);
+
+            return FromPascalCase(input);
+        }
+    }
+}
