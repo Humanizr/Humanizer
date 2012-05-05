db66957f2dab6673ff9cb6d2f2516c58b7ac7790
Mehdi Khalili <me@mehdi-khalili.com>
2012-05-05 12:57:35 +1000

    Added nuget spec

--- src/Humanizer/Extensions/StringExtensions.cs
+++ src/Humanizer/Extensions/StringExtensions.cs
@@ -23,6 +23,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.using System;
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
--- src/Humanizer/Properties/AssemblyInfo.cs
+++ src/Humanizer/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Humanizer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A small framework that turns your normal strings, type names, enum fields, date fields ETC into a human friendly format")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mehdi Khalili")]
 [assembly: AssemblyProduct("Humanizer")]
 [assembly: AssemblyCopyright("Copyright ©  2012  Mehdi Khalili")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1")]
-[assembly: AssemblyFileVersion("0.1")]
+[assembly: AssemblyVersion("0.1.1")]
+[assembly: AssemblyFileVersion("0.1.beta")]
