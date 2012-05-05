26d0783bb93b852059a30f9f4de8c01bd98fb98f
Mehdi Khalili <me@mehdi-khalili.com>
2012-05-05 12:22:13 +1000

    Added a baseline for Enum humanizer

---
+++ src/Humanizer.Tests/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using Xunit;
+using Humanize.Extensions;
+
+namespace Humanizer.Tests.Extensions
+{
+    public enum EnumUnderTest
+    {
+        [Description(EnumExtensionsTests.CustomDescription)]
+        MemberWithDescriptionAttribute,
+        MemberWithoutDescriptionAttribute
+    }
+
+    public class EnumExtensionsTests
+    {
+        public const string CustomDescription = "Some Description";
+
+        [Fact] 
+        public void HonorsDescriptionAttribute()
+        {
+            Assert.Equal(CustomDescription, EnumUnderTest.MemberWithDescriptionAttribute.Humanize());
+        }
+
+        [Fact] 
+        public void CanHumanizeMembersWithoutDescriptionAttribute()
+        {
+            Assert.Equal("Member without description attribute", EnumUnderTest.MemberWithoutDescriptionAttribute.Humanize());
+        }
+    }
+}
---
+++ src/Humanizer/Extensions/EnumExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Humanize.Extensions
+{
+    public static class EnumExtensions
+    {
+         public static string Humanize(this Enum input)
+         {
+             Type type = input.GetType();
+             MemberInfo[] memInfo = type.GetMember(input.ToString());
+
+             if (memInfo.Length > 0)
+             {
+                 object[] attrs = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                 if (attrs.Length > 0)
+                 {
+                     return ((DescriptionAttribute)attrs[0]).Description;
+                 }
+             }
+
+             return input.ToString().Humanize();
+         }
+    }
+}
