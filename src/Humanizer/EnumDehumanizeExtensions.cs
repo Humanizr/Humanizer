b521cd64502c31491cbbd9c4149c985634bcd729
MehdiK <me@mehdi-khalili.com>
2013-08-06 18:45:09 +0430

    API documentation for Dehumanization

--- src/Humanizer/EnumDehumanizeExtensions.cs
+++ src/Humanizer/EnumDehumanizeExtensions.cs
@@ -4,6 +4,13 @@ namespace Humanizer
 {
     public static class EnumDehumanizeExtensions
     {
+        /// <summary>
+        /// Dehumanizes a string into the Enum it was originally Humanized from!
+        /// </summary>
+        /// <typeparam name="TTargetEnum">The target enum</typeparam>
+        /// <param name="input">The string to be converted</param>
+        /// <param name="casing">A hint to Dehumanizer around what casing was originally used for humanization</param>
+        /// <returns></returns>
         public static Enum DehumanizeTo<TTargetEnum>(this string input, LetterCasing? casing = null) 
         {
             var values = (TTargetEnum[]) Enum.GetValues(typeof (TTargetEnum));
