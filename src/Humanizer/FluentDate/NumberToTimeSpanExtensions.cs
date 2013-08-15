838e92cfea64033d69a80ed11520c51a08f2622a
MehdiK <me@mehdi-khalili.com>
2013-08-15 14:29:48 +0430

    Implemented NumberToTimeSpan extensions

---
+++ src/Humanizer/FluentDate/NumberToTimeSpanExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Humanizer
+{
+    public static class NumberToTimeSpanExtensions
+    {
+        public static TimeSpan Days(this int input)
+        {
+            return new TimeSpan(input, 0, 0, 0);
+        }
+
+        public static TimeSpan Hours(this int input)
+        {
+            return new TimeSpan(0, input, 0, 0);
+        }
+
+        public static TimeSpan Minutes(this int input)
+        {
+            return new TimeSpan(0, 0, input, 0);
+        }
+
+        public static TimeSpan Seconds(this int input)
+        {
+            return new TimeSpan(0, 0, 0, input);
+        }
+    }
+}
