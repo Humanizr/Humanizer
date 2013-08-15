a622d92352b0b36d54d418c7e9b3b6699db1d24b
MehdiK <me@mehdi-khalili.com>
2013-08-15 14:32:48 +0430

    documenting NumberToTimeSpan extensions

--- src/Humanizer/FluentDate/NumberToTimeSpanExtensions.cs
+++ src/Humanizer/FluentDate/NumberToTimeSpanExtensions.cs
@@ -3,21 +3,41 @@ namespace Humanizer
 {
     public static class NumberToTimeSpanExtensions
     {
+        /// <summary>
+        /// 2.Days() == new TimeSpan(2, 0, 0, 0)
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
         public static TimeSpan Days(this int input)
         {
             return new TimeSpan(input, 0, 0, 0);
         }
 
+        /// <summary>
+        /// 3.Hours() == new TimeSpan(0, 3, 0, 0)
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
         public static TimeSpan Hours(this int input)
         {
             return new TimeSpan(0, input, 0, 0);
         }
 
+        /// <summary>
+        /// 4.Minutes() == new TimeSpan(0, 0, 4, 0) 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
         public static TimeSpan Minutes(this int input)
         {
             return new TimeSpan(0, 0, input, 0);
         }
 
+        /// <summary>
+        /// 5.Seconds() == new TimeSpan(0, 0, 0, 5) 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
         public static TimeSpan Seconds(this int input)
         {
             return new TimeSpan(0, 0, 0, input);
