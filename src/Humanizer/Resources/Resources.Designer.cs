82f45c1851971298f345500ea6ee21980c601387
MehdiK <me@mehdi-khalili.com>
2013-08-21 16:54:02 +0430

    moved resources to the new Resources folder

--- src/Humanizer/Resources/Resources.Designer.cs
+++ src/Humanizer/Resources/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Humanizer {
+namespace Humanizer.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Humanizer {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Humanizer.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Humanizer.Resources.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
