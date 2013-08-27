c1545510d9e303c88827acc9294dc9570e84bf8b
Victor Grigoriu <vgrigoriu@gmail.com>
2013-08-27 13:46:34 +0300

    Replace string with StringBuilder
    to avoid successive string concatenations

--- src/Humanizer/NumberToWordsExtension.cs
+++ src/Humanizer/NumberToWordsExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Humanizer
 {
@@ -18,45 +19,45 @@ namespace Humanizer
             if (number < 0)
                 return "minus " + ToWords(Math.Abs(number));
 
-            string words = "";
+            var words = new StringBuilder();
 
             if ((number / 1000000) > 0)
             {
-                words += ToWords(number / 1000000) + " million ";
+                words.Append(ToWords(number / 1000000) + " million ");
                 number %= 1000000;
             }
 
             if ((number / 1000) > 0)
             {
-                words += ToWords(number / 1000) + " thousand ";
+                words.Append(ToWords(number / 1000) + " thousand ");
                 number %= 1000;
             }
 
             if ((number / 100) > 0)
             {
-                words += ToWords(number / 100) + " hundred ";
+                words.Append(ToWords(number / 100) + " hundred ");
                 number %= 100;
             }
 
             if (number > 0)
             {
-                if (words != "")
-                    words += "and ";
+                if (words.Length != 0)
+                    words.Append("and ");
 
                 var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
                 var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
 
                 if (number < 20)
-                    words += unitsMap[number];
+                    words.Append(unitsMap[number]);
                 else
                 {
-                    words += tensMap[number / 10];
+                    words.Append(tensMap[number / 10]);
                     if ((number % 10) > 0)
-                        words += "-" + unitsMap[number % 10];
+                        words.Append("-" + unitsMap[number % 10]);
                 }
             }
 
-            return words;
+            return words.ToString();
         }
     }
 }
