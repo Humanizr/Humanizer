3f58e037f651791f3878e511e8a62cd7d56242d2
Victor Grigoriu <vgrigoriu@gmail.com>
2013-08-30 08:31:41 +0300

    Move culture helper to its own class

--- src/Humanizer.Tests/Extensions/DateHumanizeTests.ro-RO.cs
+++ src/Humanizer.Tests/Extensions/DateHumanizeTests.ro-RO.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Globalization;
-using System.Threading;
 
 using Xunit;
 
@@ -33,31 +32,6 @@ namespace Humanizer.Tests.Extensions
         private static CurrentCultureChanger RomanianCulture()
         {
             return new CurrentCultureChanger(new CultureInfo("ro-RO"));
-        }
-
-        /// <summary>
-        /// Temporarily change the CurrentUICulture and restore it back
-        /// </summary>
-        private class CurrentCultureChanger: IDisposable
-        {
-            private readonly CultureInfo initialCulture;
-            private readonly Thread initialThread;
-
-            public CurrentCultureChanger(CultureInfo cultureInfo)
-            {
-                if (cultureInfo == null)
-                    throw new ArgumentNullException("cultureInfo");
-
-                initialThread = Thread.CurrentThread;
-                initialCulture = initialThread.CurrentUICulture;
-
-                initialThread.CurrentUICulture = cultureInfo;
-            }
-
-            public void Dispose()
-            {
-                initialThread.CurrentUICulture = initialCulture;
-            }
         }
     }
 }
