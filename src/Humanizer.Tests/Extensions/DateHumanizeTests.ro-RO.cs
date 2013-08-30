721ed8d6473b3e2656f07356e406a255cf7d0616
Victor Grigoriu <vgrigoriu@gmail.com>
2013-08-30 08:30:38 +0300

    Add culture changer helper

--- src/Humanizer.Tests/Extensions/DateHumanizeTests.ro-RO.cs
+++ src/Humanizer.Tests/Extensions/DateHumanizeTests.ro-RO.cs
@@ -11,27 +11,53 @@ namespace Humanizer.Tests.Extensions
         [Fact]
         public void RomanianTranslationIsCorrectForThreeHoursAgo()
         {
-            var currentCulture = Thread.CurrentThread.CurrentUICulture;
-            Thread.CurrentThread.CurrentUICulture = new CultureInfo("ro-RO");
+            using (RomanianCulture())
+            {
+                var threeHoursAgo = DateTime.UtcNow.AddHours(-3).Humanize();
 
-            var threeHoursAgo = DateTime.UtcNow.AddHours(-3).Humanize();
-
-            Thread.CurrentThread.CurrentUICulture = currentCulture;
-
-            Assert.Equal("acum 3 ore", threeHoursAgo);
+                Assert.Equal("acum 3 ore", threeHoursAgo);
+            }
         }
 
         [Fact]
         public void RomanianTranslationIsCorrectFor20HoursAgo()
         {
-            var currentCulture = Thread.CurrentThread.CurrentUICulture;
-            Thread.CurrentThread.CurrentUICulture = new CultureInfo("ro-RO");
+            using (RomanianCulture())
+            {
+                var threeHoursAgo = DateTime.UtcNow.AddHours(-20).Humanize();
 
-            var threeHoursAgo = DateTime.UtcNow.AddHours(-20).Humanize();
+                Assert.Equal("acum 20 de ore", threeHoursAgo);
+            }
+        }
 
-            Thread.CurrentThread.CurrentUICulture = currentCulture;
+        private static CurrentCultureChanger RomanianCulture()
+        {
+            return new CurrentCultureChanger(new CultureInfo("ro-RO"));
+        }
 
-            Assert.Equal("acum 20 de ore", threeHoursAgo);
+        /// <summary>
+        /// Temporarily change the CurrentUICulture and restore it back
+        /// </summary>
+        private class CurrentCultureChanger: IDisposable
+        {
+            private readonly CultureInfo initialCulture;
+            private readonly Thread initialThread;
+
+            public CurrentCultureChanger(CultureInfo cultureInfo)
+            {
+                if (cultureInfo == null)
+                    throw new ArgumentNullException("cultureInfo");
+
+                initialThread = Thread.CurrentThread;
+                initialCulture = initialThread.CurrentUICulture;
+
+                initialThread.CurrentUICulture = cultureInfo;
+            }
+
+            public void Dispose()
+            {
+                initialThread.CurrentUICulture = initialCulture;
+            }
         }
     }
 }
