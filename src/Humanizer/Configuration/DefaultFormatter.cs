ff30f0338cf4c849790bff83c029ab82683f7fc6
Victor Grigoriu <vgrigoriu@gmail.com>
2013-08-30 08:38:10 +0300

    Move RomanianFormatter to its own class

--- src/Humanizer/Configuration/DefaultFormatter.cs
+++ src/Humanizer/Configuration/DefaultFormatter.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-
-using Humanizer.Properties;
+﻿using Humanizer.Properties;
 
 namespace Humanizer.Configuration
 {
@@ -69,34 +67,6 @@ namespace Humanizer.Configuration
         public virtual string DateHumanize_yesterday()
         {
             return Resources.DateHumanize_yesterday;
-        }
-    }
-
-    class RomanianFormatter : DefaultFormatter
-    {
-        public override string DateHumanize__hours_ago(int number)
-        {
-            var numberOfHours = number;
-            Debug.Assert(numberOfHours > 1);
-
-            if (0 < numberOfHours%100 && numberOfHours%100 < 20)
-            {
-                return base.DateHumanize__hours_ago(numberOfHours);
-            }
-
-            return string.Format("acum {0} de ore", numberOfHours);
-        }
-
-        public override string DateHumanize__minutes_ago(int number)
-        {
-            Debug.Assert(number > 1);
-
-            if (0 < number % 100 && number % 100 < 20)
-            {
-                return base.DateHumanize__minutes_ago(number);
-            }
-
-            return string.Format("acum {0} de minute", number);
         }
     }
 }
---
+++ src/Humanizer/Configuration/RomanianFormatter.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace Humanizer.Configuration
+{
+    class RomanianFormatter : DefaultFormatter
+    {
+        public override string DateHumanize__hours_ago(int number)
+        {
+            var numberOfHours = number;
+            Debug.Assert(numberOfHours > 1);
+
+            if (0 < numberOfHours%100 && numberOfHours%100 < 20)
+            {
+                return base.DateHumanize__hours_ago(numberOfHours);
+            }
+
+            return string.Format("acum {0} de ore", numberOfHours);
+        }
+
+        public override string DateHumanize__minutes_ago(int number)
+        {
+            Debug.Assert(number > 1);
+
+            if (0 < number % 100 && number % 100 < 20)
+            {
+                return base.DateHumanize__minutes_ago(number);
+            }
+
+            return string.Format("acum {0} de minute", number);
+        }
+    }
+}
