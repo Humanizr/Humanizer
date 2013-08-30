29a23ecb87bbb29e926dcc9303e2b0f0ac5e5dd7
Victor Grigoriu <vgrigoriu@gmail.com>
2013-08-30 08:37:26 +0300

    Return correct Romanian string for "minutes ago"

--- src/Humanizer.Tests/Extensions/DateHumanizeTests.ro-RO.cs
+++ src/Humanizer.Tests/Extensions/DateHumanizeTests.ro-RO.cs
@@ -29,6 +29,29 @@ namespace Humanizer.Tests.Extensions
             }
         }
 
+        [Fact]
+        public void RomanianTranslationIsCorrectFor19MinutesAgo()
+        {
+            using (RomanianCulture())
+            {
+                var nineteenMinutesAgo = DateTime.UtcNow.AddMinutes(-19).Humanize();
+
+                Assert.Equal("acum 19 minute", nineteenMinutesAgo);
+            }
+        }
+
+        [Fact]
+        public void RomanianTranslationIsCorrectFor44MinutesAgo()
+        {
+            using (RomanianCulture())
+            {
+                var fourtyFourMinutesAgo = DateTime.UtcNow.AddMinutes(-44).Humanize();
+
+                Assert.Equal("acum 44 de minute", fourtyFourMinutesAgo);
+            }
+        }
+
+
         private static CurrentCultureChanger RomanianCulture()
         {
             return new CurrentCultureChanger(new CultureInfo("ro-RO"));
--- src/Humanizer/Configuration/DefaultFormatter.cs
+++ src/Humanizer/Configuration/DefaultFormatter.cs
@@ -86,5 +86,17 @@ namespace Humanizer.Configuration
 
             return string.Format("acum {0} de ore", numberOfHours);
         }
+
+        public override string DateHumanize__minutes_ago(int number)
+        {
+            Debug.Assert(number > 1);
+
+            if (0 < number % 100 && number % 100 < 20)
+            {
+                return base.DateHumanize__minutes_ago(number);
+            }
+
+            return string.Format("acum {0} de minute", number);
+        }
     }
 }
