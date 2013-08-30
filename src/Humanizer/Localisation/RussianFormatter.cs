52a827687b8e0e41881491a5dfaeb16b40634734
Alexander I. Zaytsev <hazzik@gmail.com>
2013-08-30 19:33:24 +1200

    Add Resources.ru.resx

--- src/Humanizer/Localisation/RussianFormatter.cs
+++ src/Humanizer/Localisation/RussianFormatter.cs
@@ -2,92 +2,61 @@
 {
     internal class RussianFormatter : DefaultFormatter
     {
-        private const string Ago = "{0} {1} назад";
-
-        private static string Select(int number, string singular, string paucal, string plural)
+        private static string Format(string resourceKey, int number)
         {
-            var mod100 = number % 100;
-            if (mod100 / 10 != 1)
+            var format = Resources.GetResource(GetResourceKey(resourceKey, number));
+
+            return string.Format(format, number);
+        }
+
+        private static string GetResourceKey(string resourceKey, int number)
+        {
+            var mod100 = number%100;
+            if (mod100/10 != 1)
             {
-                var mod10 = number % 10;
+                var mod10 = number%10;
 
                 if (mod10 == 1) // 1, 21, 31, 41 ... 91, 101, 121 ..
                 {
-                    return singular;
+                    return resourceKey + "_singular";
                 }
 
                 if (mod10 > 1 && mod10 < 5) // 2, 3, 4, 22, 23, 24 ...
                 {
-                    return paucal;
+                    return resourceKey + "_paucal";
                 }
             }
-
-            return plural;
-        }
-
-        public override string DateHumanize_one_second_ago()
-        {
-            return "секунду назад";
+            return resourceKey;
         }
 
         public override string DateHumanize__seconds_ago(int number)
         {
-            return string.Format(Ago, number, Select(number, "секунду", "секунды", "секунд"));
-        }
-
-        public override string DateHumanize_a_minute_ago()
-        {
-            return "минуту назад";
+            return Format(ResourceKeys.DateHumanize__seconds_ago, number);
         }
 
         public override string DateHumanize__minutes_ago(int number)
         {
-            return string.Format(Ago, number, Select(number, "минуту", "минуты", "минут"));
-        }
-
-        public override string DateHumanize_an_hour_ago()
-        {
-            return "час назад";
+            return Format(ResourceKeys.DateHumanize__minutes_ago, number);
         }
 
         public override string DateHumanize__hours_ago(int number)
         {
-            return string.Format(Ago, number, Select(number, "час", "часа", "часов"));
-        }
-
-        public override string DateHumanize_yesterday()
-        {
-            return "вчера";
+            return Format(ResourceKeys.DateHumanize__hours_ago, number);
         }
 
         public override string DateHumanize__days_ago(int number)
         {
-            return string.Format(Ago, number, Select(number, "день", "дня", "дней"));
-        }
-
-        public override string DateHumanize_one_month_ago()
-        {
-            return "месяц назад";
+            return Format(ResourceKeys.DateHumanize__days_ago, number);
         }
 
         public override string DateHumanize__months_ago(int number)
         {
-            return string.Format(Ago, number, Select(number, "месяц", "месяца", "месяцев"));
-        }
-
-        public override string DateHumanize_one_year_ago()
-        {
-            return "год назад";
+            return Format(ResourceKeys.DateHumanize__months_ago, number);
         }
 
         public override string DateHumanize__years_ago(int number)
         {
-            return string.Format(Ago, number, Select(number, "год", "года", "лет"));
-        }
-
-        public override string DateHumanize_not_yet()
-        {
-            return "в будущем";
+            return Format(ResourceKeys.DateHumanize__years_ago, number);
         }
     }
 }
