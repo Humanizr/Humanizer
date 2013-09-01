e2c6ef3a44011d99ab9270600bb683515988e39a
Pekka Heikura <pekkah@gmail.com>
2013-09-01 12:45:25 +0300

    fi-FI: Basic date humanization
    Humanize dates using fi-FI localized resources

---
+++ src/Humanizer.Tests/Extensions/Localisation/DateHumanizeTests.fi-FI.cs
@@ -0,0 +1,89 @@
+﻿namespace Humanizer.Tests.Extensions.Localisation
+{
+    using System;
+    using Xunit;
+    using Xunit.Extensions;
+
+    public class DateHumanizeTests_fiFI : AmbientCulture
+    {
+        public DateHumanizeTests_fiFI()
+            : base("fi-Fi")
+        {
+        }
+
+        [Theory]
+        [InlineData(-10, "10 päivää sitten")]
+        [InlineData(-3, "3 päivää sitten")]
+        [InlineData(-2, "2 päivää sitten")]
+		[InlineData(-1, "eilen")]
+        public void DaysAgo(int days, string expected)
+        {
+            var date = DateTime.UtcNow.AddDays(days);
+			Assert.Equal(expected, date.Humanize());
+        }
+
+        [Theory]
+        [InlineData(-10, "10 tuntia sitten")]
+        [InlineData(-3, "3 tuntia sitten")]
+        [InlineData(-2, "2 tuntia sitten")]
+        [InlineData(-1, "tunti sitten")]
+        public void HoursAgo(int hours, string expected)
+        {
+            var date = DateTime.UtcNow.AddHours(hours);
+            Assert.Equal(expected, date.Humanize());
+        }
+
+        [Theory]
+        [InlineData(-10, "10 minuuttia sitten")]
+        [InlineData(-3, "3 minuuttia sitten")]
+        [InlineData(-2, "2 minuuttia sitten")]
+        [InlineData(-1, "minuutti sitten")]
+        public void MinutesAgo(int minutes, string expected)
+        {
+            var date = DateTime.UtcNow.AddMinutes(minutes);
+            Assert.Equal(expected, date.Humanize());
+        }
+
+        [Theory]
+        [InlineData(-10, "10 kuukautta sitten")]
+        [InlineData(-3, "3 kuukautta sitten")]
+        [InlineData(-2, "2 kuukautta sitten")]
+        [InlineData(-1, "kuukausi sitten")]
+        public void MonthsAgo(int months, string expected)
+        {
+            var date = DateTime.UtcNow.AddMonths(months);
+            Assert.Equal(expected, date.Humanize());
+        }
+
+        [Theory]
+        [InlineData(-10, "10 sekunttia sitten")]
+        [InlineData(-3, "3 sekunttia sitten")]
+        [InlineData(-2, "2 sekunttia sitten")]
+        [InlineData(-1, "sekuntti sitten")]
+        public void SecondsAgo(int seconds, string expected)
+        {
+            var date = DateTime.UtcNow.AddSeconds(seconds);
+            Assert.Equal(expected, date.Humanize());
+        }
+
+        [Theory]
+        [InlineData(-10, "10 vuotta sitten")]
+        [InlineData(-3, "3 vuotta sitten")]
+        [InlineData(-2, "2 vuotta sitten")]
+        [InlineData(-1, "vuosi sitten")]
+        public void YearsAgo(int years, string expected)
+        {
+            var date = DateTime.UtcNow.AddYears(years);
+            Assert.Equal(expected, date.Humanize());
+        }
+
+        [Fact]
+        public void NotYet()
+        {
+			// does not yet mean in future?
+            var date = DateTime.UtcNow.AddDays(1);
+
+			Assert.Equal("ei vielä", date.Humanize());
+        }
+    }
+}
