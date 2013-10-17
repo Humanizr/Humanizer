7bfada8bc44f90aa29ab4560271bd3d98183ee3b
MehdiK <me@mehdi-khalili.com>
2013-10-17 16:26:56 +0330

    cleaned up Dasherize, Pascalize and Camelize

--- src/Humanizer.Tests/Extensions/StringHumanizeTests.cs
+++ src/Humanizer.Tests/Extensions/StringHumanizeTests.cs
@@ -1,8 +1,9 @@
 ﻿using Xunit;
+using Xunit.Extensions;
 
 namespace Humanizer.Tests.Extensions
 {
-    public class StringExtensionsTests
+    public class StringHumanizeTests
     {
         [Fact]
         public void PascalCaseInputStringIsTurnedIntoSentence()
@@ -174,6 +175,45 @@ namespace Humanizer.Tests.Extensions
             Assert.Equal(
                 "CAN HUMANIZE INTO UPPER CASE",
                 "Can_Humanize_into_Upper_case".Humanize(LetterCasing.AllCaps));
+        }
+        
+        [InlineData("some_title", "some-title")]
+        [InlineData("some-title", "some-title")]
+        [InlineData("some_title_goes_here", "some-title-goes-here")]
+        [InlineData("some_title and_another", "some-title and-another")]
+        [Theory]
+        public void Dasherize(string input, string expectedOutput)
+        {
+            Assert.Equal(input.Dasherize(), expectedOutput);
+        }
+
+        [Theory]
+        [InlineData("customer", "Customer")]
+        [InlineData("CUSTOMER", "CUSTOMER")]
+        [InlineData("CUStomer", "CUStomer")]
+        [InlineData("customer_name", "CustomerName")]
+        [InlineData("customer_first_name", "CustomerFirstName")]
+        [InlineData("customer_first_name_goes_here", "CustomerFirstNameGoesHere")]
+        [InlineData("customer name", "Customer name")]
+        public void Pascalize(string input, string expectedOutput)
+        {
+            Assert.Equal(expectedOutput, input.Pascalize());
+        }
+
+        /// <summary>
+        /// Same as pascalize, except first char is lowercase
+        /// </summary>
+        [Theory]
+        [InlineData("customer", "customer")]
+        [InlineData("CUSTOMER", "cUSTOMER")]
+        [InlineData("CUStomer", "cUStomer")]
+        [InlineData("customer_name", "customerName")]
+        [InlineData("customer_first_name", "customerFirstName")]
+        [InlineData("customer_first_name_goes_here", "customerFirstNameGoesHere")]
+        [InlineData("customer name", "customer name")]
+        public void Camelize(string input, string expectedOutput)
+        {
+            Assert.Equal(expectedOutput, input.Camelize());
         }
     }
 }
