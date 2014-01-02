eb24c05764630709f83fa17d2fe0deb04d80875e
MehdiK <me@mehdi-khalili.com>
2014-01-02 09:43:48 +0330

    added in githubflowversion

---
+++ tools/GitHubFlowVersion/install.ps1
@@ -0,0 +1,51 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$ErrorActionPreference = "Stop"
+
+if ($project -ne $null)
+{
+    Foreach ($item in $project.ProjectItems)
+    {
+        if ($item.Name -eq "ToBeRemoved.txt")
+        {
+            $item.Delete()
+        }
+    }
+}
+
+$gitDir = $null
+$workingDirectory = (Get-Item $project.FullName).Directory
+Write-Host "Looking for .git directory, starting in $workingDirectory"
+while ($true)
+{
+    $possibleGitDir = Join-Path $workingDirectory.FullName ".git"
+    if (Test-Path $possibleGitDir)
+    {
+        $gitDir = $possibleGitDir
+        Break
+    }
+    $parent = $workingDirectory.Parent
+    if ($parent -eq $null)
+    {
+        Break
+    }
+    $workingDirectory = $parent;
+}
+
+if ($gitDir -ne $null)
+{
+    Write-Host "Found git directory for project at $gitDir"
+    $repositoryDir = (get-item $gitDir -Force).Parent.FullName
+    $gitHubFlowToolsDir = Join-Path $repositoryDir "tools\GitHubFlowVersion"
+    if ((Test-Path $gitHubFlowToolsDir -PathType Container) -eq $false)
+    {
+        Write-Host "Creating directory $gitHubFlowToolsDir"
+    }
+    
+    Write-Host "GitHubFlowVersion tools installed to $gitHubFlowToolsDir"
+    Copy-Item $toolsPath –destination $gitHubFlowToolsDir -recurse -container -force
+}
+else
+{
+    Write-Host "Cannot find git directory"
+}
