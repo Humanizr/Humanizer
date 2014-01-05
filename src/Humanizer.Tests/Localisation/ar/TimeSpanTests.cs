8b6a26c590486cffa21f0deeffe622648166dcc6
Ahmed EL-Harouny <ahmedelharouny@yahoo.com>
2014-01-06 09:57:33 +1100

    Add Unit Tests for Arabic localization of TimeSpan humanized strings

---
+++ src/Humanizer.Tests/Localisation/ar/TimeSpanTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Xunit;
+
+namespace Humanizer.Tests.Localisation.ar
+{
+    public class TimeSpanHumanizeExtensionsTests : AmbientCulture
+    {
+        public TimeSpanHumanizeExtensionsTests() : base("ar") {         }
+
+        [Fact]
+        public void OneWeek()
+        {
+            Assert.Equal("أسبوع واحد", TimeSpan.FromDays(7).Humanize());
+        }
+
+        [Fact]
+        public void OneDay()
+        {
+            Assert.Equal("يوم واحد", TimeSpan.FromDays(1).Humanize());
+        }
+
+        [Fact]
+        public void OneHour()
+        {
+            Assert.Equal("ساعة واحدة", TimeSpan.FromHours(1).Humanize());
+        }
+
+        [Fact]
+        public void OneMinute()
+        {
+            Assert.Equal("دقيقة واحدة", TimeSpan.FromMinutes(1).Humanize());
+        }
+
+        [Fact]
+        public void OneSecond()
+        {
+            Assert.Equal("ثانية واحدة", TimeSpan.FromSeconds(1).Humanize());
+        }
+
+        [Fact]
+        public void OneMillisecond()
+        {
+            Assert.Equal("جزء من الثانية", TimeSpan.FromMilliseconds(1).Humanize());
+        }
+
+        [Fact]
+        public void NoTime()
+        {
+            Assert.Equal("حالاً", TimeSpan.Zero.Humanize());
+        }
+    }
+}
