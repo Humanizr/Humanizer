4bfe27b33a4051aea9745f0f4e4ffd576cd6d8ed
Ahmed EL-Harouny <ahmedelharouny@yahoo.com>
2014-01-08 12:22:54 +1100

    Fixing a review issue - codebase convention

--- src/Humanizer/Localisation/ArabicFormatter.cs
+++ src/Humanizer/Localisation/ArabicFormatter.cs
@@ -7,9 +7,8 @@
         protected override string GetResourceKey(string resourceKey, int number)
         {
             if (number == 2)
-            {
                 return resourceKey + DualPostfix;
-            }
+
             return resourceKey;
         }
     }
