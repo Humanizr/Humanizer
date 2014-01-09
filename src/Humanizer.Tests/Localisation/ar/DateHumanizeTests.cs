711e270deafd8717ad92239865d1a7bf73949911
Ahmed EL-Harouny <ahmedelharouny@yahoo.com>
2014-01-09 23:19:19 +1100

    Add Arabic (more than 2) units support for TimeSpan and DateHumanize

--- src/Humanizer.Tests/Localisation/ar/DateHumanizeTests.cs
+++ src/Humanizer.Tests/Localisation/ar/DateHumanizeTests.cs
@@ -8,9 +8,12 @@ namespace Humanizer.Tests.Localisation.ar
     {
         public DateHumanizeTests() : base("ar") { }
 
+
         [Theory]
-        [InlineData(-2, "منذ يومين")]
         [InlineData(-1, "أمس")]
+        [InlineData(-2, "منذ يومين")]
+        [InlineData(-3, "منذ 3 أيام")]
+        [InlineData(-11, "منذ 11 يوم")]
         public void DaysAgo(int days, string expected)
         {
             Assert.Equal(expected, DateTime.UtcNow.AddDays(days).Humanize());
@@ -19,6 +22,8 @@ namespace Humanizer.Tests.Localisation.ar
         [Theory]
         [InlineData(-2, "منذ ساعتين")]
         [InlineData(-1, "منذ ساعة واحدة")]
+        [InlineData(-3, "منذ 3 ساعات")]
+        [InlineData(-11, "منذ 11 ساعة")]
         public void HoursAgo(int hours, string expected)
         {
             Assert.Equal(expected, DateTime.UtcNow.AddHours(hours).Humanize());
@@ -27,6 +32,8 @@ namespace Humanizer.Tests.Localisation.ar
         [Theory]
         [InlineData(-2, "منذ دقيقتين")]
         [InlineData(-1, "منذ دقيقة واحدة")]
+        [InlineData(-3, "منذ 3 دقائق")]
+        [InlineData(-11, "منذ 11 دقيقة")]
         public void MinutesAgo(int minutes, string expected)
         {
             Assert.Equal(expected, DateTime.UtcNow.AddMinutes(minutes).Humanize());
@@ -35,6 +42,8 @@ namespace Humanizer.Tests.Localisation.ar
         [Theory]
         [InlineData(-2, "منذ شهرين")]
         [InlineData(-1, "منذ شهر واحد")]
+        [InlineData(-3, "منذ 3 أشهر")]
+        [InlineData(-11, "منذ 11 شهر")]
         public void MonthsAgo(int months, string expected)
         {
             Assert.Equal(expected, DateTime.UtcNow.AddMonths(months).Humanize());
@@ -43,14 +52,18 @@ namespace Humanizer.Tests.Localisation.ar
         [Theory]
         [InlineData(-2, "منذ ثانيتين")]
         [InlineData(-1, "منذ ثانية واحدة")]
+        [InlineData(-3, "منذ 3 ثوان")]
+        [InlineData(-11, "منذ 11 ثانية")]
         public void SecondsAgo(int seconds, string expected)
         {
             Assert.Equal(expected, DateTime.UtcNow.AddSeconds(seconds).Humanize());
         }
 
         [Theory]
-        [InlineData(-2, "منذ سنتين")]
+        [InlineData(-2, "منذ عامين")]
         [InlineData(-1, "العام السابق")]
+        [InlineData(-3, "منذ 3 أعوام")]
+        [InlineData(-11, "منذ 11 عام")]
         public void YearsAgo(int years, string expected)
         {
             Assert.Equal(expected, DateTime.UtcNow.AddYears(years).Humanize());
--- src/Humanizer.Tests/Localisation/ar/TimeSpanTests.cs
+++ src/Humanizer.Tests/Localisation/ar/TimeSpanTests.cs
@@ -11,6 +11,8 @@ namespace Humanizer.Tests.Localisation.ar
         [Theory]
         [InlineData(7, "أسبوع واحد")]
         [InlineData(14, "أسبوعين")]
+        [InlineData(21, "3 أسابيع")]
+        [InlineData(77, "11 أسبوع")]
         public void Weeks(int days, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromDays(days).Humanize());
@@ -20,6 +22,7 @@ namespace Humanizer.Tests.Localisation.ar
         [Theory]
         [InlineData(1, "يوم واحد")]
         [InlineData(2, "يومين")]
+        [InlineData(3, "3 أيام")]
         public void Days(int days, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromDays(days).Humanize());
@@ -28,6 +31,8 @@ namespace Humanizer.Tests.Localisation.ar
         [Theory]
         [InlineData(1, "ساعة واحدة")]
         [InlineData(2, "ساعتين")]
+        [InlineData(3, "3 ساعات")]
+        [InlineData(11, "11 ساعة")]
         public void Hours(int hours, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromHours(hours).Humanize());
@@ -36,6 +41,8 @@ namespace Humanizer.Tests.Localisation.ar
         [Theory]
         [InlineData(1, "دقيقة واحدة")]
         [InlineData(2, "دقيقتين")]
+        [InlineData(3, "3 دقائق")]
+        [InlineData(11, "11 دقيقة")]
         public void Minutes(int minutes, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromMinutes(minutes).Humanize());
@@ -45,6 +52,8 @@ namespace Humanizer.Tests.Localisation.ar
         [Theory]
         [InlineData(1, "ثانية واحدة")]
         [InlineData(2, "ثانيتين")]
+        [InlineData(3, "3 ثوان")]
+        [InlineData(11, "11 ثانية")]
         public void Seconds(int seconds, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromSeconds(seconds).Humanize());
@@ -53,6 +62,8 @@ namespace Humanizer.Tests.Localisation.ar
         [Theory]
         [InlineData(1, "جزء من الثانية")]
         [InlineData(2, "جزئين من الثانية")]
+        [InlineData(3, "3 أجزاء من الثانية")]
+        [InlineData(11, "11 جزء من الثانية")]
         public void Milliseconds(int milliseconds, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromMilliseconds(milliseconds).Humanize());
