9be7f971cadd892b9a34d86d9afab33324a4ce9d
Nigel Sampson <nigel.sampson@compiledexperience.com>
2014-02-12 14:07:41 +1300

    NeutralResourcesLanguage resolves #75 in Windows Store apps

--- src/Humanizer/Properties/AssemblyInfo.cs
+++ src/Humanizer/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -33,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0")]
 [assembly: AssemblyFileVersion("1.0")]
+[assembly: NeutralResourcesLanguage("en")]
