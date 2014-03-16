e71c5caf765ac88e14c19898c6905588b774c677
George Hahn <george@genericmaker.com>
2014-03-16 05:18:55 -0400

    Added full tests (don't see a good way to parameterize these -_-)

--- src/Humanizer.Tests/ByteSizeTests.cs
+++ src/Humanizer.Tests/ByteSizeTests.cs
@@ -20,6 +20,61 @@ namespace Humanizer.Tests
         }
 
         [Fact]
+        public void Gigabytes()
+        {
+            var size = (2.0).Gigabytes();
+            Assert.Equal(ByteSize.FromGigaBytes(2), size);
+        }
+
+        [Fact]
+        public void HumanizesGB()
+        {
+            var humanized = (2.0).Gigabytes().Humanize();
+            Assert.Equal("2 GB", humanized);
+        }
+
+        [Fact]
+        public void Kilobytes()
+        {
+            var size = (2.0).Kilobytes();
+            Assert.Equal(ByteSize.FromKiloBytes(2), size);
+        }
+
+        [Fact]
+        public void HumanizesKB()
+        {
+            var humanized = (2.0).Kilobytes().Humanize();
+            Assert.Equal("2 KB", humanized);
+        }
+
+        [Fact]
+        public void Bytes()
+        {
+            var size = (2.0).Bytes();
+            Assert.Equal(ByteSize.FromBytes(2), size);
+        }
+
+        [Fact]
+        public void HumanizesBytes()
+        {
+            var humanized = (2.0).Bytes().Humanize();
+            Assert.Equal("2 B", humanized);
+        }
+
+        [Fact]
+        public void Bits()
+        {
+            var size = ((long)2).Bits();
+            Assert.Equal(ByteSize.FromBits(2), size);
+        }
+
+        [Fact]
+        public void HumanizesBits()
+        {
+            var humanized = ((long)2).Bits().Humanize();
+            Assert.Equal("2 b", humanized);
+        }
+        [Fact]
         public void HumanizesWithFormat()
         {
             // TODO
