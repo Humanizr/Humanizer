1448f851e237a4117643a9e617209fcc627a3363
MehdiK <me@mehdi-khalili.com>
2014-03-19 09:27:46 +0430

    Fixes DateTime.Humanize random test fails

--- src/Humanizer.Tests/DateHumanizeTests.cs
+++ src/Humanizer.Tests/DateHumanizeTests.cs
@@ -11,8 +11,9 @@ namespace Humanizer.Tests
             var utcNow = DateTime.UtcNow;
             var localNow = DateTime.Now;
 
-            Assert.Equal(expectedString, utcNow.Add(deltaFromNow).Humanize());
-            Assert.Equal(expectedString, localNow.Add(deltaFromNow).Humanize(false));
+            // feels like the only way to avoid breaking tests because CPU ticks over is to inject the base date
+            Assert.Equal(expectedString, utcNow.Add(deltaFromNow).Humanize(utcDate: true, dateToCompareAgainst: utcNow));
+            Assert.Equal(expectedString, localNow.Add(deltaFromNow).Humanize(utcDate: false, dateToCompareAgainst: localNow));
         }
 
         static void VerifyWithDateInjection(string expectedString, TimeSpan deltaFromNow)
