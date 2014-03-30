9c98702702221bfbd1bf8dd27c13e373d1394cda
Wahid Shalaly <waheedsayed@gmail.com>
2014-03-30 21:00:03 +1100

    Added unit tests for StringExtensions

---
+++ src/Humanizer.Tests/StringExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+
+namespace Humanizer.Tests
+{
+    public class StringExtensionsTests
+    {
+        private const string format = "This is a format with three numbers: {0}-{1}-{2}.";
+        private const string expected = "This is a format with three numbers: 1-2-3.";
+
+        [Fact]
+        public void CanFormatStringWithExactNumberOfArguments()
+        {
+            string actual = format.FormatWith(1, 2, 3);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CanFormatStringWithMoreArguments()
+        {
+            string actual = format.FormatWith(1, 2, 3, 4, 5);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CannotFormatStringWithLessArguments()
+        {
+            Assert.Throws<FormatException>(() => format.FormatWith(1, 2));
+        }
+    }
+}
