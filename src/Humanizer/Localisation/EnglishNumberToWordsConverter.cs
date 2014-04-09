60f4e2cd3227ff19af314d876bfcae57fe6138c3
Alexander I. Zaytsev <hazzik@gmail.com>
2014-04-10 10:01:12 +1200

    EnglishNumberToWordsConverter - promote units and tens maps to a static fields to reduce allocations.

--- src/Humanizer/Localisation/EnglishNumberToWordsConverter.cs
+++ src/Humanizer/Localisation/EnglishNumberToWordsConverter.cs
@@ -5,37 +5,40 @@ namespace Humanizer.Localisation
 {
     internal class EnglishNumberToWordsConverter : INumberToWordsConverter
     {
+        private static readonly string[] UnitsMap = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+        private static readonly string[] TensMap = { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
         public string Convert(int number)
         {
             if (number == 0)
                 return "zero";
 
             if (number < 0)
-                return string.Format("minus {0}", Math.Abs(number).ToWords());
+                return string.Format("minus {0}", Convert(-number));
 
             var parts = new List<string>();
 
             if ((number / 1000000000) > 0)
             {
-                parts.Add(string.Format("{0} billion", (number / 1000000000).ToWords()));
+                parts.Add(string.Format("{0} billion", Convert(number / 1000000000)));
                 number %= 1000000000;
             }
 
             if ((number / 1000000) > 0)
             {
-                parts.Add(string.Format("{0} million", (number / 1000000).ToWords()));
+                parts.Add(string.Format("{0} million", Convert(number / 1000000)));
                 number %= 1000000;
             }
 
             if ((number / 1000) > 0)
             {
-                parts.Add(string.Format("{0} thousand", NumberToWordsExtension.ToWords(number / 1000)));
+                parts.Add(string.Format("{0} thousand", Convert(number / 1000)));
                 number %= 1000;
             }
 
             if ((number / 100) > 0)
             {
-                parts.Add(string.Format("{0} hundred", NumberToWordsExtension.ToWords(number / 100)));
+                parts.Add(string.Format("{0} hundred", Convert(number / 100)));
                 number %= 100;
             }
 
@@ -44,16 +47,13 @@ namespace Humanizer.Localisation
                 if (parts.Count != 0)
                     parts.Add("and");
 
-                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
-                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-
                 if (number < 20)
-                    parts.Add(unitsMap[number]);
+                    parts.Add(UnitsMap[number]);
                 else
                 {
-                    var lastPart = tensMap[number / 10];
+                    var lastPart = TensMap[number / 10];
                     if ((number % 10) > 0)
-                        lastPart += string.Format("-{0}", unitsMap[number % 10]);
+                        lastPart += string.Format("-{0}", UnitsMap[number % 10]);
 
                     parts.Add(lastPart);
                 }
