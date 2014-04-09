48439796b24ff0480592e034e73c71eb8149f348
Glenn F. Henriksen <glenn@henriksen.no>
2014-04-09 10:55:53 +0200

    Fixed ett/et gramatical error in nb-NO resource file
    Added some tests for timespan in nb-NO locale. Fixed ett/et errors in
    the resource file

---
+++ src/Humanizer.Tests/Localisation/nb-NO/TimeSpanHumanizeTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Humanizer.Tests.Localisation.nbNO
+{
+    public class TimeSpanHumanizeTests : AmbientCulture
+    {
+        public TimeSpanHumanizeTests() : base("nb-NO") { }
+
+        [Theory]
+        [InlineData(7, "en uke")]
+        public void Weeks(int days, string expected)
+        {
+            Assert.Equal(expected, TimeSpan.FromDays(days).Humanize());
+        }
+
+        [Theory]
+        [InlineData(1, "en dag")]
+        public void Days(int days, string expected)
+        {
+            Assert.Equal(expected, TimeSpan.FromDays(days).Humanize());
+        }
+
+        [Theory]
+        [InlineData(1, "en time")]
+        public void Hours(int hours, string expected)
+        {
+            Assert.Equal(expected, TimeSpan.FromHours(hours).Humanize());
+        }
+
+        [Theory]
+        [InlineData(1, "ett minutt")]
+        public void Minutes(int minutes, string expected)
+        {
+            Assert.Equal(expected, TimeSpan.FromMinutes(minutes).Humanize());
+        }
+
+        [Theory]
+        [InlineData(1, "ett sekund")]
+        public void Seconds(int seconds, string expected)
+        {
+            Assert.Equal(expected, TimeSpan.FromSeconds(seconds).Humanize());
+        }
+
+        [Theory]
+        [InlineData(1, "ett millisekund")]
+        public void Milliseconds(int milliseconds, string expected)
+        {
+            Assert.Equal(expected, TimeSpan.FromMilliseconds(milliseconds).Humanize());
+        }
+
+        [Fact]
+        public void NoTime()
+        {
+            Assert.Equal("ingen tid", TimeSpan.Zero.Humanize());
+        }
+    }
+}
