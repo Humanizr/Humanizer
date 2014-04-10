d303747db462b09e80772dceb3e599fc0d78f359
Mahmoud Ali <muddibr@gmail.com>
2014-04-10 19:45:04 -0400

    Changed the NumberToWords Converter to find the right converter by the language Name aswell. This is needed for languages that can be ambiguous in Two Letter ISO Code (pt-BR, pt-PT both have 'pt' Two Letter ISO code).

---
+++ src/Humanizer.Tests/Localisation/NumerToWordsFactoryTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Humanizer.Tests.Localisation
+{
+    public class NumerToWordsFactoryTests
+    {
+        [Fact]
+        public void CanGetRFCStandardLanguageSpecificFactory()
+        {
+
+            using (new AmbientCulture("pt-BR"))
+            {
+                string retorno = 1000000000.ToWords();
+                Assert.NotEqual("one billion",retorno);
+            }
+        }
+
+        [Fact]
+        public void CanGetTwoLetterISOLanguageSpecificFactory()
+        {
+
+            using (new AmbientCulture("ar"))
+            {
+                string retorno = 1000000000.ToWords();
+                Assert.NotEqual("one billion", retorno);
+            }
+        }
+    }
+}
--- src/Humanizer/NumberToWordsExtension.cs
+++ src/Humanizer/NumberToWordsExtension.cs
@@ -1,9 +1,6 @@
-﻿using Humanizer.Localisation.NumberToWords;
-using System;
 using System.Collections.Generic;
 using System.Globalization;
 
-namespace Humanizer {
     /// <summary>
     /// Transform a number into words; e.g. 1 => one
     /// </summary>
@@ -16,6 +13,7 @@ namespace Humanizer {
                 { "fa", () => new FarsiNumberToWordsConverter() },
                 { "es", () => new SpanishNumberToWordsConverter() },
                 { "pl", () => new PolishNumberToWordsConverter() }
+                { "pt-BR", () => new BrazilianPortugueseNumberToWordsConverter() },
             };
 
         /// <summary>
@@ -33,6 +31,10 @@ namespace Humanizer {
             get 
             {
                 Func<INumberToWordsConverter> converterFactory;
+
+                if (ConverterFactories.TryGetValue(CultureInfo.CurrentUICulture.Name, out converterFactory))
+                    return converterFactory();
+
                 if (ConverterFactories.TryGetValue(CultureInfo.CurrentUICulture.TwoLetterISOLanguageName, out converterFactory))
                     return converterFactory();
 
