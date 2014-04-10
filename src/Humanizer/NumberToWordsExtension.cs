d59b5d3f868bca2f66cc174ab479b4bc9b146e54
Marcin Nowacki <nowacki.marcin@gmail.com>
2014-04-10 22:12:49 +0200

    Added Polish translation for ToWords

--- src/Humanizer/NumberToWordsExtension.cs
+++ src/Humanizer/NumberToWordsExtension.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using Humanizer.Localisation.NumberToWords;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
-using Humanizer.Localisation.NumberToWords;
 
 namespace Humanizer {
     /// <summary>
@@ -14,7 +14,8 @@ namespace Humanizer {
                 { "en", () => new EnglishNumberToWordsConverter() },
                 { "ar", () => new ArabicNumberToWordsConverter() },
                 { "fa", () => new FarsiNumberToWordsConverter() },
-                { "es", () => new SpanishNumberToWordsConverter() }
+                { "es", () => new SpanishNumberToWordsConverter() },
+                { "pl", () => new PolishNumberToWordsConverter() }
             };
 
         /// <summary>
