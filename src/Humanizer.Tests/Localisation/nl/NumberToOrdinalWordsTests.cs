2ec8e0dbe5d0abaede0e82e414501f623c7fc60c
Ryan Heath <ryan.q.heath@gmail.com>
2014-04-10 16:55:00 +0200

    replaced tabs with spaces

--- src/Humanizer.Tests/Localisation/nl/NumberToOrdinalWordsTests.cs
+++ src/Humanizer.Tests/Localisation/nl/NumberToOrdinalWordsTests.cs
@@ -5,9 +5,9 @@ namespace Humanizer.Tests.Localisation.nl
 {
     public class NumberToOrdinalWordsTests : AmbientCulture
     {
-		public NumberToOrdinalWordsTests() : base("nl-NL") { }
+        public NumberToOrdinalWordsTests() : base("nl-NL") { }
 
-		[Theory]
+        [Theory]
         [InlineData(0, "nulde")]
         [InlineData(1, "eerste")]
         [InlineData(2, "tweede")]
@@ -26,11 +26,11 @@ namespace Humanizer.Tests.Localisation.nl
         [InlineData(15, "vijftiende")]
         [InlineData(16, "zestiende")]
         [InlineData(17, "zeventiende")]
-		[InlineData(18, "achttiende")]
+        [InlineData(18, "achttiende")]
         [InlineData(19, "negentiende")]
         [InlineData(20, "twintigste")]
         [InlineData(21, "eenentwintigste")]
-		[InlineData(22, "tweeëntwintigste")]
+        [InlineData(22, "tweeëntwintigste")]
         [InlineData(30, "dertigste")]
         [InlineData(40, "veertigste")]
         [InlineData(50, "vijftigste")]
@@ -41,24 +41,24 @@ namespace Humanizer.Tests.Localisation.nl
         [InlineData(95, "vijfennegentigste")]
         [InlineData(96, "zesennegentigste")]
         [InlineData(100, "honderdste")]
-		[InlineData(101, "honderdeerste")]
-		[InlineData(106, "honderdzesde")]
-		[InlineData(108, "honderdachtste")]
-		[InlineData(112, "honderdtwaalfde")]
+        [InlineData(101, "honderdeerste")]
+        [InlineData(106, "honderdzesde")]
+        [InlineData(108, "honderdachtste")]
+        [InlineData(112, "honderdtwaalfde")]
         [InlineData(120, "honderdtwintigste")]
         [InlineData(121, "honderdeenentwintigste")]
         [InlineData(1000, "duizendste")]
         [InlineData(1001, "duizend eerste")]
-		[InlineData(1005, "duizend vijfde")]
-		[InlineData(1008, "duizend achtste")]
-		[InlineData(1012, "duizend twaalfde")]
+        [InlineData(1005, "duizend vijfde")]
+        [InlineData(1008, "duizend achtste")]
+        [InlineData(1012, "duizend twaalfde")]
         [InlineData(1021, "duizend eenentwintigste")]
         [InlineData(10000, "tienduizendste")]
         [InlineData(10121, "tienduizend honderdeenentwintigste")]
         [InlineData(100000, "honderdduizendste")]
-		[InlineData(100001, "honderdduizend eerste")]
+        [InlineData(100001, "honderdduizend eerste")]
         [InlineData(1000000, "een miljoenste")]
-		[InlineData(1000001, "een miljoen eerste")]
+        [InlineData(1000001, "een miljoen eerste")]
         public void ToOrdinalWords(int number, string words)
         {
             Assert.Equal(words, number.ToOrdinalWords());
--- src/Humanizer/Localisation/NumberToWords/DutchNumberToWordsConverter.cs
+++ src/Humanizer/Localisation/NumberToWords/DutchNumberToWordsConverter.cs
@@ -3,27 +3,27 @@ using System.Collections.Generic;
 
 namespace Humanizer.Localisation.NumberToWords
 {
-	/// <summary>
-	/// Dutch spelling of numbers is not really officially regulated.
-	/// There are a few different rules that can be applied.
-	/// Used the rules as stated here.
-	/// http://www.beterspellen.nl/website/?pag=110
-	/// </summary>
+    /// <summary>
+    /// Dutch spelling of numbers is not really officially regulated.
+    /// There are a few different rules that can be applied.
+    /// Used the rules as stated here.
+    /// http://www.beterspellen.nl/website/?pag=110
+    /// </summary>
     internal class DutchNumberToWordsConverter : INumberToWordsConverter
     {
         private static readonly string[] UnitsMap = { "nul", "een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "tien", "elf", "twaalf", "dertien", "veertien", "vijftien", "zestien", "zeventien", "achttien", "negentien" };
         private static readonly string[] TensMap = { "nul", "tien", "twintig", "dertig", "veertig", "vijftig", "zestig", "zeventig", "tachtig", "negentig" };
 
-	    class Fact
-	    {
-		    public int Value { get; set; }
-			public string Name { get; set; }
-			public string Prefix { get; set; }
-			public string Postfix { get; set; }
-			public bool DisplayOneUnit { get; set; }
-	    }
+        class Fact
+        {
+            public int Value { get; set; }
+            public string Name { get; set; }
+            public string Prefix { get; set; }
+            public string Postfix { get; set; }
+            public bool DisplayOneUnit { get; set; }
+        }
 
-	    private static readonly Fact[] Hunderds =
+        private static readonly Fact[] Hunderds =
 	    {
 		    new Fact {Value = 1000000000, Name = "miljard", Prefix = " ", Postfix = " ", DisplayOneUnit = true},
 		    new Fact {Value = 1000000,    Name = "miljoen", Prefix = " ", Postfix = " ", DisplayOneUnit = true},
@@ -38,59 +38,59 @@ namespace Humanizer.Localisation.NumberToWords
             {8, "achtste"},
         };
 
-	    public string Convert(int number)
-	    {
-			if (number == 0)
-				return UnitsMap[0];
+        public string Convert(int number)
+        {
+            if (number == 0)
+                return UnitsMap[0];
 
-			if (number < 0)
-				return string.Format("min {0}", Convert(-number));
+            if (number < 0)
+                return string.Format("min {0}", Convert(-number));
 
-		    var word = "";
+            var word = "";
 
-	        foreach (var m in Hunderds)
-	        {
-		        var divided = number/m.Value;
+            foreach (var m in Hunderds)
+            {
+                var divided = number / m.Value;
 
-		        if (divided <= 0) continue;
+                if (divided <= 0) continue;
 
-		        if (divided == 1 && !m.DisplayOneUnit)
-		        {
-			        word += m.Name;
-		        }
-		        else
-		        {
-			        word += Convert(divided) + m.Prefix + m.Name;
-		        }
-					
-		        number %= m.Value;
-		        if (number > 0)
-		        {
-			        word += m.Postfix;
-		        }
-	        }
+                if (divided == 1 && !m.DisplayOneUnit)
+                {
+                    word += m.Name;
+                }
+                else
+                {
+                    word += Convert(divided) + m.Prefix + m.Name;
+                }
+
+                number %= m.Value;
+                if (number > 0)
+                {
+                    word += m.Postfix;
+                }
+            }
 
             if (number > 0)
             {
-	            if (number < 20)
-	            {
-		            word += UnitsMap[number];
-	            }
-	            else
-	            {
-		            var tens = TensMap[number/10];
-		            var unit = number%10;
-		            if (unit > 0)
-		            {
-			            var units = UnitsMap[unit];
-						var trema = units.EndsWith("e");
-						word += units + (trema ? "ën" : "en") + tens;
-		            }
-		            else
-		            {
-						word += tens;    
-		            }
-	            }
+                if (number < 20)
+                {
+                    word += UnitsMap[number];
+                }
+                else
+                {
+                    var tens = TensMap[number / 10];
+                    var unit = number % 10;
+                    if (unit > 0)
+                    {
+                        var units = UnitsMap[unit];
+                        var trema = units.EndsWith("e");
+                        word += units + (trema ? "ën" : "en") + tens;
+                    }
+                    else
+                    {
+                        word += tens;
+                    }
+                }
             }
 
             return word;
@@ -98,41 +98,41 @@ namespace Humanizer.Localisation.NumberToWords
 
         public string ConvertToOrdinal(int number)
         {
-			var exceptionalTens = number % 100;
+            var exceptionalTens = number % 100;
 
-			// 112 => 12 => honderdtwaalfde
-			if (exceptionalTens < 20 && number > 20 && exceptionalTens > 0)
-			{
-				// replace normal word with ordinal version
-				return ReplaceNormalWordWithOrdinalWord(number, exceptionalTens);
-			}
+            // 112 => 12 => honderdtwaalfde
+            if (exceptionalTens < 20 && number > 20 && exceptionalTens > 0)
+            {
+                // replace normal word with ordinal version
+                return ReplaceNormalWordWithOrdinalWord(number, exceptionalTens);
+            }
 
-			var exceptionalUnit = number % 10;
+            var exceptionalUnit = number % 10;
 
-			string exceptionalWord;
-			if (exceptionalTens < 10 && ExceptionNumbersToWords(exceptionalUnit, out exceptionalWord))
-	        {
-				// replace normal word with ordinal version
-		        return ReplaceNormalWordWithOrdinalWord(number, exceptionalUnit, exceptionalWord);
-	        }
+            string exceptionalWord;
+            if (exceptionalTens < 10 && ExceptionNumbersToWords(exceptionalUnit, out exceptionalWord))
+            {
+                // replace normal word with ordinal version
+                return ReplaceNormalWordWithOrdinalWord(number, exceptionalUnit, exceptionalWord);
+            }
 
-	        return number < 20 ? Convert(number) + "de" : Convert(number) + "ste";
+            return number < 20 ? Convert(number) + "de" : Convert(number) + "ste";
         }
 
-		private string ReplaceNormalWordWithOrdinalWord(int number, int exceptionalNumber)
-		{
-			return ReplaceNormalWordWithOrdinalWord(number, exceptionalNumber, Convert(exceptionalNumber));
-		}
+        private string ReplaceNormalWordWithOrdinalWord(int number, int exceptionalNumber)
+        {
+            return ReplaceNormalWordWithOrdinalWord(number, exceptionalNumber, Convert(exceptionalNumber));
+        }
 
-		private string ReplaceNormalWordWithOrdinalWord(int number, int exceptionalNumber, string ordinalWord)
-		{
-			var word = Convert(number);
-			var normalWord = Convert(exceptionalNumber);
-			var pos = word.LastIndexOf(normalWord, StringComparison.CurrentCulture);
-			return word.Substring(0, pos) + ordinalWord;
-		}
+        private string ReplaceNormalWordWithOrdinalWord(int number, int exceptionalNumber, string ordinalWord)
+        {
+            var word = Convert(number);
+            var normalWord = Convert(exceptionalNumber);
+            var pos = word.LastIndexOf(normalWord, StringComparison.CurrentCulture);
+            return word.Substring(0, pos) + ordinalWord;
+        }
 
-		private static bool ExceptionNumbersToWords(int number, out string words)
+        private static bool ExceptionNumbersToWords(int number, out string words)
         {
             return OrdinalExceptions.TryGetValue(number, out words);
         }
