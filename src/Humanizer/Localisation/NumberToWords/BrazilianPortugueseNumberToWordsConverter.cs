90908207d47ef0994ced1da1029578a85d87a2f1
Mahmoud Ali <muddibr@gmail.com>
2014-04-10 19:45:04 -0400

    Changed the NumberToWords Converter to find the right converter by the language Name aswell. This is needed for languages that can be ambiguous in Two Letter ISO Code (pt-BR, pt-PT both have 'pt' Two Letter ISO code).

---
+++ src/Humanizer/Localisation/NumberToWords/BrazilianPortugueseNumberToWordsConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Humanizer.Localisation.NumberToWords
+{
+    internal class BrazilianPortugueseNumberToWordsConverter : DefaultNumberToWordsConverter
+    {
+        public override string Convert(int number)
+        {
+            return "not implemented";
+        }
+    }
+}
