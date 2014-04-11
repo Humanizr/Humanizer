09f99554e8d223bffb0f0b523722a259b6e725bc
gyurisc <gyurisc@gmail.com>
2014-04-11 20:19:54 +0200

    fixing issues as requested in the pull request

--- src/Humanizer.Tests/Localisation/hu/DateHumanizeTests.cs
+++ src/Humanizer.Tests/Localisation/hu/DateHumanizeTests.cs
@@ -13,8 +13,6 @@ namespace Humanizer.Tests.Localisation.hu
         [Theory]
         [InlineData(1, "egy másodperce")]
         [InlineData(10, "10 másodperce")]
-        [InlineData(59, "59 másodperce")]
-        [InlineData(60, "egy perce")]
         public void SecondsAgo(int seconds, string expected)
         {
             DateHumanize.Verify(expected, seconds, TimeUnit.Second, Tense.Past);
@@ -23,8 +21,6 @@ namespace Humanizer.Tests.Localisation.hu
         [Theory]
         [InlineData(1, "egy másodperc múlva")]
         [InlineData(10, "10 másodperc múlva")]
-        [InlineData(59, "59 másodperc múlva")]
-        [InlineData(60, "egy perc múlva")]
         public void SecondsFromNow(int seconds, string expected)
         {
             DateHumanize.Verify(expected, seconds, TimeUnit.Second, Tense.Future);
@@ -33,9 +29,6 @@ namespace Humanizer.Tests.Localisation.hu
         [Theory]
         [InlineData(1, "egy perce")]
         [InlineData(10, "10 perce")]
-        [InlineData(44, "44 perce")]
-        [InlineData(45, "egy órája")]
-        [InlineData(120, "2 órája")]
         public void MinutesAgo(int minutes, string expected)
         {
             DateHumanize.Verify(expected, minutes, TimeUnit.Minute, Tense.Past);
@@ -44,10 +37,6 @@ namespace Humanizer.Tests.Localisation.hu
         [Theory]
         [InlineData(1, "egy perc múlva")]
         [InlineData(10, "10 perc múlva")]
-        [InlineData(44, "44 perc múlva")]
-        [InlineData(45, "egy óra múlva")]
-        [InlineData(119, "egy óra múlva")]
-        [InlineData(120, "2 óra múlva")]
         public void MinutesFromNow(int minutes, string expected)
         {
             DateHumanize.Verify(expected, minutes, TimeUnit.Minute, Tense.Future);
@@ -56,8 +45,6 @@ namespace Humanizer.Tests.Localisation.hu
         [Theory]
         [InlineData(1, "egy órája")]
         [InlineData(10, "10 órája")]
-        [InlineData(23, "23 órája")]
-        [InlineData(24, "tegnap")]
         public void HoursAgo(int hours, string expected)
         {
             DateHumanize.Verify(expected, hours, TimeUnit.Hour, Tense.Past);
@@ -66,8 +53,6 @@ namespace Humanizer.Tests.Localisation.hu
         [Theory]
         [InlineData(1, "egy óra múlva")]
         [InlineData(10, "10 óra múlva")]
-        [InlineData(23, "23 óra múlva")]
-        [InlineData(24, "holnap")]
         public void HoursFromNow(int hours, string expected)
         {
             DateHumanize.Verify(expected, hours, TimeUnit.Hour, Tense.Future);
@@ -76,8 +61,6 @@ namespace Humanizer.Tests.Localisation.hu
         [Theory]
         [InlineData(1, "tegnap")]
         [InlineData(10, "10 napja")]
-        [InlineData(28, "28 napja")]
-        [InlineData(32, "egy hónapja")]
         public void DaysAgo(int days, string expected)
         {
             DateHumanize.Verify(expected, days, TimeUnit.Day, Tense.Past);
@@ -86,8 +69,6 @@ namespace Humanizer.Tests.Localisation.hu
         [Theory]
         [InlineData(1, "holnap")]
         [InlineData(10, "10 nap múlva")]
-        [InlineData(28, "28 nap múlva")]
-        [InlineData(32, "egy hónap múlva")]
         public void DaysFromNow(int days, string expected)
         {
             DateHumanize.Verify(expected, days, TimeUnit.Day, Tense.Future);
@@ -96,8 +77,6 @@ namespace Humanizer.Tests.Localisation.hu
         [Theory]
         [InlineData(1, "egy hónapja")]
         [InlineData(10, "10 hónapja")]
-        [InlineData(11, "11 hónapja")]
-        [InlineData(12, "egy éve")]
         public void MonthsAgo(int months, string expected)
         {
             DateHumanize.Verify(expected, months, TimeUnit.Month, Tense.Past);
@@ -106,8 +85,6 @@ namespace Humanizer.Tests.Localisation.hu
         [Theory]
         [InlineData(1, "egy hónap múlva")]
         [InlineData(10, "10 hónap múlva")]
-        [InlineData(11, "11 hónap múlva")]
-        [InlineData(12, "egy év múlva")]
         public void MonthsFromNow(int months, string expected)
         {
             DateHumanize.Verify(expected, months, TimeUnit.Month, Tense.Future);
