3f34c18c8adef43add3074a3c96946851d8530fc
Dede Soeleman <soeleman@gmail.com>
2014-04-12 13:54:18 +0700

    Adding relevan test on Localization

--- src/Humanizer.Tests/Localisation/id/DateHumanizeTests.cs
+++ src/Humanizer.Tests/Localisation/id/DateHumanizeTests.cs
@@ -8,9 +8,9 @@ namespace Humanizer.Tests.Localisation.id
         public DateHumanizeTests() : base("id-ID") { }
 
         [Theory]
-        [InlineData(1, "sedetik yang lalu")]
-        [InlineData(59, "59 detik yang lalu")]
-        [InlineData(60, "semenit yang lalu")]
+        [InlineData(-1, "sedetik yang lalu")]
+        [InlineData(120, "2 menit yang lalu")]
+        [InlineData(90, "semenit yang lalu")]
         public void SecondsAgo(int seconds, string expected)
         {
             DateHumanize.Verify(expected, seconds, TimeUnit.Second, Tense.Past);
@@ -18,8 +18,8 @@ namespace Humanizer.Tests.Localisation.id
 
         [Theory]
         [InlineData(1, "sedetik dari sekarang")]
-        [InlineData(59, "59 detik dari sekarang")]
         [InlineData(60, "semenit dari sekarang")]
+        [InlineData(120, "2 menit dari sekarang")]
         public void SecondsFromNow(int seconds, string expected)
         {
             DateHumanize.Verify(expected, seconds, TimeUnit.Second, Tense.Future);
@@ -27,9 +27,9 @@ namespace Humanizer.Tests.Localisation.id
 
         [Theory]
         [InlineData(1, "semenit yang lalu")]
-        [InlineData(10, "10 menit yang lalu")]
+        [InlineData(15, "15 menit yang lalu")]
         [InlineData(45, "sejam yang lalu")]
-        [InlineData(120, "2 jam yang lalu")]
+        [InlineData(150, "2 jam yang lalu")]
         public void MinutesAgo(int minutes, string expected)
         {
             DateHumanize.Verify(expected, minutes, TimeUnit.Minute, Tense.Past);
@@ -37,9 +37,9 @@ namespace Humanizer.Tests.Localisation.id
 
         [Theory]
         [InlineData(1, "semenit dari sekarang")]
-        [InlineData(10, "10 menit dari sekarang")]
+        [InlineData(15, "15 menit dari sekarang")]
         [InlineData(45, "sejam dari sekarang")]
-        [InlineData(120, "2 jam dari sekarang")]
+        [InlineData(150, "2 jam dari sekarang")]
         public void MinutesFromNow(int minutes, string expected)
         {
             DateHumanize.Verify(expected, minutes, TimeUnit.Minute, Tense.Future);
@@ -49,6 +49,7 @@ namespace Humanizer.Tests.Localisation.id
         [InlineData(1, "sejam yang lalu")]
         [InlineData(10, "10 jam yang lalu")]
         [InlineData(24, "kemarin")]
+        [InlineData(48, "2 hari yang lalu")]
         public void HoursAgo(int hours, string expected)
         {
             DateHumanize.Verify(expected, hours, TimeUnit.Hour, Tense.Past);
@@ -58,6 +59,7 @@ namespace Humanizer.Tests.Localisation.id
         [InlineData(1, "sejam dari sekarang")]
         [InlineData(10, "10 jam dari sekarang")]
         [InlineData(24, "besok")]
+        [InlineData(48, "2 hari dari sekarang")]
         public void HoursFromNow(int hours, string expected)
         {
             DateHumanize.Verify(expected, hours, TimeUnit.Hour, Tense.Future);
@@ -65,8 +67,8 @@ namespace Humanizer.Tests.Localisation.id
 
         [Theory]
         [InlineData(1, "kemarin")]
-        [InlineData(10, "10 hari yang lalu")]
-        [InlineData(32, "sebulan yang lalu")]
+        [InlineData(15, "15 hari yang lalu")]
+        [InlineData(38, "sebulan yang lalu")]
         public void DaysAgo(int days, string expected)
         {
             DateHumanize.Verify(expected, days, TimeUnit.Day, Tense.Past);
@@ -76,6 +78,7 @@ namespace Humanizer.Tests.Localisation.id
         [InlineData(1, "besok")]
         [InlineData(10, "10 hari dari sekarang")]
         [InlineData(32, "sebulan dari sekarang")]
+        [InlineData(80, "2 bulan dari sekarang")]
         public void DaysFromNow(int days, string expected)
         {
             DateHumanize.Verify(expected, days, TimeUnit.Day, Tense.Future);
@@ -85,6 +88,7 @@ namespace Humanizer.Tests.Localisation.id
         [InlineData(1, "sebulan yang lalu")]
         [InlineData(10, "10 bulan yang lalu")]
         [InlineData(12, "setahun yang lalu")]
+        [InlineData(32, "2 tahun yang lalu")]
         public void MonthsAgo(int months, string expected)
         {
             DateHumanize.Verify(expected, months, TimeUnit.Month, Tense.Past);
@@ -92,24 +96,25 @@ namespace Humanizer.Tests.Localisation.id
 
         [Theory]
         [InlineData(1, "sebulan dari sekarang")]
-        [InlineData(10, "10 bulan dari sekarang")]
+        [InlineData(8, "8 bulan dari sekarang")]
         [InlineData(12, "setahun dari sekarang")]
+        [InlineData(26, "2 tahun dari sekarang")]
         public void MonthsFromNow(int months, string expected)
         {
             DateHumanize.Verify(expected, months, TimeUnit.Month, Tense.Future);
         }
 
         [Theory]
-        [InlineData(1, "setahun yang lalu")]
-        [InlineData(2, "2 tahun yang lalu")]
+        [InlineData(-1, "setahun yang lalu")]
+        [InlineData(-2, "2 tahun yang lalu")]
         public void YearsAgo(int years, string expected)
         {
             DateHumanize.Verify(expected, years, TimeUnit.Year, Tense.Past);
         }
 
         [Theory]
-        [InlineData(1, "setahun dari sekarang")]
-        [InlineData(2, "2 tahun dari sekarang")]
+        [InlineData(-1, "setahun dari sekarang")]
+        [InlineData(-5, "5 tahun dari sekarang")]
         public void YearsFromNow(int years, string expected)
         {
             DateHumanize.Verify(expected, years, TimeUnit.Year, Tense.Future);
--- src/Humanizer.Tests/Localisation/id/TimeSpanHumanizeTests.cs
+++ src/Humanizer.Tests/Localisation/id/TimeSpanHumanizeTests.cs
@@ -9,7 +9,10 @@ namespace Humanizer.Tests.Localisation.id
         public TimeSpanHumanizeTests() : base("id-ID") { }
 
         [Theory]
+        [InlineData(-7, "waktu kosong")]
         [InlineData(7, "1 minggu")]
+        [InlineData(21, "3 minggu")]
+        [InlineData(367, "52 minggu")]
         public void Weeks(int days, string expected)
         {
             var actual = TimeSpan.FromDays(days).Humanize();
@@ -17,7 +20,8 @@ namespace Humanizer.Tests.Localisation.id
         }
 
         [Theory]
-        [InlineData(1, "1 hari")]
+        [InlineData(3, "3 hari")]
+        [InlineData(8, "1 minggu")]
         public void Days(int days, string expected)
         {
             var actual = TimeSpan.FromDays(days).Humanize();
@@ -25,7 +29,9 @@ namespace Humanizer.Tests.Localisation.id
         }
 
         [Theory]
-        [InlineData(1, "1 jam")]
+        [InlineData(12, "12 jam")]
+        [InlineData(24, "1 hari")]
+        [InlineData(25, "1 hari")]
         public void Hours(int hours, string expected)
         {
             var actual = TimeSpan.FromHours(hours).Humanize();
@@ -34,6 +40,8 @@ namespace Humanizer.Tests.Localisation.id
 
         [Theory]
         [InlineData(1, "1 menit")]
+        [InlineData(60, "1 jam")]
+        [InlineData(120, "2 jam")]
         public void Minutes(int minutes, string expected)
         {
             var actual = TimeSpan.FromMinutes(minutes).Humanize();
@@ -41,8 +49,9 @@ namespace Humanizer.Tests.Localisation.id
         }
 
         [Theory]
-        [InlineData(60, "1 menit")]
         [InlineData(1, "1 detik")]
+        [InlineData(60, "1 menit")]
+        [InlineData(150, "2 menit")]
         public void Seconds(int seconds, string expected)
         {
             var actual = TimeSpan.FromSeconds(seconds).Humanize();
@@ -50,8 +59,9 @@ namespace Humanizer.Tests.Localisation.id
         }
 
         [Theory]
-        [InlineData(2500, "2 detik")]
         [InlineData(2, "2 milidetik")]
+        [InlineData(2500, "2 detik")]
+        [InlineData(65000, "1 menit")]
         public void Milliseconds(int ms, string expected)
         {
             var actual = TimeSpan.FromMilliseconds(ms).Humanize();
