ccabf355de058d2b6ee7d8258556c4aebacdcca5
Igal Tabachnik <hmemcpy@gmail.com>
2014-04-12 20:58:23 +0300

    ToWords Hebrew implementation

---
+++ src/Humanizer.Tests/Localisation/he/NumberToWordsTests.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+using Xunit.Extensions;
+
+namespace Humanizer.Tests.Localisation.he
+{
+    public class NumberToWordsTests : AmbientCulture
+    {
+        public NumberToWordsTests() : base("he") { }
+
+        [Theory]
+        [InlineData(0, "אפס")]
+        [InlineData(1, "אחת")]
+        [InlineData(2, "שתיים")]
+        [InlineData(3, "שלוש")]
+        [InlineData(4, "ארבע")]
+        [InlineData(5, "חמש")]
+        [InlineData(6, "שש")]
+        [InlineData(7, "שבע")]
+        [InlineData(8, "שמונה")]
+        [InlineData(9, "תשע")]
+        [InlineData(10, "עשר")]
+        [InlineData(11, "אחת עשרה")]
+        [InlineData(12, "שתים עשרה")]
+        [InlineData(19, "תשע עשרה")]
+        [InlineData(20, "עשרים")]
+        [InlineData(22, "עשרים ושתיים")]
+        [InlineData(50, "חמישים")]
+        [InlineData(99, "תשעים ותשע")]
+        [InlineData(100, "מאה")]
+        [InlineData(101, "מאה ואחת")]
+        [InlineData(111, "מאה ואחת עשרה")]
+        [InlineData(200, "מאתיים")]
+        [InlineData(241, "מאתיים ארבעים ואחת")]
+        [InlineData(500, "חמש מאות")]
+        [InlineData(505, "חמש מאות וחמש")]
+        [InlineData(725, "שבע מאות עשרים וחמש")]
+        //[InlineData(1000, "אלף")]
+        //[InlineData(1024, "אלף עשרים וארבע")]
+        //[InlineData(1040, "אלף ארבעים")]
+        //[InlineData(20000, "עשרים אלף")]
+        //[InlineData(500000, "חמש מאוד אלף")]
+        //[InlineData(500001, "חמש מאות אלף ואחת")]
+        //[InlineData(1000000, "מיליון")]
+        //[InlineData(1000001, "מיליון ואחת")]
+        //[InlineData(1000000000, "מיליארד")]
+        //[InlineData(1000000001, "מיליארד ואחת")]
+        //[InlineData(9876543210, "תשע מיליארד שמונה מאות שבעים ושש מיליון חמש מאות ארבעים ושלוש אלף מאתיים ועשר")]
+        public void ToWords(int number, string expected)
+        {
+            Assert.Equal(expected, number.ToWords());
+        }
+    }
+}
---
+++ src/Humanizer/Localisation/NumberToWords/HebrewNumberToWordsConverter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Humanizer.Localisation.NumberToWords
+{
+    internal class HebrewNumberToWordsConverter : DefaultNumberToWordsConverter
+    {
+        private static readonly string[] UnitsFeminine  = { "אחת", "שתיים", "שלוש", "ארבע", "חמש", "שש", "שבע", "שמונה", "תשע", "עשר" };
+        private static readonly string[] UnitsMasculine = { "אחד", "שניים", "שלושה", "ארבעה", "חמישה", "שישה", "שבעה", "שמונה", "תשעה", "עשרה" };
+        private static readonly string[] TensUnit       = { "עשר", "עשרים", "שלושים", "ארבעים", "חמישים", "שישים", "שבעים", "שמונים", "תשעים" };
+
+        public override string Convert(int number)
+        {
+            // in Hebrew, the default number gender form is feminine.
+            return Convert(number, GrammaticalGender.Feminine);
+        }
+
+        public override string Convert(int number, GrammaticalGender gender)
+        {
+            if (number < 0)
+                return string.Format("מינוס {0}", Convert(-number, gender));
+            
+            if (number == 0)
+                return "אפס";
+
+            if (number <= 10)
+                return gender == GrammaticalGender.Masculine ? UnitsMasculine[number - 1] : UnitsFeminine[number - 1];
+
+            if (number < 20)
+            {
+                string unit = Convert(number % 10, gender);
+                unit = unit.Replace("יי", "י");
+                return string.Format("{0} {1}", unit, gender == GrammaticalGender.Masculine ? "עשר" : "עשרה");
+            }
+            if (number < 100)
+            {
+                if (number % 10 == 0)
+                    return TensUnit[number / 10 - 1];
+
+                string unit = Convert(number % 10, gender);
+                return string.Format("{0} ו{1}", TensUnit[number / 10 - 1], unit);
+            }
+            if (number < 1000)
+            {
+                if (number == 100) return "מאה";
+                if (number == 200) return "מאתיים";
+
+                int hundredsDigit = number / 100;
+                if ((hundredsDigit * 100) == number)
+                    return UnitsFeminine[hundredsDigit - 1] + " מאות";
+
+                if (hundredsDigit == 1)
+                    return ToHundredsString("מאה", number, gender);
+                if (hundredsDigit == 2)
+                    return ToHundredsString("מאתיים", number, gender);
+
+                return ToHundredsString(UnitsFeminine[hundredsDigit - 1] + " מאות", number, gender);
+
+            }
+
+            return number.ToString();
+        }
+
+        private string ToHundredsString(string prefix, int number, GrammaticalGender gender)
+        {
+            int tens = number % 100;
+            return string.Format("{0} {1}", 
+                prefix, 
+                tens < 20
+                    ? "ו" + Convert(tens, gender)
+                    : Convert(tens, gender));
+        }
+    }
+}
