bf3a2ea93babb5567b2eb5b24262b24b7c0d6d99
Igal Tabachnik <hmemcpy@gmail.com>
2014-04-14 02:44:04 +0300

    FromNow implementation: Years, Months, Days, Hours, Minutes, Seconds

--- src/Humanizer.Tests/Localisation/he/DateHumanizeTests.cs
+++ src/Humanizer.Tests/Localisation/he/DateHumanizeTests.cs
@@ -66,5 +66,65 @@ namespace Humanizer.Tests.Localisation.he
         {
             DateHumanize.Verify(expected, years, TimeUnit.Year, Tense.Past);
         }
+
+        [Theory]
+        [InlineData(2, "בעוד יומיים")]
+        [InlineData(1, "מחר")]
+        [InlineData(3, "בעוד 3 ימים")]
+        [InlineData(11, "בעוד 11 יום")]
+        public void DaysFromNow(int days, string expected)
+        {
+            DateHumanize.Verify(expected, days, TimeUnit.Day, Tense.Future);
+        }
+
+        [Theory]
+        [InlineData(2, "בעוד חודשיים")]
+        [InlineData(1, "בעוד חודש")]
+        [InlineData(10, "בעוד 10 חודשים")]
+        [InlineData(11, "בעוד 11 חודשים")]
+        public void MonthsFromNow(int months, string expected)
+        {
+            DateHumanize.Verify(expected, months, TimeUnit.Month, Tense.Future);
+        }
+
+        [Theory]
+        [InlineData(2, "בעוד שנתיים")]
+        [InlineData(1, "בעוד שנה")]
+        [InlineData(3, "בעוד 3 שנים")]
+        [InlineData(11, "בעוד 11 שנה")]
+        public void YearsFromNow(int years, string expected)
+        {
+            DateHumanize.Verify(expected, years, TimeUnit.Year, Tense.Future);
+        }
+
+        [Theory]
+        [InlineData(2, "בעוד שעתיים")]
+        [InlineData(1, "בעוד שעה")]
+        [InlineData(3, "בעוד 3 שעות")]
+        [InlineData(11, "בעוד 11 שעות")]
+        public void HoursFromNow(int hours, string expected)
+        {
+            DateHumanize.Verify(expected, hours, TimeUnit.Hour, Tense.Future);
+        }
+
+        [Theory]
+        [InlineData(2, "בעוד 2 דקות")]
+        [InlineData(1, "בעוד דקה")]
+        [InlineData(3, "בעוד 3 דקות")]
+        [InlineData(11, "בעוד 11 דקות")]
+        public void MinutesFromNow(int minutes, string expected)
+        {
+            DateHumanize.Verify(expected, minutes, TimeUnit.Minute, Tense.Future);
+        }
+
+        [Theory]
+        [InlineData(2, "בעוד 2 שניות")]
+        [InlineData(1, "בעוד שנייה")]
+        [InlineData(3, "בעוד 3 שניות")]
+        [InlineData(11, "בעוד 11 שניות")]
+        public void SecondsFromNow(int seconds, string expected)
+        {
+            DateHumanize.Verify(expected, seconds, TimeUnit.Second, Tense.Future);
+        }
     }
 }
