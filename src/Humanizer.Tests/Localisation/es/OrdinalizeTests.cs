dd9bc7abd22e22207a5e33cd948629620392cc47
Thomas Hunsaker <thomas.hunsaker@gmail.com>
2014-04-17 11:32:43 -0700

    Updated Ordinalize format

--- src/Humanizer.Tests/Localisation/es/OrdinalizeTests.cs
+++ src/Humanizer.Tests/Localisation/es/OrdinalizeTests.cs
@@ -12,18 +12,18 @@ namespace Humanizer.Tests.Localisation.es
 
         [Theory]
         [InlineData("0", "0")]
-        [InlineData("1", "1er")]
+        [InlineData("1", "1.º")]
         [InlineData("2", "2.º")]
-        [InlineData("3", "3er")]
+        [InlineData("3", "3.º")]
         [InlineData("4", "4.º")]
         [InlineData("5", "5.º")]
         [InlineData("6", "6.º")]
-        [InlineData("23", "23er")]
+        [InlineData("23", "23.º")]
         [InlineData("100", "100.º")]
-        [InlineData("101", "101er")]
+        [InlineData("101", "101.º")]
         [InlineData("102", "102.º")]
-        [InlineData("103", "103er")]
-        [InlineData("1001", "1001er")]
+        [InlineData("103", "103.º")]
+        [InlineData("1001", "1001.º")]
         public void OrdinalizeString(string number, string ordinalized)
         {
             Assert.Equal(number.Ordinalize(GrammaticalGender.Masculine), ordinalized);
@@ -50,19 +50,19 @@ namespace Humanizer.Tests.Localisation.es
 
         [Theory]
         [InlineData(0, "0")]
-        [InlineData(1, "1er")]
+        [InlineData(1, "1.º")]
         [InlineData(2, "2.º")]
-        [InlineData(3, "3er")]
+        [InlineData(3, "3.º")]
         [InlineData(4, "4.º")]
         [InlineData(5, "5.º")]
         [InlineData(6, "6.º")]
         [InlineData(10, "10.º")]
-        [InlineData(23, "23er")]
+        [InlineData(23, "23.º")]
         [InlineData(100, "100.º")]
-        [InlineData(101, "101er")]
+        [InlineData(101, "101.º")]
         [InlineData(102, "102.º")]
-        [InlineData(103, "103er")]
-        [InlineData(1001, "1001er")]
+        [InlineData(103, "103.º")]
+        [InlineData(1001, "1001.º")]
         public void OrdinalizeNumber(int number, string ordinalized)
         {
             Assert.Equal(number.Ordinalize(GrammaticalGender.Masculine), ordinalized);
