54ca41f3eae93cc33e2d5d909b7f7ac0d17c6674
Ali Hmer <alitohami@gmail.com>
2014-04-18 11:20:11 -0600

    Added more tests for new Arabic resource settings

--- src/Humanizer.Tests/Localisation/ar/DateHumanizeTests.cs
+++ src/Humanizer.Tests/Localisation/ar/DateHumanizeTests.cs
@@ -1,4 +1,4 @@
-﻿using Humanizer.Localisation;
+using Humanizer.Localisation;
 using Xunit.Extensions;
 
 namespace Humanizer.Tests.Localisation.ar
@@ -19,6 +19,16 @@ namespace Humanizer.Tests.Localisation.ar
         }
 
         [Theory]
+        [InlineData(1, "في غضون يوم واحد من الآن")]
+        [InlineData(2, "في غضون يومين من الآن")]
+        [InlineData(10, "في غضون 10 أيام من الآن")]
+        [InlineData(17, "في غضون 17 يوم من الآن")]
+        public void DaysFromNow(int days, string expected)
+        {
+            DateHumanize.Verify(expected, days, TimeUnit.Day, Tense.Future);
+        }
+
+        [Theory]
         [InlineData(-2, "منذ ساعتين")]
         [InlineData(-1, "منذ ساعة واحدة")]
         [InlineData(-3, "منذ 3 ساعات")]
@@ -26,6 +36,16 @@ namespace Humanizer.Tests.Localisation.ar
         public void HoursAgo(int hours, string expected)
         {
             DateHumanize.Verify(expected, hours, TimeUnit.Hour, Tense.Past);
+        }
+
+        [Theory]
+        [InlineData(1, "في غضون ساعة واحدة من الآن")]
+        [InlineData(2, "في غضون ساعتين من الآن")]
+        [InlineData(10, "في غضون 10 ساعات من الآن")]
+        [InlineData(23, "في غضون 23 ساعة من الآن")]
+        public void HoursFromNow(int hours, string expected)
+        {
+            DateHumanize.Verify(expected, hours, TimeUnit.Hour, Tense.Future);
         }
 
         [Theory]
@@ -39,6 +59,16 @@ namespace Humanizer.Tests.Localisation.ar
         }
 
         [Theory]
+        [InlineData(1, "في غضون دقيقة واحدة من الآن")]
+        [InlineData(2, "في غضون دقيقتين من الآن")]
+        [InlineData(10, "في غضون 10 دقائق من الآن")]
+        [InlineData(23, "في غضون 23 دقيقة من الآن")]
+        public void MinutesFromNow(int minutes, string expected)
+        {
+            DateHumanize.Verify(expected, minutes, TimeUnit.Minute, Tense.Future);
+        }
+
+        [Theory]
         [InlineData(-2, "منذ شهرين")]
         [InlineData(-1, "منذ شهر واحد")]
         [InlineData(-3, "منذ 3 أشهر")]
@@ -46,6 +76,15 @@ namespace Humanizer.Tests.Localisation.ar
         public void MonthsAgo(int months, string expected)
         {
             DateHumanize.Verify(expected, months, TimeUnit.Month, Tense.Past);
+        }
+
+        [Theory]
+        [InlineData(1, "في غضون شهر واحد من الآن")]
+        [InlineData(2, "في غضون شهرين من الآن")]
+        [InlineData(10, "في غضون 10 أشهر من الآن")]
+        public void MonthsFromNow(int months, string expected)
+        {
+            DateHumanize.Verify(expected, months, TimeUnit.Month, Tense.Future);
         }
 
         [Theory]
@@ -59,6 +98,16 @@ namespace Humanizer.Tests.Localisation.ar
         }
 
         [Theory]
+        [InlineData(1, "في غضون ثانية واحدة من الآن")]
+        [InlineData(2, "في غضون ثانيتين من الآن")]
+        [InlineData(10, "في غضون 10 ثوان من الآن")]
+        [InlineData(24, "في غضون 24 ثانية من الآن")]
+        public void SecondsFromNow(int seconds, string expected)
+        {
+            DateHumanize.Verify(expected, seconds, TimeUnit.Second, Tense.Future);
+        }
+
+        [Theory]
         [InlineData(-2, "منذ عامين")]
         [InlineData(-1, "العام السابق")]
         [InlineData(-3, "منذ 3 أعوام")]
@@ -66,6 +115,16 @@ namespace Humanizer.Tests.Localisation.ar
         public void YearsAgo(int years, string expected)
         {
             DateHumanize.Verify(expected, years, TimeUnit.Year, Tense.Past);
+        }
+
+        [Theory]
+        [InlineData(1, "في غضون سنة واحدة من الآن")]
+        [InlineData(2, "في غضون سنتين من الآن")]
+        [InlineData(7, "في غضون 7 سنوات من الآن")]
+        [InlineData(55, "في غضون 55 سنة من الآن")]
+        public void YearsFromNow(int years, string expected)
+        {
+            DateHumanize.Verify(expected, years, TimeUnit.Year, Tense.Future);
         }
     }
 }
