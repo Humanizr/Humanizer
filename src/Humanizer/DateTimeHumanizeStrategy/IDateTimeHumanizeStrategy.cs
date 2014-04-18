6554e67de4e459c441f1cc5eb7e35e40d7751fe7
Adam Chester <adamchester@gmail.com>
2014-04-18 15:07:55 +0930

    Documented IDateTimeHumanizeStrategy

--- src/Humanizer/DateTimeHumanizeStrategy/IDateTimeHumanizeStrategy.cs
+++ src/Humanizer/DateTimeHumanizeStrategy/IDateTimeHumanizeStrategy.cs
@@ -2,8 +2,14 @@
 
 namespace Humanizer.DateTimeHumanizeStrategy
 {
+    /// <summary>
+    /// A strategy for converting a 'distance of time' into words.
+    /// </summary>
     public interface IDateTimeHumanizeStrategy
     {
+        /// <summary>
+        /// Calculates the distance of time in words between two provided dates.
+        /// </summary>
         string Humanize(DateTime input, DateTime comparisonBase);
     }
 }
