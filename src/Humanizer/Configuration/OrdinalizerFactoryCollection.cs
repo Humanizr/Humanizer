58c4db140007d64d51369e3f1df14ac8c2a1e00c
Justin Edwards <jedwards@gigmasters.com>
2014-04-19 18:49:05 -0400

    Moved factory collections to their own classes, allowed public access via Configurator, and made the default factories lazy loaded

---
+++ src/Humanizer/Configuration/OrdinalizerFactoryCollection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Humanizer.Localisation.Ordinalizers;
+
+namespace Humanizer.Configuration
+{
+    public class OrdinalizerFactoryCollection : FactoryCollection<IOrdinalizer>
+    {
+        private static Lazy<EnglishOrdinalizer> _lazyEnglishOrdinalizer = new Lazy<EnglishOrdinalizer>();
+        private static Lazy<SpanishOrdinalizer> _lazySpanishOrdinalizer = new Lazy<SpanishOrdinalizer>();
+        private static Lazy<BrazilianPortugueseOrdinalizer> _lazyBrazilianPortugueseOrdinalizer = new Lazy<BrazilianPortugueseOrdinalizer>();
+        private static Lazy<RussianOrdinalizer> _lazyRussianOrdinalizer = new Lazy<RussianOrdinalizer>();
+        private static Lazy<DefaultOrdinalizer> _lazyDefaultOrdinalizer = new Lazy<DefaultOrdinalizer>();
+
+        public OrdinalizerFactoryCollection() : base(
+            new Dictionary<string, Func<IOrdinalizer>>(StringComparer.OrdinalIgnoreCase)
+            {
+                {"en", () => _lazyEnglishOrdinalizer.Value},
+                {"es", () => _lazySpanishOrdinalizer.Value},
+                {"pt-BR", () => _lazyBrazilianPortugueseOrdinalizer.Value},
+                {"ru", () => _lazyRussianOrdinalizer.Value},
+                {"default", () => _lazyDefaultOrdinalizer.Value}
+            }) { }
+    }
+}
