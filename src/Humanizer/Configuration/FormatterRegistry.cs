cffdcdd6bf0c48eeb09127fd990f81384c3a981a
MehdiK <me@mehdi-khalili.com>
2014-04-25 16:35:20 +0430

    simplified the registration mechanism & removed Localiser

--- src/Humanizer/Configuration/FormatterRegistry.cs
+++ src/Humanizer/Configuration/FormatterRegistry.cs
@@ -4,17 +4,17 @@ namespace Humanizer.Configuration
 {
     internal class FormatterRegistry : LocaliserRegistry<IFormatter>
     {
-        public FormatterRegistry() : base(
-            () => new DefaultFormatter(),
-            new Localiser<IFormatter>("ro", () => new RomanianFormatter()),
-            new Localiser<IFormatter>("ru", () => new RussianFormatter()),
-            new Localiser<IFormatter>("ar", () => new ArabicFormatter()),
-            new Localiser<IFormatter>("he", () => new HebrewFormatter()),
-            new Localiser<IFormatter>("sk", () => new CzechSlovakPolishFormatter()),
-            new Localiser<IFormatter>("cs", () => new CzechSlovakPolishFormatter()),
-            new Localiser<IFormatter>("pl", () => new CzechSlovakPolishFormatter()),
-            new Localiser<IFormatter>("sr", () => new SerbianFormatter()))
+        public FormatterRegistry()
         {
+            RegisterDefault<DefaultFormatter>();
+            Register<RomanianFormatter>("ro");
+            Register<RussianFormatter>("ru");
+            Register<ArabicFormatter>("ar");
+            Register<HebrewFormatter>("he");
+            Register<CzechSlovakPolishFormatter>("sk");
+            Register<CzechSlovakPolishFormatter>("cs");
+            Register<CzechSlovakPolishFormatter>("pl");
+            Register<SerbianFormatter>("sr");
         }
     }
 }
--- src/Humanizer/Configuration/NumberToWordsConverterRegistry.cs
+++ src/Humanizer/Configuration/NumberToWordsConverterRegistry.cs
@@ -5,18 +5,18 @@ namespace Humanizer.Configuration
     internal class NumberToWordsConverterRegistry : LocaliserRegistry<INumberToWordsConverter>
     {
         public NumberToWordsConverterRegistry()
-            : base(() => new DefaultNumberToWordsConverter(),
-                new Localiser<INumberToWordsConverter>("en", () => new EnglishNumberToWordsConverter()),
-                new Localiser<INumberToWordsConverter>("ar", () => new ArabicNumberToWordsConverter()),
-                new Localiser<INumberToWordsConverter>("fa", () => new FarsiNumberToWordsConverter()),
-                new Localiser<INumberToWordsConverter>("es", () => new SpanishNumberToWordsConverter()),
-                new Localiser<INumberToWordsConverter>("pl", () => new PolishNumberToWordsConverter()),
-                new Localiser<INumberToWordsConverter>("pt-BR", () => new BrazilianPortugueseNumberToWordsConverter()),
-                new Localiser<INumberToWordsConverter>("ru", () => new RussianNumberToWordsConverter()),
-                new Localiser<INumberToWordsConverter>("fr", () => new FrenchNumberToWordsConverter()),
-                new Localiser<INumberToWordsConverter>("nl", () => new DutchNumberToWordsConverter()),
-                new Localiser<INumberToWordsConverter>("he", () => new HebrewNumberToWordsConverter()))
         {
+            RegisterDefault<DefaultNumberToWordsConverter>();
+            Register<EnglishNumberToWordsConverter>("en");
+            Register<ArabicNumberToWordsConverter>("ar");
+            Register<FarsiNumberToWordsConverter>("fa");
+            Register<SpanishNumberToWordsConverter>("es");
+            Register<PolishNumberToWordsConverter>("pl");
+            Register<BrazilianPortugueseNumberToWordsConverter>("pt-BR");
+            Register<RussianNumberToWordsConverter>("ru");
+            Register<FrenchNumberToWordsConverter>("fr");
+            Register<DutchNumberToWordsConverter>("nl");
+            Register<HebrewNumberToWordsConverter>("he");
         }
     }
 }
