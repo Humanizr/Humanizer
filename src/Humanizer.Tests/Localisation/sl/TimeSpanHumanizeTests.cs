094ca5bb77ee4cee89dfd9049af52ca67a93b9c2
Mitja Bezenšek <mitja.bezensek@istudio.si>
2014-04-25 17:32:16 +0200

    Add additional formatting rules for numbers three and four.

--- src/Humanizer.Tests/Localisation/sl/TimeSpanHumanizeTests.cs
+++ src/Humanizer.Tests/Localisation/sl/TimeSpanHumanizeTests.cs
@@ -11,8 +11,8 @@ namespace Humanizer.Tests.Localisation.sl
         [Theory]
         [InlineData(7, "1 teden")]
         [InlineData(14, "2 tedna")]
-        [InlineData(21, "3 tednov")]
-        [InlineData(28, "4 tednov")]
+        [InlineData(21, "3 tedne")]
+        [InlineData(28, "4 tedne")]
         [InlineData(35, "5 tednov")]
         [InlineData(77, "11 tednov")]
         public void Weeks(int days, string expected)
@@ -22,8 +22,10 @@ namespace Humanizer.Tests.Localisation.sl
 
         [Theory]
         [InlineData(1, "1 dan")]
-        [InlineData(2, "2 dni")]
+        [InlineData(2, "2 dneva")]
         [InlineData(3, "3 dni")]
+        [InlineData(4, "4 dni")]
+        [InlineData(5, "5 dni")]
         public void Days(int days, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromDays(days).Humanize());
@@ -32,8 +34,8 @@ namespace Humanizer.Tests.Localisation.sl
         [Theory]
         [InlineData(1, "1 ura")]
         [InlineData(2, "2 uri")]
-        [InlineData(3, "3 ur")]
-        [InlineData(4, "4 ur")]
+        [InlineData(3, "3 ure")]
+        [InlineData(4, "4 ure")]
         [InlineData(5, "5 ur")]
         public void Hours(int hours, string expected)
         {
@@ -43,8 +45,8 @@ namespace Humanizer.Tests.Localisation.sl
         [Theory]
         [InlineData(1, "1 minuta")]
         [InlineData(2, "2 minuti")]
-        [InlineData(3, "3 minut")]
-        [InlineData(4, "4 minut")]
+        [InlineData(3, "3 minute")]
+        [InlineData(4, "4 minute")]
         [InlineData(5, "5 minut")]
         public void Minutes(int minutes, string expected)
         {
@@ -55,8 +57,8 @@ namespace Humanizer.Tests.Localisation.sl
         [Theory]
         [InlineData(1, "1 sekunda")]
         [InlineData(2, "2 sekundi")]
-        [InlineData(3, "3 sekund")]
-        [InlineData(4, "4 sekund")]
+        [InlineData(3, "3 sekunde")]
+        [InlineData(4, "4 sekunde")]
         [InlineData(5, "5 sekund")]
         public void Seconds(int seconds, string expected)
         {
@@ -66,8 +68,8 @@ namespace Humanizer.Tests.Localisation.sl
         [Theory]
         [InlineData(1, "1 milisekunda")]
         [InlineData(2, "2 milisekundi")]
-        [InlineData(3, "3 milisekund")]
-        [InlineData(4, "4 milisekund")]
+        [InlineData(3, "3 milisekunde")]
+        [InlineData(4, "4 milisekunde")]
         [InlineData(5, "5 milisekund")]
         public void Milliseconds(int milliseconds, string expected)
         {
@@ -77,7 +79,7 @@ namespace Humanizer.Tests.Localisation.sl
         [Fact]
         public void NoTime()
         {
-            Assert.Equal("ni časa", TimeSpan.Zero.Humanize());
+            Assert.Equal("nič časa", TimeSpan.Zero.Humanize());
         }
     }
 }
