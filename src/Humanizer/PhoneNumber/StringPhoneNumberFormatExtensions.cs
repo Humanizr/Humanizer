532c2fa8e917b6b4ec47626ab5aca103dd259b1d
Philipp Voronin <fvoronin@gmail.com>
2014-05-09 12:43:51 +0600

    Added Phone Number Formatter
    for Russian mobile phone numbers to begin with.

---
+++ src/Humanizer/PhoneNumber/StringPhoneNumberFormatExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Humanizer.PhoneNumber
+{
+    /// <summary>
+    /// Extension methods for String type.
+    /// </summary>
+    public static class StringPhoneNumberFormatExtensions
+    {
+        private static readonly Lazy<PhoneNumberFormatter> Formatter = new Lazy<PhoneNumberFormatter>(() => new PhoneNumberFormatter());
+
+        /// <summary>
+        /// Extension method to format phone number
+        /// </summary>
+        /// <param name="phoneNumber">Phone number</param>
+        /// <returns>Formatted phone number</returns>
+        public static string FormatPhone(this string phoneNumber)
+        {
+            return Formatter.Value.Format(phoneNumber);
+        }
+    }
+}
