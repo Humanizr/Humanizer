323073bcf0fd46ccc07c3b1d551db4f3231488ef
rkttu <rkttu@rkttu.com>
2014-05-15 01:38:20 +0900

    Initial version of Korean localisation added.

---
+++ src/Humanizer.Tests/Localisation/ko-KR/DateHumanizeTests.cs
@@ -0,0 +1,113 @@
+﻿using Humanizer.Localisation;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Humanizer.Tests.Localisation.ko
+{
+    public class DateHumanizeTests : AmbientCulture
+    {
+        public DateHumanizeTests() : base("ko-KR") { }
+
+        [Theory]
+        [InlineData(1, "1초 전")]
+        [InlineData(2, "2초 전")]
+        public void SecondsAgo(int seconds, string expected) 
+        {
+            DateHumanize.Verify(expected, seconds, TimeUnit.Second, Tense.Past);
+        }
+
+        [Theory]
+        [InlineData(1, "1초 후")]
+        [InlineData(2, "2초 후")]
+        public void SecondsFromNow(int seconds, string expected)
+        {
+            DateHumanize.Verify(expected, seconds, TimeUnit.Second, Tense.Future);
+        }
+
+        [Theory]
+        [InlineData(1, "1분 전")]
+        [InlineData(2, "2분 전")]
+        public void MinutesAgo(int minutes, string expected)
+        {
+            DateHumanize.Verify(expected, minutes, TimeUnit.Minute, Tense.Past);
+        }
+
+        [Theory]
+        [InlineData(1, "1분 후")]
+        [InlineData(2, "2분 후")]
+        public void MinutesFromNow(int minutes, string expected)
+        {
+            DateHumanize.Verify(expected, minutes, TimeUnit.Minute, Tense.Future);
+        }
+
+        [Theory]
+        [InlineData(1, "1시간 전")]
+        [InlineData(2, "2시간 전")]
+        public void HoursAgo(int hours, string expected)
+        {
+            DateHumanize.Verify(expected, hours, TimeUnit.Hour, Tense.Past);
+        }
+
+        [Theory]
+        [InlineData(1, "1시간 후")]
+        [InlineData(2, "2시간 후")]
+        public void HoursFromNow(int hours, string expected)
+        {
+            DateHumanize.Verify(expected, hours, TimeUnit.Hour, Tense.Future);
+        }
+
+        [Theory]
+        [InlineData(1, "어제")]
+        [InlineData(2, "2일 전")]
+        public void DaysAgo(int days, string expected)
+        {
+            DateHumanize.Verify(expected, days, TimeUnit.Day, Tense.Past);
+        }
+
+        [Theory]
+        [InlineData(1, "내일")]
+        [InlineData(2, "2일 후")]
+        public void DaysFromNow(int days, string expected)
+        {
+            DateHumanize.Verify(expected, days, TimeUnit.Day, Tense.Future);
+        }
+
+        [Theory]
+        [InlineData(1, "1개월 전")]
+        [InlineData(2, "2개월 전")]
+        public void MonthsAgo(int months, string expected)
+        {
+            DateHumanize.Verify(expected, months, TimeUnit.Month, Tense.Past);
+        }
+
+        [Theory]
+        [InlineData(1, "1개월 후")]
+        [InlineData(2, "2개월 후")]
+        public void MonthsFromNow(int months, string expected)
+        {
+            DateHumanize.Verify(expected, months, TimeUnit.Month, Tense.Future);
+        }
+
+        [Theory]
+        [InlineData(1, "1년 전")]
+        [InlineData(2, "2년 전")]
+        public void YearsAgo(int years, string expected)
+        {
+            DateHumanize.Verify(expected, years, TimeUnit.Year, Tense.Past);
+        }
+
+        [Theory]
+        [InlineData(1, "1년 후")]
+        [InlineData(2, "2년 후")]
+        public void YearsFromNow(int years, string expected)
+        {
+            DateHumanize.Verify(expected, years, TimeUnit.Year, Tense.Future);
+        }
+
+        [Fact]
+        public void Now()
+        {
+            DateHumanize.Verify("지금", 0, TimeUnit.Day, Tense.Past);
+        }
+    }
+}
---
+++ src/Humanizer.Tests/Localisation/ko-KR/TimeSpanHumanizeTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Humanizer.Tests.Localisation.ko
+{
+    public class TimeSpanHumanizeTests : AmbientCulture
+    {
+        public TimeSpanHumanizeTests() : base("ko-KR") { }
+
+        [Theory]
+        [InlineData(7, "1주")]
+        [InlineData(14, "2주")]
+        public void Weeks(int days, string expected)
+        {
+            Assert.Equal(expected, TimeSpan.FromDays(days).Humanize());
+        }
+
+        [Theory]
+        [InlineData(1, "1일")]
+        [InlineData(2, "2일")]
+        public void Days(int days, string expected)
+        {
+            Assert.Equal(expected, TimeSpan.FromDays(days).Humanize());
+        }
+
+        [Theory]
+        [InlineData(1, "1시간")]
+        [InlineData(2, "2시간")]
+        public void Hours(int hours, string expected)
+        {
+            Assert.Equal(expected, TimeSpan.FromHours(hours).Humanize());
+        }
+
+        [Theory]
+        [InlineData(1, "1분")]
+        [InlineData(2, "2분")]
+        public void Minutes(int minutes, string expected)
+        {
+            Assert.Equal(expected, TimeSpan.FromMinutes(minutes).Humanize());
+        }
+
+        [Theory]
+        [InlineData(1, "1초")]
+        [InlineData(2, "2초")]
+        public void Seconds(int seconds, string expected)
+        {
+            Assert.Equal(expected, TimeSpan.FromSeconds(seconds).Humanize());
+        }
+
+        [Theory]
+        [InlineData(1, "1밀리 초")]
+        [InlineData(2, "2밀리 초")]
+        public void Milliseconds(int milliseconds, string expected)
+        {
+            Assert.Equal(expected, TimeSpan.FromMilliseconds(milliseconds).Humanize());
+        }
+
+        [Fact]
+        public void NoTime()
+        {
+            Assert.Equal("0초", TimeSpan.Zero.Humanize());
+        }
+    }
+}
