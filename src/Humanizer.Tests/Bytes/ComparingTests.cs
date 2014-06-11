d4594fa17302e0dbf019dca7903b61b822a57813
Max Malook <community@malook.de>
2014-06-11 23:33:10 +0200

    provide untyped comparison for ByteSize

---
+++ src/Humanizer.Tests/Bytes/ComparingTests.cs
@@ -0,0 +1,35 @@
+﻿using Humanizer.Bytes;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Humanizer.Tests.Bytes
+{
+    public class ComparingTests
+    {
+        [Theory]
+        [InlineData(13, 23, -1)]
+        [InlineData(23, 23, 0)]
+        [InlineData(45, 23, 1)]
+        public void CompareStrongTyped(double value, double valueToCompareWith, int expectedResult)
+        {
+            var valueSize = new ByteSize(value);
+            var otherSize = new ByteSize(valueToCompareWith);
+            var result = valueSize.CompareTo(otherSize);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(13, 23, -1)]
+        [InlineData(23, 23, 0)]
+        [InlineData(45, 23, 1)]
+        public void CompareUntyped(double value, double valueToCompareWith, int expectedResult)
+        {
+            var valueSize = new ByteSize(value);
+            object otherSize = new ByteSize(valueToCompareWith);
+            var result = valueSize.CompareTo(otherSize);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
