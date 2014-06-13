10857099d7425239253f80ca3892e5689c78fe1e
Shuhel Ahmed <shuhel@ymail.com>
2014-06-13 16:13:35 +0600

    translated TimeSpanHumanize with tests

--- src/Humanizer.Tests/Localisation/bn-BD/TimeSpanHumanizeTests.cs
+++ src/Humanizer.Tests/Localisation/bn-BD/TimeSpanHumanizeTests.cs
@@ -64,51 +64,50 @@ namespace Humanizer.Tests.Localisation.bnBD
         }
     }
 }
-        public TimeSpanHumanizeTests() : base("fa") { }
 
         [Theory]
-        [InlineData(7, "یک هفته")]
-        [InlineData(77, "11 هفته")]
+        [InlineData(7, "এক সপ্তাহ")]
+        [InlineData(14, "2 সপ্তাহ")]
         public void Weeks(int days, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromDays(days).Humanize());
         }
 
         [Theory]
-        [InlineData(1, "یک روز")]
-        [InlineData(3, "3 روز")]
+        [InlineData(1, "এক দিন")]
+        [InlineData(2, "2 দিন")]
         public void Days(int days, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromDays(days).Humanize());
         }
 
         [Theory]
-        [InlineData(1, "یک ساعت")]
-        [InlineData(11, "11 ساعت")]
+        [InlineData(1, "এক ঘণ্টা")]
+        [InlineData(2, "2 ঘণ্টা")]
         public void Hours(int hours, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromHours(hours).Humanize());
         }
 
         [Theory]
-        [InlineData(1, "یک دقیقه")]
-        [InlineData(11, "11 دقیقه")]
+        [InlineData(1, "এক মিনিট")]
+        [InlineData(2, "2 মিনিট")]
         public void Minutes(int minutes, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromMinutes(minutes).Humanize());
         }
 
         [Theory]
-        [InlineData(1, "یک ثانیه")]
-        [InlineData(11, "11 ثانیه")]
+        [InlineData(1, "এক সেকেন্ড")]
+        [InlineData(2, "2 সেকেন্ড")]
         public void Seconds(int seconds, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromSeconds(seconds).Humanize());
         }
 
         [Theory]
-        [InlineData(1, "یک میلی ثانیه")]
-        [InlineData(11, "11 میلی ثانیه")]
+        [InlineData(1, "এক মিলিসেকেন্ড")]
+        [InlineData(2, "2 মিলিসেকেন্ড")]
         public void Milliseconds(int milliseconds, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromMilliseconds(milliseconds).Humanize());
@@ -117,7 +116,8 @@ namespace Humanizer.Tests.Localisation.bnBD
         [Fact]
         public void NoTime()
         {
-            Assert.Equal("الآن", TimeSpan.Zero.Humanize());
+            // This one really doesn't make a lot of sense but again... w/e
+            Assert.Equal("শুন্য সময়", TimeSpan.Zero.Humanize());
         }
     }
 }
