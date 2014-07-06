ec1b9318743471549e90a17d71706ea00f1ffb49
George Hahn <george@genericmaker.com>
2014-07-05 20:19:24 -0400

    Added byte rate calculation functons, closes #296

---
+++ src/Humanizer.Tests/Bytes/ByteRateTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Humanizer.Bytes;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Humanizer.Tests.Bytes
+{
+    public class ByteRateTests : AmbientCulture
+    {
+        public ByteRateTests() : base("en") { }
+        
+        [Theory]
+        [InlineData(400, 1, "400 B/s")]
+        [InlineData(4 * 1024, 1, "4 KB/s")]
+        [InlineData(4 * 1024 * 1024, 1, "4 MB/s")]
+        [InlineData(4 * 2 * 1024 * 1024, 2, "4 MB/s")]
+        [InlineData(4 * 1024, 0.1, "40 KB/s")]
+        [InlineData(15 * 60 * 1024 * 1024, 60, "15 MB/s")]
+        public void HumanizesRates(long inputBytes, double perSeconds, string expectedValue)
+        {
+            var size = new ByteSize(inputBytes);
+            var interval = TimeSpan.FromSeconds(perSeconds);
+
+            var rate = size.Per(interval).ToRatePerSecond();
+
+            Assert.Equal(expectedValue, rate);
+        }
+    }
+}
---
+++ src/Humanizer/Bytes/ByteRate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Humanizer.Bytes
+{
+    public class ByteRate
+    {
+        public ByteSize size { get; private set;}
+
+        public TimeSpan interval { get; private set; }
+
+        public ByteRate(ByteSize size, TimeSpan interval)
+        {
+            this.size = size;
+            this.interval = interval;
+        }
+
+        /// <summary>
+        /// Return this rate as a string, EG "2 MB/s"
+        /// </summary>
+        /// <returns></returns>
+        public string ToRatePerSecond()
+        {
+            return (new ByteSize(size.Bytes / interval.TotalSeconds)).Humanize() + "/s";
+        }
+    }
+}
