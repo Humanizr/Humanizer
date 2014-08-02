974fafca4e1525281a2a50cdc35342b9a39385d3
brz <borzoo.esmailloo@gmail.com>
2014-08-03 00:15:47 +0430

    Added more meaningful exception to DefaultFormatter.TimespanHumanize

--- release_notes.md
+++ release_notes.md
@@ -1,5 +1,6 @@
 ###In Development
   - [#320](https://github.com/MehdiK/Humanizer/pull/320): Fixed Dehumanize actually humanizing an already dehumanized string
+  - [#322](https://github.com/MehdiK/Humanizer/pull/322): DefaultFormatter.TimeSpanHumanize throws a more meaningful exception when called with TimeUnits larger than TimeUnit.Week
 
 [Commits](https://github.com/MehdiK/Humanizer/compare/v1.28.0...master)
 
---
+++ src/Humanizer.Tests/Localisation/DefaultFormatterTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Humanizer.Localisation;
+using Humanizer.Localisation.Formatters;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Humanizer.Tests.Localisation
+{
+    public class DefaultFormatterTests
+    {
+        [Theory]
+        [InlineData(TimeUnit.Month, 1)]
+        [InlineData(TimeUnit.Month, 2)]
+        [InlineData(TimeUnit.Month, 10)]
+        [InlineData(TimeUnit.Year, 1)]
+        [InlineData(TimeUnit.Year, 2)]
+        [InlineData(TimeUnit.Year, 10)]
+        public void TimeSpanHumanizeThrowsExceptionForTimeUnitsLargerThanWeek(TimeUnit timeUnit, int unit)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new DefaultFormatter(CultureInfo.InvariantCulture.Name).TimeSpanHumanize(timeUnit, unit));
+        }
+    }
+}
