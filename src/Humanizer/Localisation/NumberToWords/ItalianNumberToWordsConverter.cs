d113e6b0f42cc03933df4a847e76651e821ccfbc
Giuseppe Piscopo <piscopo.giuseppe@gmail.com>
2014-08-26 15:39:42 +0200

    L10n: Refactor: removed inner class in ItalianNumberToWordsConverter

--- src/Humanizer/Localisation/NumberToWords/ItalianNumberToWordsConverter.cs
+++ src/Humanizer/Localisation/NumberToWords/ItalianNumberToWordsConverter.cs
@@ -28,19 +28,17 @@ namespace Humanizer.Localisation.NumberToWords
             public ItalianNumberCruncher(int number)
             {
                 _number = number;
+                _threeDigitChops = ChopEveryThreeDigits(_number);
+                _nextSet = ThreeDigitSets.Units;
             }
             
             public string Convert()
             {
                 string word = String.Empty;
                 
-                List<int> threeDigitChops = ChopEveryThreeDigits(_number);
-                
-                ThreeDigitSequenceConverter sequenceConverter = new ThreeDigitSequenceConverter();
-                
-                foreach(int chop in threeDigitChops)
+                foreach(int chop in _threeDigitChops)
                 {
-                    Func<int, string> chopToString = sequenceConverter.GetNext();
+                    Func<int, string> chopToString = GetNextChopConverter();
                     
                     word = chopToString(chop) + word;
                 }
@@ -49,6 +47,9 @@ namespace Humanizer.Localisation.NumberToWords
             }
             
             protected readonly int _number;
+            protected readonly List<int> _threeDigitChops;
+            
+            protected ThreeDigitSets _nextSet;
             
             protected static List<int> ChopEveryThreeDigits(int number)
             {
@@ -67,154 +68,144 @@ namespace Humanizer.Localisation.NumberToWords
                 return chops;
             }
             
-            class ThreeDigitSequenceConverter
+            public Func<int, string> GetNextChopConverter()
             {
-                public ThreeDigitSequenceConverter()
-                {
-                    _nextSet = ThreeDigitSets.Units;
-                }
+                Func<int, string> converter;
                 
-                public Func<int, string> GetNext()
+                switch (_nextSet)
                 {
-                    Func<int, string> converter;
-                    
-                    switch (_nextSet)
-                    {
-                        case ThreeDigitSets.Units:
-                            converter = UnitsConverter;
-                            _nextSet = ThreeDigitSets.Thousands;
-                            break;
-                            
-                        case ThreeDigitSets.Thousands:
-                            converter = ThousandsConverter;
-                            _nextSet = ThreeDigitSets.Millions;
-                            break;
-                            
-                        case ThreeDigitSets.Millions:
-                            converter = null;
-                            break;
-                            
-                        case ThreeDigitSets.Billions:
-                            converter = null;
-                            break;
-                            
-                        case ThreeDigitSets.More:
-                            converter = null;
-                            break;
-                            
-                        default:
-                            throw new ArgumentOutOfRangeException("Unknow ThreeDigitSet: " + _nextSet);
-                    }
-                    
-                    return converter;
-                }
-                
-                protected static string UnitsConverter(int number)
-                {
-                    int tensAndUnits = number % 100;
-                    int hundreds = (int)(number / 100);
-                    
-                    int units = tensAndUnits % 10;
-                    int tens = (int)(tensAndUnits / 10);
-                    
-                    string words = String.Empty;
-                    
-                    words += _numberToHundred[hundreds];
-                    
-                    words += _numberToTen[tens];
-                    
-                    if (tensAndUnits <= 9)
-                    {
-                        words += _numberToUnit[tensAndUnits];
-                    }
-                    else if (tensAndUnits <= 19)
-                    {
-                        words += _numberToTeens[tensAndUnits - 10];
-                    }
-                    else
-                    {
-                        if (units == 1 || units == 8)
-                        {
-                            words = words.Remove(words.Length - 1);
-                        }
+                    case ThreeDigitSets.Units:
+                        converter = UnitsConverter;
+                        _nextSet = ThreeDigitSets.Thousands;
+                        break;
                         
-                        words += _numberToUnit[units];
+                    case ThreeDigitSets.Thousands:
+                        converter = ThousandsConverter;
+                        _nextSet = ThreeDigitSets.Millions;
+                        break;
+                        
+                    case ThreeDigitSets.Millions:
+                        converter = null;
+                        break;
+                        
+                    case ThreeDigitSets.Billions:
+                        converter = null;
+                        break;
+                        
+                    case ThreeDigitSets.More:
+                        converter = null;
+                        break;
+                        
+                    default:
+                        throw new ArgumentOutOfRangeException("Unknow ThreeDigitSet: " + _nextSet);
+                }
+                
+                return converter;
+            }
+            
+            protected static string UnitsConverter(int number)
+            {
+                int tensAndUnits = number % 100;
+                int hundreds = (int)(number / 100);
+                
+                int units = tensAndUnits % 10;
+                int tens = (int)(tensAndUnits / 10);
+                
+                string words = String.Empty;
+                
+                words += _numberToHundred[hundreds];
+                
+                words += _numberToTen[tens];
+                
+                if (tensAndUnits <= 9)
+                {
+                    words += _numberToUnit[tensAndUnits];
+                }
+                else if (tensAndUnits <= 19)
+                {
+                    words += _numberToTeens[tensAndUnits - 10];
+                }
+                else
+                {
+                    if (units == 1 || units == 8)
+                    {
+                        words = words.Remove(words.Length - 1);
                     }
                     
-                    return words;
+                    words += _numberToUnit[units];
                 }
                 
-                protected static string ThousandsConverter(int number)
-                {
-                    return UnitsConverter(number) + "mila";
-                }
-                
-                protected ThreeDigitSets _nextSet;
-                
-                protected static string[] _numberToUnit = new string[]
-                {
-                    String.Empty,
-                    "uno",
-                    "due",
-                    "tre",
-                    "quattro",
-                    "cinque",
-                    "sei",
-                    "sette",
-                    "otto",
-                    "nove"
-                };
-                
-                protected static string[] _numberToTen = new string[]
-                {
-                    String.Empty,
-                    String.Empty,
-                    "venti",
-                    "trenta",
-                    "quaranta",
-                    "cinquanta",
-                    "sessanta",
-                    "settanta",
-                    "ottanta",
-                    "novanta"
-                };
-                
-                protected static string[] _numberToTeens = new string[]
-                {
-                    "dieci",
-                    "undici",
-                    "dodici",
-                    "tredici",
-                    "quattordici",
-                    "quindici",
-                    "sedici",
-                    "diciassette",
-                    "diciotto",
-                    "diciannove"
-                };
-                
-                protected static string[] _numberToHundred = new string[]
-                {
-                    String.Empty,
-                    "cento",
-                    "duecento",
-                    "trecento",
-                    "quattrocento",
-                    "cinquecento",
-                    "seicento",
-                    "settecento",
-                    "ottocento",
-                    "novecento"
-                };
-                
-                protected enum ThreeDigitSets
-                {
-                    Units,
-                    Thousands,
-                    Millions,
-                    Billions,
-                    More
-                }
+                return words;
+            }
+            
+            protected static string ThousandsConverter(int number)
+            {
+                return UnitsConverter(number) + "mila";
+            }
+            
+            protected static string[] _numberToUnit = new string[]
+            {
+                String.Empty,
+                "uno",
+                "due",
+                "tre",
+                "quattro",
+                "cinque",
+                "sei",
+                "sette",
+                "otto",
+                "nove"
+            };
+            
+            protected static string[] _numberToTen = new string[]
+            {
+                String.Empty,
+                String.Empty,
+                "venti",
+                "trenta",
+                "quaranta",
+                "cinquanta",
+                "sessanta",
+                "settanta",
+                "ottanta",
+                "novanta"
+            };
+            
+            protected static string[] _numberToTeens = new string[]
+            {
+                "dieci",
+                "undici",
+                "dodici",
+                "tredici",
+                "quattordici",
+                "quindici",
+                "sedici",
+                "diciassette",
+                "diciotto",
+                "diciannove"
+            };
+            
+            protected static string[] _numberToHundred = new string[]
+            {
+                String.Empty,
+                "cento",
+                "duecento",
+                "trecento",
+                "quattrocento",
+                "cinquecento",
+                "seicento",
+                "settecento",
+                "ottocento",
+                "novecento"
+            };
+            
+            protected enum ThreeDigitSets
+            {
+                Units,
+                Thousands,
+                Millions,
+                Billions,
+                More
             }
         }
     }
