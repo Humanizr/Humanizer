0c9ff0adf3bc9747d0f40ceac402f186a2b9ee84
Max Malook <community@malook.de>
2014-11-04 23:04:10 +0100

    introduce optional considerEmptyParts parameter to TimeSpan Humanize method

--- src/Humanizer/TimeSpanHumanizeExtensions.cs
+++ src/Humanizer/TimeSpanHumanizeExtensions.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Text;
 using Humanizer.Configuration;
 using Humanizer.Localisation;
 using Humanizer.Localisation.Formatters;
@@ -20,10 +19,17 @@ namespace Humanizer
         /// <param name="timeSpan"></param>
         /// <param name="precision">The maximum number of time units to return. Defaulted is 1 which means the largest unit is returned</param>
         /// <param name="culture">Culture to use. If null, current thread's UI culture is used.</param>
+        /// <param name="considerEmptyParts">Controls whether empty time units should be considered when calculating the number of time units to return.</param>
         /// <returns></returns>
-        public static string Humanize(this TimeSpan timeSpan, int precision = 1, CultureInfo culture = null)
+        public static string Humanize(this TimeSpan timeSpan, int precision = 1, CultureInfo culture = null, bool considerEmptyParts = false)
         {
-            return string.Join(", ", GetTimeParts(timeSpan, culture).Take(precision).Where(x => x != null));
+            var timeParts = GetTimeParts(timeSpan, culture);
+            if (!considerEmptyParts)
+                timeParts = timeParts.Where(x => x != null);
+            timeParts = timeParts.Take(precision);
+            if (considerEmptyParts)
+                timeParts = timeParts.Where(x => x != null);
+            return string.Join(", ", timeParts);
         }
 
         private static IEnumerable<string> GetTimeParts(TimeSpan timespan, CultureInfo culture)
@@ -64,62 +70,6 @@ namespace Humanizer
             return unit != 0
                 ? formatter.TimeSpanHumanize(timeUnit, unit)
                 : null;
-        }
-
-        private static string GetTimePart(TimeSpan timespan, CultureInfo culture)
-        {
-            var formatter = Configurator.GetFormatter(culture);
-            if (timespan.Days >= 7)
-                return formatter.TimeSpanHumanize(TimeUnit.Week, timespan.Days/7);
-
-            if (timespan.Days >= 1)
-                return formatter.TimeSpanHumanize(TimeUnit.Day, timespan.Days);
-
-            if (timespan.Hours >= 1)
-                return formatter.TimeSpanHumanize(TimeUnit.Hour, timespan.Hours);
-
-            if (timespan.Minutes >= 1)
-                return formatter.TimeSpanHumanize(TimeUnit.Minute, timespan.Minutes);
-
-            if (timespan.Seconds >= 1)
-                return formatter.TimeSpanHumanize(TimeUnit.Second, timespan.Seconds);
-
-            if (timespan.Milliseconds >= 1)
-                return formatter.TimeSpanHumanize(TimeUnit.Millisecond, timespan.Milliseconds);
-
-            return formatter.TimeSpanHumanize_Zero();
-        }
-
-        static TimeSpan TakeOutTheLargestUnit(TimeSpan timeSpan)
-        {
-            return timeSpan - LargestUnit(timeSpan);
-        }
-
-        static TimeSpan LargestUnit(TimeSpan timeSpan)
-        {
-            var days = timeSpan.Days;
-            if (days >= 7)
-                return TimeSpan.FromDays((days/7) * 7);
-            if (days >= 1)
-                return TimeSpan.FromDays(days);
-
-            var hours = timeSpan.Hours;
-            if (hours >= 1)
-                return TimeSpan.FromHours(hours);
-
-            var minutes = timeSpan.Minutes;
-            if (minutes >= 1)
-                return TimeSpan.FromMinutes(minutes);
-
-            var seconds = timeSpan.Seconds;
-            if (seconds >= 1)
-                return TimeSpan.FromSeconds(seconds);
-
-            var milliseconds = timeSpan.Milliseconds;
-            if (milliseconds >= 1)
-                return TimeSpan.FromMilliseconds(milliseconds);
-
-            return TimeSpan.Zero;
         }
     }
 }
