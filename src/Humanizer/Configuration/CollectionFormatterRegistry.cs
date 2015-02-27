059e3fd32d353ac2c4bc8f66be59ab19d7a3451b
Thomas Mentzel <thomas.mentzel@schoenhofer.de>
2015-02-27 09:44:56 +0100

    release notes extended, Collection formatter support for Danish, Dutch, Portuguese and Norwegian

--- src/Humanizer/Configuration/CollectionFormatterRegistry.cs
+++ src/Humanizer/Configuration/CollectionFormatterRegistry.cs
@@ -10,6 +10,11 @@ namespace Humanizer.Configuration
             Register("en", new OxfordStyleCollectionFormatter("and"));
             Register("it", new RegularStyleCollectionFormatter("e"));
             Register("de", new RegularStyleCollectionFormatter("und"));
+            Register("dk", new RegularStyleCollectionFormatter("og"));
+            Register("nl", new RegularStyleCollectionFormatter("en"));
+            Register("pt", new RegularStyleCollectionFormatter("e"));
+            Register("nn", new RegularStyleCollectionFormatter("og"));
+            Register("nb", new RegularStyleCollectionFormatter("og"));
         }
     }
 }
