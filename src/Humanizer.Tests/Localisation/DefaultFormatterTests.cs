4e96736146832b377f247320c0c887c13884a7c5
Thomas Mentzel <thomas.mentzel@softwarekueche.de>
2015-03-10 19:47:49 +0100

    test for issue 362 .. which shows the fix works (for es, which is not implemented)

--- src/Humanizer.Tests/Localisation/DefaultFormatterTests.cs
+++ src/Humanizer.Tests/Localisation/DefaultFormatterTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Threading;
 using Humanizer.Localisation;
 using Humanizer.Localisation.Formatters;
 using Xunit;
@@ -19,6 +20,26 @@ namespace Humanizer.Tests.Localisation
         public void TimeSpanHumanizeThrowsExceptionForTimeUnitsLargerThanWeek(TimeUnit timeUnit, int unit)
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => new DefaultFormatter(CultureInfo.InvariantCulture.Name).TimeSpanHumanize(timeUnit, unit));
+        }
+
+        [Fact]
+        public void Issue_392_A_collection_formatter_for_the_current_culture_has_not_been_implemented_yet()
+        {
+            var originalCulture = Thread.CurrentThread.CurrentCulture;
+            var originalUiCulture = Thread.CurrentThread.CurrentUICulture;
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("es");
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture("es");
+
+            // start: code from issue report
+            var a = new[] { DateTime.UtcNow, DateTime.UtcNow.AddDays(10) };
+            var b = a.Humanize(); // THROWS!
+            // end: code from issue report
+
+            Assert.Equal(a[0] + " & " + a[1], b);
+
+            Thread.CurrentThread.CurrentCulture = originalCulture;
+            Thread.CurrentThread.CurrentUICulture = originalUiCulture;
         }
     }
 }
