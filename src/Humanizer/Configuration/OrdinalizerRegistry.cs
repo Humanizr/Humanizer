8e9a6ec4f736081300cbaf981748e0637dc2624c
Damiaan <damiaan@comm-it.be>
2015-05-24 13:49:15 +0200

    Add Dutch Ordinalizer

--- src/Humanizer/Configuration/OrdinalizerRegistry.cs
+++ src/Humanizer/Configuration/OrdinalizerRegistry.cs
@@ -10,6 +10,7 @@ namespace Humanizer.Configuration
             Register("en", new EnglishOrdinalizer());
             Register("es", new SpanishOrdinalizer());
             Register("it", new ItalianOrdinalizer());
+            Register("nl", new DutchOrdinalizer());
             Register("pt-BR", new BrazilianPortugueseOrdinalizer());
             Register("ru", new RussianOrdinalizer());
             Register("tr", new TurkishOrdinalizer());
