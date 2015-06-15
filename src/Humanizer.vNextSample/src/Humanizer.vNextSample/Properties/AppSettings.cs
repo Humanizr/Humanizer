94b9edf19428433103d97e74b68154d98efbe1c2
pandah <hrusikesh.panda@dowjones.com>
2015-06-15 15:08:00 -0400

    Initial cut

---
+++ src/Humanizer.vNextSample/src/Humanizer.vNextSample/Properties/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Humanizer.vNextSample
+{
+    public class AppSettings
+    {
+        public string SiteTitle { get; set; }
+    }
+}
