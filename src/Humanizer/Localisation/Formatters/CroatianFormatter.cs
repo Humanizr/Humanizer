a204909f36c18bfed2d9b399a5b2f55e9fe41dda
Matea Pejčinović SPAN d.o.o. <Matea.Pejcinovic@span.eu>
2015-07-09 08:51:18 +0200

    DateTime for Croatian

---
+++ src/Humanizer/Localisation/Formatters/CroatianFormatter.cs
@@ -0,0 +1,20 @@
+namespace Humanizer.Localisation.Formatters
+{
+    internal class CroatianFormatter : DefaultFormatter
+    {
+        private const string DualTrialQuadralPostfix = "_DualTrialQuadral";
+
+        public CroatianFormatter()
+            : base("hr")
+        {
+        }
+
+        protected override string GetResourceKey(string resourceKey, int number)
+        {
+            if ((number % 10 == 2 || number % 10 == 3 || number % 10 == 4) && number != 12 && number != 13 && number != 14)
+                return resourceKey + DualTrialQuadralPostfix;
+
+            return resourceKey;
+        }
+    }
+}
