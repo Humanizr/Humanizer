c047a97de908acdc39b7aec3ea772e88173327f0
Cristian Stoica <cristianstoica85@gmail.com>
2015-08-27 03:12:14 +0300

    Added Romanian Number to Words Converter, Romanian Ordinalizer and test cases.
    Fixed Romanian localization string 'roRo' -> 'roRO'.
    Fixed documentation (few typos and mistakes).

---
+++ src/Humanizer/Localisation/NumberToWords/RomanianNumberToWordsConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Humanizer.Localisation.NumberToWords.Romanian;
+
+namespace Humanizer.Localisation.NumberToWords
+{
+    internal class RomanianNumberToWordsConverter : GenderedNumberToWordsConverter
+    {
+        public override string Convert(int number, GrammaticalGender gender)
+        {
+            RomanianCardinalNumberConverter converter = new RomanianCardinalNumberConverter();
+            return converter.Convert(number, gender);
+        }
+
+        public override string ConvertToOrdinal(int number, GrammaticalGender gender)
+        {
+            RomanianOrdinalNumberConverter converter = new RomanianOrdinalNumberConverter();
+            return converter.Convert(number, gender);
+        }
+    }
+}
