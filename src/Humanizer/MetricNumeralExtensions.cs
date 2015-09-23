5cd60c0d871db19f9b8548d0cf540d992ab3d4f7
aloisdg <aloisdegouvello@live.fr>
2015-09-23 23:29:08 +0530

    Docs: Add missing doc for Replace

--- src/Humanizer/MetricNumeralExtensions.cs
+++ src/Humanizer/MetricNumeralExtensions.cs
@@ -27,182 +27,187 @@ using System.Linq;
 
 namespace Humanizer
 {
-        /// <summary>
-        /// Contains extension methods for changing a number to Metric representation (ToMetric)
-        /// and from Metric representation back to the number (FromMetric)
-        /// </summary>
-        public static class MetricNumeralExtensions
-        {
-                /// <summary>
-                /// Symbols is a list of every symbols for the Metric system.
-                /// </summary>
-                private static readonly char[][] Symbols =
+	/// <summary>
+	/// Contains extension methods for changing a number to Metric representation (ToMetric)
+	/// and from Metric representation back to the number (FromMetric)
+	/// </summary>
+	public static class MetricNumeralExtensions
+	{
+		/// <summary>
+		/// Symbols is a list of every symbols for the Metric system.
+		/// </summary>
+		private static readonly char[][] Symbols =
                 {
                         new[] { 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y' },
                         new[] { 'm', '\u03bc', 'n', 'p', 'f', 'a', 'z', 'y' }
                 };
 
-                /// <summary>
-                /// Names link a Metric symbol (as key) to its name (as value).
-                /// </summary>
-                /// <remarks>
-                /// We dont support :
-                /// {'h', "hecto"},
-                /// {'da', "deca" }, // !string
-                /// {'d', "deci" },
-                /// {'c', "centi"},
-                /// </remarks>
-                private static readonly Dictionary<char, string> Names = new Dictionary<char, string>()
+		/// <summary>
+		/// Names link a Metric symbol (as key) to its name (as value).
+		/// </summary>
+		/// <remarks>
+		/// We dont support :
+		/// {'h', "hecto"},
+		/// {'da', "deca" }, // !string
+		/// {'d', "deci" },
+		/// {'c', "centi"},
+		/// </remarks>
+		private static readonly Dictionary<char, string> Names = new Dictionary<char, string>()
                 {
                          {'Y', "yotta" }, {'Z', "zetta" }, {'E', "exa" }, {'P', "peta" }, {'T', "tera" }, {'G', "giga" }, {'M', "mega" }, {'k', "kilo" },
                          {'m', "milli" }, {'μ', "micro" }, {'n', "nano" }, {'p', "pico" }, {'f', "femto" }, {'a', "atto" }, {'z', "zepto" }, {'y', "yocto" }
                 };
 
-                /// <summary>
-                /// Converts a Metric representation into a number.
-                /// </summary>
-                /// <remarks>
-                /// We don't support input in the format {number}{name} nor {number} {name}.
-                /// We only provide a solution for {number}{symbol} and {number} {symbol}.
-                /// </remarks>
-                /// <param name="input">Metric representation to convert to a number</param>
-                /// <example>
-                /// "1k".FromMetric() => 1000d
-                /// "123".FromMetric() => 123d
-                /// "100m".FromMetric() => 1E-1
-                /// </example>
-                /// <returns>A number after a conversion from a Metric representation.</returns>
-                public static double FromMetric(this string input)
-                {
-                        if (input == null) throw new ArgumentNullException("input");
-                        input = input.Trim();
-                        input = ReplaceNameBySymbol(input);
-                        if (input.Length == 0 || input.IsInvalidMetricNumeral())
-                                throw new ArgumentException("Empty or invalid Metric string.", "input");
-                        input = input.Replace(" ", String.Empty);
-                        var last = input[input.Length - 1];
-                        if (!Char.IsLetter(last)) return Double.Parse(input);
-                        Func<char[], double> getExponent = symbols => (symbols.IndexOf(last) + 1) * 3;
-                        var number = Double.Parse(input.Remove(input.Length - 1));
-                        var exponent = Math.Pow(10, Symbols[0].Contains(last) ? getExponent(Symbols[0]) : -getExponent(Symbols[1]));
-                        return number * exponent;
-                }
+		/// <summary>
+		/// Converts a Metric representation into a number.
+		/// </summary>
+		/// <remarks>
+		/// We don't support input in the format {number}{name} nor {number} {name}.
+		/// We only provide a solution for {number}{symbol} and {number} {symbol}.
+		/// </remarks>
+		/// <param name="input">Metric representation to convert to a number</param>
+		/// <example>
+		/// "1k".FromMetric() => 1000d
+		/// "123".FromMetric() => 123d
+		/// "100m".FromMetric() => 1E-1
+		/// </example>
+		/// <returns>A number after a conversion from a Metric representation.</returns>
+		public static double FromMetric(this string input)
+		{
+			if (input == null) throw new ArgumentNullException("input");
+			input = input.Trim();
+			input = ReplaceNameBySymbol(input);
+			if (input.Length == 0 || input.IsInvalidMetricNumeral())
+				throw new ArgumentException("Empty or invalid Metric string.", "input");
+			input = input.Replace(" ", String.Empty);
+			var last = input[input.Length - 1];
+			if (!Char.IsLetter(last)) return Double.Parse(input);
+			Func<char[], double> getExponent = symbols => (symbols.IndexOf(last) + 1) * 3;
+			var number = Double.Parse(input.Remove(input.Length - 1));
+			var exponent = Math.Pow(10, Symbols[0].Contains(last) ? getExponent(Symbols[0]) : -getExponent(Symbols[1]));
+			return number * exponent;
+		}
 
-                private static string ReplaceNameBySymbol(string input)
-                {
-                        return Names.Aggregate(input, (current, name) => current.Replace(name.Value, name.Key.ToString()));
-                }
+		/// <summary>
+		/// Replace every symbol's name by its symbol representation.
+		/// </summary>
+		/// <param name="input">Metric representation with a name or a symbol</param>
+		/// <returns>A metric representation with a symbol</returns>
+		private static string ReplaceNameBySymbol(string input)
+		{
+			return Names.Aggregate(input, (current, name) => current.Replace(name.Value, name.Key.ToString()));
+		}
 
-                /// <summary>
-                /// Converts a number into a valid and Human-readable Metric representation.
-                /// </summary>
-                /// <remarks>
-                /// Inspired by a snippet from Thom Smith.
-                /// <see cref="http://stackoverflow.com/questions/12181024/formatting-a-number-with-a-metric-prefix"/>
-                /// </remarks>
-                /// <param name="input">Number to convert to a Metric representation.</param>
-                /// <param name="isSplitedBySpace">True will split the number and the symbol with a whitespace.</param>
-                /// <param name="useSymbol">True will use symbol instead of name</param>
-                /// <example>
-                /// 1000d.ToMetric() => "1k"
-                /// 123d.ToMetric() => "123"
-                /// 1E-1.ToMetric() => "100m"
-                /// </example>
-                /// <returns>A valid Metric representation</returns>
-                public static string ToMetric(this double input, bool isSplitedBySpace = false, bool useSymbol = true)
-                {
-                        if (input.Equals(0)) return input.ToString();
-                        if (input.IsOutOfRange()) throw new ArgumentOutOfRangeException("input");
-                        var exponent = (int)Math.Floor(Math.Log10(Math.Abs(input)) / 3);
-                        if (exponent == 0) return input.ToString();
-	                var number = input*Math.Pow(1000, -exponent);
-	                var symbol = Math.Sign(exponent) == 1 ? Symbols[0][exponent - 1] : Symbols[1][-exponent - 1];
-                        return number
-                                + (isSplitedBySpace ? " " : String.Empty)
-                                + GetUnit(symbol, useSymbol);
-                }
+		/// <summary>
+		/// Converts a number into a valid and Human-readable Metric representation.
+		/// </summary>
+		/// <remarks>
+		/// Inspired by a snippet from Thom Smith.
+		/// <see cref="http://stackoverflow.com/questions/12181024/formatting-a-number-with-a-metric-prefix"/>
+		/// </remarks>
+		/// <param name="input">Number to convert to a Metric representation.</param>
+		/// <param name="isSplitedBySpace">True will split the number and the symbol with a whitespace.</param>
+		/// <param name="useSymbol">True will use symbol instead of name</param>
+		/// <example>
+		/// 1000d.ToMetric() => "1k"
+		/// 123d.ToMetric() => "123"
+		/// 1E-1.ToMetric() => "100m"
+		/// </example>
+		/// <returns>A valid Metric representation</returns>
+		public static string ToMetric(this double input, bool isSplitedBySpace = false, bool useSymbol = true)
+		{
+			if (input.Equals(0)) return input.ToString();
+			if (input.IsOutOfRange()) throw new ArgumentOutOfRangeException("input");
+			var exponent = (int)Math.Floor(Math.Log10(Math.Abs(input)) / 3);
+			if (exponent == 0) return input.ToString();
+			var number = input * Math.Pow(1000, -exponent);
+			var symbol = Math.Sign(exponent) == 1 ? Symbols[0][exponent - 1] : Symbols[1][-exponent - 1];
+			return number
+				+ (isSplitedBySpace ? " " : String.Empty)
+				+ GetUnit(symbol, useSymbol);
+		}
 
-                /// <summary>
-                /// Converts a number into a valid and Human-readable Metric representation.
-                /// </summary>
-                /// <remarks>
-                /// Inspired by a snippet from Thom Smith.
-                /// <see cref="http://stackoverflow.com/questions/12181024/formatting-a-number-with-a-metric-prefix"/>
-                /// </remarks>
-                /// <param name="input">Number to convert to a Metric representation.</param>
-                /// <param name="isSplitedBySpace">True will split the number and the symbol with a whitespace.</param>
-                /// <param name="useSymbol">True will use symbol instead of name</param>
-                /// <example>
-                /// 1000.ToMetric() => "1k"
-                /// 123.ToMetric() => "123"
-                /// 1E-1.ToMetric() => "100m"
-                /// </example>
-                /// <returns>A valid Metric representation</returns>
-                public static string ToMetric(this int input, bool isSplitedBySpace = false, bool useSymbol = true)
-                {
-                        return Convert.ToDouble(input).ToMetric(isSplitedBySpace, useSymbol);
-                }
+		/// <summary>
+		/// Converts a number into a valid and Human-readable Metric representation.
+		/// </summary>
+		/// <remarks>
+		/// Inspired by a snippet from Thom Smith.
+		/// <see cref="http://stackoverflow.com/questions/12181024/formatting-a-number-with-a-metric-prefix"/>
+		/// </remarks>
+		/// <param name="input">Number to convert to a Metric representation.</param>
+		/// <param name="isSplitedBySpace">True will split the number and the symbol with a whitespace.</param>
+		/// <param name="useSymbol">True will use symbol instead of name</param>
+		/// <example>
+		/// 1000.ToMetric() => "1k"
+		/// 123.ToMetric() => "123"
+		/// 1E-1.ToMetric() => "100m"
+		/// </example>
+		/// <returns>A valid Metric representation</returns>
+		public static string ToMetric(this int input, bool isSplitedBySpace = false, bool useSymbol = true)
+		{
+			return Convert.ToDouble(input).ToMetric(isSplitedBySpace, useSymbol);
+		}
 
-                /// <summary>
-                /// Get the unit from a symbol of from the symbol's name.
-                /// </summary>
-                /// <param name="symbol">The symbol linked to the unit</param>
-                /// <param name="useSymbol">True will use symbol instead of name</param>
-                /// <returns>A symbol or a symbol's name</returns>
-                private static string GetUnit(char symbol, bool useSymbol)
-                {
-                        return useSymbol ? symbol.ToString() : Names[symbol];
-                }
+		/// <summary>
+		/// Get the unit from a symbol of from the symbol's name.
+		/// </summary>
+		/// <param name="symbol">The symbol linked to the unit</param>
+		/// <param name="useSymbol">True will use symbol instead of name</param>
+		/// <returns>A symbol or a symbol's name</returns>
+		private static string GetUnit(char symbol, bool useSymbol)
+		{
+			return useSymbol ? symbol.ToString() : Names[symbol];
+		}
 
-                /// <summary>
-                /// Check if a Metric representation is out of the valid range.
-                /// </summary>
-                /// <param name="input">A Metric representation who might be out of the valid range.</param>
-                /// <returns>True if input is out of the valid range.</returns>
-                private static bool IsOutOfRange(this double input)
-                {
-                        const int limit = 27;
-                        var bigLimit = Math.Pow(10, limit);
-                        var smallLimit = Math.Pow(10, -limit);
-                        Func<double, double, bool> outside = (min, max) => !(max > input && input > min);
-                        return (Math.Sign(input) == 1 && outside(smallLimit, bigLimit))
-                               || (Math.Sign(input) == -1 && outside(-bigLimit, -smallLimit));
-                }
+		/// <summary>
+		/// Check if a Metric representation is out of the valid range.
+		/// </summary>
+		/// <param name="input">A Metric representation who might be out of the valid range.</param>
+		/// <returns>True if input is out of the valid range.</returns>
+		private static bool IsOutOfRange(this double input)
+		{
+			const int limit = 27;
+			var bigLimit = Math.Pow(10, limit);
+			var smallLimit = Math.Pow(10, -limit);
+			Func<double, double, bool> outside = (min, max) => !(max > input && input > min);
+			return (Math.Sign(input) == 1 && outside(smallLimit, bigLimit))
+			       || (Math.Sign(input) == -1 && outside(-bigLimit, -smallLimit));
+		}
 
-                /// <summary>
-                /// Check if a string is not a valid Metric representation.
-                /// A valid representation is in the format "{0}{1}" or "{0} {1}"
-                /// where {0} is a number and {1} is an allowed symbol.
-                /// </summary>
-                /// <remarks>
-                /// ToDo: Performance: Use (string input, out number) to escape the double use of Parse()
-                /// </remarks>
-                /// <param name="input">A string who might contain a invalid Metric representation.</param>
-                /// <returns>True if input is not a valid Metric representation.</returns>
-                private static bool IsInvalidMetricNumeral(this string input)
-                {
-                        double number;
-                        var index = input.Length - 1;
-                        var last = input[index];
-                        var isSymbol = Symbols[0].Contains(last) || Symbols[1].Contains(last);
-                        return !Double.TryParse(isSymbol ? input.Remove(index) : input, out number);
-                }
+		/// <summary>
+		/// Check if a string is not a valid Metric representation.
+		/// A valid representation is in the format "{0}{1}" or "{0} {1}"
+		/// where {0} is a number and {1} is an allowed symbol.
+		/// </summary>
+		/// <remarks>
+		/// ToDo: Performance: Use (string input, out number) to escape the double use of Parse()
+		/// </remarks>
+		/// <param name="input">A string who might contain a invalid Metric representation.</param>
+		/// <returns>True if input is not a valid Metric representation.</returns>
+		private static bool IsInvalidMetricNumeral(this string input)
+		{
+			double number;
+			var index = input.Length - 1;
+			var last = input[index];
+			var isSymbol = Symbols[0].Contains(last) || Symbols[1].Contains(last);
+			return !Double.TryParse(isSymbol ? input.Remove(index) : input, out number);
+		}
 
-                /// <summary>
-                /// Reports the zero-based index of the first occurrence of the specified Unicode
-                /// character in this string.
-                /// </summary>
-                /// <param name="chars">The string containing the value.</param>
-                /// <param name="value">A Unicode character to seek.</param>
-                /// <returns>
-                /// The zero-based index position of value if that character is found, or -1 if it is not.
-                /// </returns>
-                private static int IndexOf(this ICollection<char> chars, char value)
-                {
-                        for (var i = 0; i < chars.Count; i++)
-                                if (chars.ElementAt(i).Equals(value))
-                                        return i;
-                        return -1;
-                }
-        }
+		/// <summary>
+		/// Reports the zero-based index of the first occurrence of the specified Unicode
+		/// character in this string.
+		/// </summary>
+		/// <param name="chars">The string containing the value.</param>
+		/// <param name="value">A Unicode character to seek.</param>
+		/// <returns>
+		/// The zero-based index position of value if that character is found, or -1 if it is not.
+		/// </returns>
+		private static int IndexOf(this ICollection<char> chars, char value)
+		{
+			for (var i = 0; i < chars.Count; i++)
+				if (chars.ElementAt(i).Equals(value))
+					return i;
+			return -1;
+		}
+	}
 }
