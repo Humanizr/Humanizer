c097bb88107c06e47e22a5903286f76461faecd5
Oren Novotny <oren@novotny.org>
2015-10-20 10:00:16 -0400

    Add locking around verify method to handle multi-threaded tests

--- src/Humanizer.Tests/DateHumanize.cs
+++ src/Humanizer.Tests/DateHumanize.cs
@@ -9,6 +9,7 @@ namespace Humanizer.Tests
 {
     public class DateHumanize
     {
+        readonly static object LockObject = new object();
         static void VerifyWithCurrentDate(string expectedString, TimeSpan deltaFromNow, CultureInfo culture)
         {
             var utcNow = DateTime.UtcNow;
@@ -34,50 +35,54 @@ namespace Humanizer.Tests
 
         public static void Verify(string expectedString, int unit, TimeUnit timeUnit, Tense tense, double? precision = null, CultureInfo culture = null, DateTime? baseDate = null, DateTime? baseDateUtc = null)
         {
-            if (precision.HasValue)
-                Configurator.DateTimeHumanizeStrategy = new PrecisionDateTimeHumanizeStrategy(precision.Value);
-            else
-                Configurator.DateTimeHumanizeStrategy = new DefaultDateTimeHumanizeStrategy();
-
-            var deltaFromNow = new TimeSpan();
-            unit = Math.Abs(unit);
-
-            if (tense == Tense.Past)
-                unit = -unit;
-
-            switch (timeUnit)
+            // We lock this as these tests can be multi-threaded and we're setting a static
+            lock (LockObject)
             {
-                case TimeUnit.Millisecond:
-                    deltaFromNow = TimeSpan.FromMilliseconds(unit);
-                    break;
-                case TimeUnit.Second:
-                    deltaFromNow = TimeSpan.FromSeconds(unit);
-                    break;
-                case TimeUnit.Minute:
-                    deltaFromNow = TimeSpan.FromMinutes(unit);
-                    break;
-                case TimeUnit.Hour:
-                    deltaFromNow = TimeSpan.FromHours(unit);
-                    break;
-                case TimeUnit.Day:
-                    deltaFromNow = TimeSpan.FromDays(unit);
-                    break;
-                case TimeUnit.Month:
-                    deltaFromNow = TimeSpan.FromDays(unit*31);
-                    break;
-                case TimeUnit.Year:
-                    deltaFromNow = TimeSpan.FromDays(unit*366);
-                    break;
-            }
+                if (precision.HasValue)
+                    Configurator.DateTimeHumanizeStrategy = new PrecisionDateTimeHumanizeStrategy(precision.Value);
+                else
+                    Configurator.DateTimeHumanizeStrategy = new DefaultDateTimeHumanizeStrategy();
 
-            if (baseDate == null)
-            { 
-                VerifyWithCurrentDate(expectedString, deltaFromNow, culture);
-                VerifyWithDateInjection(expectedString, deltaFromNow, culture);
-            }
-            else
-            {
-                VerifyWithDate(expectedString, deltaFromNow, culture, baseDate.Value, baseDateUtc.Value);
+                var deltaFromNow = new TimeSpan();
+                unit = Math.Abs(unit);
+
+                if (tense == Tense.Past)
+                    unit = -unit;
+
+                switch (timeUnit)
+                {
+                    case TimeUnit.Millisecond:
+                        deltaFromNow = TimeSpan.FromMilliseconds(unit);
+                        break;
+                    case TimeUnit.Second:
+                        deltaFromNow = TimeSpan.FromSeconds(unit);
+                        break;
+                    case TimeUnit.Minute:
+                        deltaFromNow = TimeSpan.FromMinutes(unit);
+                        break;
+                    case TimeUnit.Hour:
+                        deltaFromNow = TimeSpan.FromHours(unit);
+                        break;
+                    case TimeUnit.Day:
+                        deltaFromNow = TimeSpan.FromDays(unit);
+                        break;
+                    case TimeUnit.Month:
+                        deltaFromNow = TimeSpan.FromDays(unit*31);
+                        break;
+                    case TimeUnit.Year:
+                        deltaFromNow = TimeSpan.FromDays(unit*366);
+                        break;
+                }
+
+                if (baseDate == null)
+                {
+                    VerifyWithCurrentDate(expectedString, deltaFromNow, culture);
+                    VerifyWithDateInjection(expectedString, deltaFromNow, culture);
+                }
+                else
+                {
+                    VerifyWithDate(expectedString, deltaFromNow, culture, baseDate.Value, baseDateUtc.Value);
+                }
             }
         }
     }
