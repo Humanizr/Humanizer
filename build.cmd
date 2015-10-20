60ee14f53849fa6c87282a136f00753d3ac8f171
Oren Novotny <oren@novotny.org>
2015-10-19 21:06:52 -0400

    call ps build

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
-@%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild build.proj /m /clp:Verbosity=minimal /p:TargetPortable="true"
-@%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild build.proj /m /clp:Verbosity=minimal /p:TargetPortable="false"
+@powershell .\build.ps1 -buildPortable "true"
+@powershell .\build.ps1 -buildPortable "false"
