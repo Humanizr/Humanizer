b176b824f8be890821364540432f03306b53b52e
Oren Novotny <oren@novotny.org>
2015-10-25 16:00:57 -0400

    Update to latest ApiApprover

--- src/Humanizer.Tests/ApiApprover/PublicApiApprovalTest.cs
+++ src/Humanizer.Tests/ApiApprover/PublicApiApprovalTest.cs
@@ -1,6 +1,8 @@
-﻿using ApiApprover;
+﻿using System.IO;
+using ApiApprover;
 using ApprovalTests;
 using ApprovalTests.Reporters;
+using Mono.Cecil;
 using Xunit;
 
 namespace Humanizer.Tests.ApiApprover
@@ -9,10 +11,22 @@ namespace Humanizer.Tests.ApiApprover
     {
         [Fact]
         [UseReporter(typeof(DiffReporter))] 
+        [IgnoreLineEndings(true)]
         public void approve_public_api()
         {
-            var assembly = typeof(StringHumanizeExtensions).Assembly;
-            var publicApi = PublicApiGenerator.CreatePublicApiForAssembly(assembly);
+            var assemblyPath = typeof(StringHumanizeExtensions).Assembly.Location;
+
+            var assemblyResolver = new DefaultAssemblyResolver();
+            assemblyResolver.AddSearchDirectory(Path.GetDirectoryName(assemblyPath));
+
+            var readSymbols = File.Exists(Path.ChangeExtension(assemblyPath, ".pdb"));
+            var asm = AssemblyDefinition.ReadAssembly(assemblyPath, new ReaderParameters(ReadingMode.Deferred)
+            {
+                ReadSymbols = readSymbols,
+                AssemblyResolver = assemblyResolver
+            });
+
+            var publicApi = PublicApiGenerator.CreatePublicApiForAssembly(asm);
             Approvals.Verify(publicApi);
         }
     }
