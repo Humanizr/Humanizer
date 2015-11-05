f7ad8e261354a90ad115839fe091a3f58277fd46
Gavyn <gavyn.riebau@gmail.com>
2015-11-05 11:23:32 +0800

    Handle humanizing bitfield enums (i.e. one that uses the FlagsAttribute)

---
+++ src/Humanizer.Tests/BitFieldEnumHumanizeTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Humanizer.Tests
+{
+    public class BitFieldEnumHumanizeTests
+    {
+        [Fact]
+        public void CanHumanizeSingleWordDescriptionAttribute()
+        {
+            Assert.Equal(BitFlagEnumTestsResources.MemberWithSingleWordDescriptionAttribute, BitFieldEnumUnderTest.RED.Humanize());
+        }
+
+        [Fact]
+        public void CanHumanizeMultipleWordDescriptionAttribute()
+        {
+            Assert.Equal(BitFlagEnumTestsResources.MemberWithMultipleWordDescriptionAttribute, BitFieldEnumUnderTest.DARK_GRAY.Humanize());
+        }
+
+        [Fact]
+        public void CanHumanizeMultipleValueBitFieldEnum()
+        {
+            var xoredBitFlag = (BitFieldEnumUnderTest.RED | BitFieldEnumUnderTest.DARK_GRAY);
+            Assert.Equal(BitFlagEnumTestsResources.ExpectedResultWhenBothValuesXored, xoredBitFlag.Humanize());
+        }
+    }
+}
---
+++ src/Humanizer.Tests/BitFieldEnumUnderTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Humanizer.Tests
+{
+    [Flags]
+    public enum BitFieldEnumUnderTest : int
+    {
+        [Description(BitFlagEnumTestsResources.MemberWithSingleWordDescriptionAttribute)]
+        RED = 1,
+        [Description(BitFlagEnumTestsResources.MemberWithMultipleWordDescriptionAttribute)]
+        DARK_GRAY = 2
+    }
+
+    public class BitFlagEnumTestsResources
+    {
+        public const string MemberWithSingleWordDescriptionAttribute = "Red";
+        public const string MemberWithMultipleWordDescriptionAttribute = "Dark Gray";
+
+        public const string ExpectedResultWhenBothValuesXored = "Red, Dark Gray";
+    }
+}
