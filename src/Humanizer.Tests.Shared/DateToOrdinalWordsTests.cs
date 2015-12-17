63818c226c32a9658ac8eb610ca2914c1db0488f
Robert Pethick <Robert.Pethick@softwire.com>
2015-12-17 09:35:53 +0000

    Add ToOrdinalWords extension with gender overload

---
+++ src/Humanizer.Tests.Shared/DateToOrdinalWordsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace Humanizer.Tests
+{
+    public class DateToOrdinalWordsTests
+    {
+        [Fact]
+        public void CanToOrdinalWords()
+        {
+            var date = new DateTime(2015, 1, 1);
+
+            Assert.Equal("1st January 2015", date.ToOrdinalWords());
+        }
+
+        [UseCulture("pt-BR")]
+        [Fact]
+        public void CanToOrdinalWordsIncludingGender()
+        {
+            var date = new DateTime(2015, 1, 1);
+
+            Assert.Equal("1º janeiro 2015", date.ToOrdinalWords(GrammaticalGender.Masculine));
+        }
+    }
+}
---
+++ src/Humanizer/DateToOrdinalWordsExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using Humanizer.Configuration;
+
+namespace Humanizer
+{
+    /// <summary>
+    /// Humanizes DateTime into human readable sentence
+    /// </summary>
+    public static class DateToOrdinalWordsExtensions
+    {
+        /// <summary>
+        /// Turns the provided date into ordinal words
+        /// </summary>
+        /// <param name="input">The date to be made into ordinal words</param>
+        /// <returns>The date in ordinal words</returns>
+        public static string ToOrdinalWords(this DateTime input)
+        {
+            return input.Day.Ordinalize() + input.ToString(" MMMM yyyy");
+        }
+        /// <summary>
+        /// Turns the provided date into ordinal words
+        /// </summary>
+        /// <param name="input">The date to be made into ordinal words</param>
+        /// <param name="gender">The grammatical gender to use for output words</param>
+        /// <returns>The date in ordinal words</returns>
+        public static string ToOrdinalWords(this DateTime input, GrammaticalGender gender)
+        {
+            return input.Day.Ordinalize(gender) + input.ToString(" MMMM yyyy");
+        }
+    }
+}
