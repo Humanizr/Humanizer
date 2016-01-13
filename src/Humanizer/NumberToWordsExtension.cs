94d6566c9aaeea1a020ff7780a42b92c31e9fa41
Robert Pethick <Robert.Pethick@softwire.com>
2016-01-13 17:05:32 +0000

    Add extra public method for int and long

--- src/Humanizer/NumberToWordsExtension.cs
+++ src/Humanizer/NumberToWordsExtension.cs
@@ -14,6 +14,42 @@ namespace Humanizer
         /// <param name="number">Number to be turned to words</param>
         /// <param name="culture">Culture to use. If null, current thread's UI culture is used.</param>
         /// <returns></returns>
+        public static string ToWords(this int number, CultureInfo culture = null)
+        {
+            return ((long)number).ToWords(culture);
+        }
+
+        /// <summary>
+        /// For locales that support gender-specific forms
+        /// </summary>
+        /// <example>
+        /// Russian:
+        /// <code>
+        ///   1.ToWords(GrammaticalGender.Masculine) -> "один"
+        ///   1.ToWords(GrammaticalGender.Feminine) -> "одна"
+        /// </code>
+        /// Hebrew:
+        /// <code>
+        ///   1.ToWords(GrammaticalGender.Masculine) -> "אחד"
+        ///   1.ToWords(GrammaticalGender.Feminine) -> "אחת"
+        /// </code>
+        /// </example>
+        /// 
+        /// <param name="number">Number to be turned to words</param>
+        /// <param name="gender">The grammatical gender to use for output words</param>
+        /// <param name="culture">Culture to use. If null, current thread's UI culture is used.</param>
+        /// <returns></returns>
+        public static string ToWords(this int number, GrammaticalGender gender, CultureInfo culture = null)
+        {
+            return ((long)number).ToWords(gender, culture);
+        }
+
+        /// <summary>
+        /// 3501.ToWords() -> "three thousand five hundred and one"
+        /// </summary>
+        /// <param name="number">Number to be turned to words</param>
+        /// <param name="culture">Culture to use. If null, current thread's UI culture is used.</param>
+        /// <returns></returns>
         public static string ToWords(this long number, CultureInfo culture = null)
         {
             return Configurator.GetNumberToWordsConverter(culture).Convert(number);
