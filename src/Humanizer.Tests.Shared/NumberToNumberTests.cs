4ad53f65c32e63d61e24a964294aab6994dd3774
Bar Arnon <i3arnon@gmail.com>
2016-01-31 01:03:35 +0200

    Fixes #518

---
+++ src/Humanizer.Tests.Shared/NumberToNumberTests.cs
@@ -0,0 +1,182 @@
+﻿using Xunit;
+
+namespace Humanizer.Tests
+{
+    public class NumberToNumberTests
+    {
+        [Fact]
+        public void IntToTens()
+        {
+            const int number = 1;
+            Assert.Equal(10, number.Tens());
+        }
+
+        [Fact]
+        public void UintToTens()
+        {
+            const uint number = 1;
+            Assert.Equal(10U, number.Tens());
+        }
+
+        [Fact]
+        public void LongToTens()
+        {
+            const long number = 1;
+            Assert.Equal(10L, number.Tens());
+        }
+
+        [Fact]
+        public void UlongToTens()
+        {
+            const ulong number = 1;
+            Assert.Equal(10UL, number.Tens());
+        }
+
+        [Fact]
+        public void DoubleToTens()
+        {
+            const double number = 1;
+            Assert.Equal(10d, number.Tens());
+        }
+
+        [Fact]
+        public void IntToHundreds()
+        {
+            const int number = 2;
+            Assert.Equal(200, number.Hundreds());
+        }
+
+        [Fact]
+        public void UintToHundreds()
+        {
+            const uint number = 2;
+            Assert.Equal(200U, number.Hundreds());
+        }
+
+        [Fact]
+        public void LongToHundreds()
+        {
+            const long number = 2;
+            Assert.Equal(200L, number.Hundreds());
+        }
+
+        [Fact]
+        public void UlongToHundreds()
+        {
+            const ulong number = 2;
+            Assert.Equal(200UL, number.Hundreds());
+        }
+
+        [Fact]
+        public void DoubleToHundreds()
+        {
+            const double number = 2;
+            Assert.Equal(200d, number.Hundreds());
+        }
+
+        [Fact]
+        public void IntToThousands()
+        {
+            const int number = 3;
+            Assert.Equal(3000, number.Thousands());
+        }
+
+        [Fact]
+        public void UintToThousands()
+        {
+            const uint number = 3;
+            Assert.Equal(3000U, number.Thousands());
+        }
+
+        [Fact]
+        public void LongToThousands()
+        {
+            const long number = 3;
+            Assert.Equal(3000L, number.Thousands());
+        }
+
+        [Fact]
+        public void UlongToThousands()
+        {
+            const ulong number = 3;
+            Assert.Equal(3000UL, number.Thousands());
+        }
+
+        [Fact]
+        public void DoubleToThousands()
+        {
+            const double number = 3;
+            Assert.Equal(3000d, number.Thousands());
+        }
+
+        [Fact]
+        public void IntToMillions()
+        {
+            const int number = 4;
+            Assert.Equal(4000000, number.Millions());
+        }
+
+        [Fact]
+        public void UintToMillions()
+        {
+            const uint number = 4;
+            Assert.Equal(4000000U, number.Millions());
+        }
+
+        [Fact]
+        public void LongToMillions()
+        {
+            const long number = 4;
+            Assert.Equal(4000000L, number.Millions());
+        }
+
+        [Fact]
+        public void UlongToMillions()
+        {
+            const ulong number = 4;
+            Assert.Equal(4000000UL, number.Millions());
+        }
+
+        [Fact]
+        public void DoubleToMillions()
+        {
+            const double number = 4;
+            Assert.Equal(4000000d, number.Millions());
+        }
+
+        [Fact]
+        public void IntToBillions()
+        {
+            const int number = 1;
+            Assert.Equal(1000000000, number.Billions());
+        }
+
+        [Fact]
+        public void UintToBillions()
+        {
+            const uint number = 1;
+            Assert.Equal(1000000000U, number.Billions());
+        }
+
+        [Fact]
+        public void LongToBillions()
+        {
+            const long number = 1;
+            Assert.Equal(1000000000L, number.Billions());
+        }
+
+        [Fact]
+        public void UlongToBillions()
+        {
+            const ulong number = 1;
+            Assert.Equal(1000000000UL, number.Billions());
+        }
+
+        [Fact]
+        public void DoubleToBillions()
+        {
+            const double number = 1;
+            Assert.Equal(1000000000d, number.Billions());
+        }
+    }
+}
---
+++ src/Humanizer/NumberToNumberExtensions.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Humanizer
+{
+    /// <summary>
+    /// Number to Number extensions
+    /// </summary>
+    public static class NumberToNumberExtensions
+    {
+        /// <summary>
+        /// 5.Tens == 50
+        /// </summary>
+        public static int Tens(this int input)
+        {
+            return input*10;
+        }
+
+        /// <summary>
+        /// 5.Tens == 50
+        /// </summary>
+        public static uint Tens(this uint input)
+        {
+            return input*10;
+        }
+
+        /// <summary>
+        /// 5.Tens == 50
+        /// </summary>
+        public static long Tens(this long input)
+        {
+            return input*10;
+        }
+
+        /// <summary>
+        /// 5.Tens == 50
+        /// </summary>
+        public static ulong Tens(this ulong input)
+        {
+            return input*10;
+        }
+
+        /// <summary>
+        /// 5.Tens == 50
+        /// </summary>
+        public static double Tens(this double input)
+        {
+            return input*10;
+        }
+
+        /// <summary>
+        /// 4.Hundreds() == 400
+        /// </summary>
+        public static int Hundreds(this int input)
+        {
+            return input*100;
+        }
+
+        /// <summary>
+        /// 4.Hundreds() == 400
+        /// </summary>
+        public static uint Hundreds(this uint input)
+        {
+            return input*100;
+        }
+
+        /// <summary>
+        /// 4.Hundreds() == 400
+        /// </summary>
+        public static long Hundreds(this long input)
+        {
+            return input*100;
+        }
+
+        /// <summary>
+        /// 4.Hundreds() == 400
+        /// </summary>
+        public static ulong Hundreds(this ulong input)
+        {
+            return input*100;
+        }
+
+        /// <summary>
+        /// 4.Hundreds() == 400
+        /// </summary>
+        public static double Hundreds(this double input)
+        {
+            return input*100;
+        }
+
+        /// <summary>
+        /// 3.Thousands() == 3000
+        /// </summary>
+        public static int Thousands(this int input)
+        {
+            return input*1000;
+        }
+
+        /// <summary>
+        /// 3.Thousands() == 3000
+        /// </summary>
+        public static uint Thousands(this uint input)
+        {
+            return input*1000;
+        }
+
+        /// <summary>
+        /// 3.Thousands() == 3000
+        /// </summary>
+        public static long Thousands(this long input)
+        {
+            return input*1000;
+        }
+
+        /// <summary>
+        /// 3.Thousands() == 3000
+        /// </summary>
+        public static ulong Thousands(this ulong input)
+        {
+            return input*1000;
+        }
+
+        /// <summary>
+        /// 3.Thousands() == 3000
+        /// </summary>
+        public static double Thousands(this double input)
+        {
+            return input*1000;
+        }
+
+        /// <summary>
+        /// 2.Millions() == 2000000
+        /// </summary>
+        public static int Millions(this int input)
+        {
+            return input*1000000;
+        }
+
+        /// <summary>
+        /// 2.Millions() == 2000000
+        /// </summary>
+        public static uint Millions(this uint input)
+        {
+            return input*1000000;
+        }
+
+        /// <summary>
+        /// 2.Millions() == 2000000
+        /// </summary>
+        public static long Millions(this long input)
+        {
+            return input*1000000;
+        }
+
+        /// <summary>
+        /// 2.Millions() == 2000000
+        /// </summary>
+        public static ulong Millions(this ulong input)
+        {
+            return input*1000000;
+        }
+
+        /// <summary>
+        /// 2.Millions() == 2000000
+        /// </summary>
+        public static double Millions(this double input)
+        {
+            return input*1000000;
+        }
+
+        /// <summary>
+        /// 1.Billions() == 1000000000
+        /// </summary>
+        public static int Billions(this int input)
+        {
+            return input*1000000000;
+        }
+
+        /// <summary>
+        /// 1.Billions() == 1000000000
+        /// </summary>
+        public static uint Billions(this uint input)
+        {
+            return input*1000000000;
+        }
+
+        /// <summary>
+        /// 1.Billions() == 1000000000
+        /// </summary>
+        public static long Billions(this long input)
+        {
+            return input*1000000000;
+        }
+
+        /// <summary>
+        /// 1.Billions() == 1000000000
+        /// </summary>
+        public static ulong Billions(this ulong input)
+        {
+            return input*1000000000;
+        }
+
+        /// <summary>
+        /// 1.Billions() == 1000000000
+        /// </summary>
+        public static double Billions(this double input)
+        {
+            return input*1000000000;
+        }
+    }
+}
