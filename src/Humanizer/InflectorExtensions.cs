de8eab3eeb5a87c5986aa6fecb50feec574e0551
EU\Claudio Varini <claudio.varini@eu.averydennison.com>
2017-02-07 13:18:40 +0100

    removed a comment and an empty char

--- src/Humanizer/InflectorExtensions.cs
+++ src/Humanizer/InflectorExtensions.cs
@@ -94,7 +94,7 @@ namespace Humanizer
         {
             return Regex.Replace(
                 Regex.Replace(
-                    Regex.Replace(input, @"([\p{Lu}]+)([\p{Lu}][\p{Ll}])", "$1_$2"), @"([\p{Ll}\d])([\p{Lu}])", "$1_$2"), @"[-\s]", "_" ).ToLower(); // end first 
+                    Regex.Replace(input, @"([\p{Lu}]+)([\p{Lu}][\p{Ll}])", "$1_$2"), @"([\p{Ll}\d])([\p{Lu}])", "$1_$2"), @"[-\s]", "_").ToLower();
         }
 
         /// <summary>
