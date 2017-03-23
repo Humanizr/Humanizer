eb110e56c72b3529cf9a7f649f499499315482a7
Alois <aloisdegouvello@live.fr>
2017-03-23 15:10:49 +0100

    clean code a bit
    I am not sure about the line 12. I think the difference between both is more clear like this.

--- src/Humanizer/Localisation/NumberToWords/FrenchNumberToWordsConverter.cs
+++ src/Humanizer/Localisation/NumberToWords/FrenchNumberToWordsConverter.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace Humanizer.Localisation.NumberToWords
 {
@@ -9,24 +9,18 @@ namespace Humanizer.Localisation.NumberToWords
             if (number == 71)
                 parts.Add("soixante et onze");
             else if (number == 80)
-                parts.Add(pluralize ? "quatre-vingts" : "quatre-vingt");
+                parts.Add("quatre-vingt" + (pluralize ? "s" : string.Empty));
             else if (number >= 70)
             {
                 var @base = number < 80 ? 60 : 80;
                 int units = number - @base;
-                var tens = @base/10;
-                parts.Add(string.Format("{0}-{1}", GetTens(tens), GetUnits(units, gender)));
+                var tens = @base / 10;
+                parts.Add($"{GetTens(tens)}-{GetUnits(units, gender)}");
             }
             else
                 base.CollectPartsUnderAHundred(parts, ref number, gender, pluralize);
         }
 
-        protected override string GetTens(int tens)
-        {
-            if (tens == 8)
-                return "quatre-vingt";
-
-            return base.GetTens(tens);
-        }
+        protected override string GetTens(int tens) => tens == 8 ? "quatre-vingt" : base.GetTens(tens);
     }
 }
