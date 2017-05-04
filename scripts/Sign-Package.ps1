a10e5895609317302f154d28b65c0fe58970a588
Oren Novotny <oren@novotny.org>
2017-05-03 20:08:59 -0400

    fix sign client syntax

--- scripts/Sign-Package.ps1
+++ scripts/Sign-Package.ps1
@@ -17,7 +17,7 @@ $nupgks = ls $currentDirectory\..\*.nupkg | Select -ExpandProperty FullName
 foreach ($nupkg in $nupgks){
 	Write-Host "Submitting $nupkg for signing"
 
-	dotnet $appPath 'zip' -c $appSettings -i $nupkg -s $env:SignClientSecret -n 'Humanizer' -d 'Humanizer' -u 'https://github.com/Humanizr/Humanizer' 
+	dotnet $appPath 'sign' -c $appSettings -i $nupkg -s $env:SignClientSecret -n 'Humanizer' -d 'Humanizer' -u 'https://github.com/Humanizr/Humanizer' 
 
 	Write-Host "Finished signing $nupkg"
 }
