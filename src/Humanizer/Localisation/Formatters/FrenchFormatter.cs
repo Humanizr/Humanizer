53e459e6ba240d35d220bd27856136aeecefd696
Casey <cbro234@gmail.com>
2018-01-06 17:17:36 -0700

    Formatter for avant-hier and apres-demain

---
+++ src/Humanizer/Localisation/Formatters/FrenchFormatter.cs
@@ -0,0 +1,20 @@
+﻿namespace Humanizer.Localisation.Formatters
+{
+    internal class FrenchFormatter : DefaultFormatter
+    {
+        private const string DualPostfix = "_Dual";
+        
+        public FrenchFormatter()
+            : base("fr")
+        {
+        }
+
+        protected override string GetResourceKey(string resourceKey, int number)
+        {
+            if (number == 2 && (resourceKey == "DateHumanize_MultipleDaysAgo" || resourceKey == "DateHumanize_MultipleDaysFromNow"))
+                return resourceKey + DualPostfix;
+
+            return resourceKey;
+        }
+    }
+}
