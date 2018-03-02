36dbeae3973d5688a23d8f326dd2c519b45d0930
jnm2 <me@jnm2.com>
2018-03-02 13:15:13 -0500

    Make room for object-returning formatter test names

--- src/Humanizer.Tests.Shared/CollectionHumanizeTests.cs
+++ src/Humanizer.Tests.Shared/CollectionHumanizeTests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Xunit;
 
@@ -15,7 +15,7 @@ namespace Humanizer.Tests
     }
 
     [UseCulture("en")]
-    public class CollectionHumanizeTests 
+    public class CollectionHumanizeTests
     {
         [Fact]
         public void HumanizeReturnsOnlyNameWhenCollectionContainsOneItem()
@@ -76,14 +76,14 @@ namespace Humanizer.Tests
         }
 
         [Fact]
-        public void HumanizeUsesObjectFormatter()
+        public void HumanizeUsesStringDisplayFormatter()
         {
             var humanized = _testCollection.Humanize(sc => string.Format("SomeObject #{0} - {1}", sc.SomeInt, sc.SomeString));
             Assert.Equal("SomeObject #1 - One, SomeObject #2 - Two, and SomeObject #3 - Three", humanized);
         }
 
         [Fact]
-        public void HumanizeUsesObjectFormatterWhenSeparatorIsProvided()
+        public void HumanizeUsesStringDisplayFormatterWhenSeparatorIsProvided()
         {
             var humanized = _testCollection.Humanize(sc => string.Format("SomeObject #{0} - {1}", sc.SomeInt, sc.SomeString), "or");
             Assert.Equal("SomeObject #1 - One, SomeObject #2 - Two, or SomeObject #3 - Three", humanized);
@@ -96,13 +96,13 @@ namespace Humanizer.Tests
         }
 
         [Fact]
-        public void HumanizeHandlesNullFormatterReturnsWithoutAnException()
+        public void HumanizeHandlesNullStringDisplayFormatterReturnsWithoutAnException()
         {
             Assert.Null(Record.Exception(() => new[] { "A", "B", "C" }.Humanize(_ => null)));
         }
 
         [Fact]
-        public void HumanizeRunsFormatterOnNulls()
+        public void HumanizeRunsStringDisplayFormatterOnNulls()
         {
             Assert.Equal("1, (null), and 3", new int?[] { 1, null, 3 }.Humanize(_ => _?.ToString() ?? "(null)"));
         }
