da025ad462e626db89a4acab347f0b03e361b9de
Oren Novotny <oren@novotny.org>
2018-06-30 09:50:55 -0400

    exclude from netcore

--- src/Humanizer.Tests/ApiApprover/DiffPlexReporter.cs
+++ src/Humanizer.Tests/ApiApprover/DiffPlexReporter.cs
@@ -1,4 +1,5 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+﻿#if NET46
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information. 
 
@@ -45,3 +46,4 @@ namespace Humanizer.Tests.ApiApprover
         }
     }
 }
+#endif
