adb47a8599a1b0f5b47323f03d28d01daa1642f8
aktheknight <alex@aktheknight.co.uk>
2019-02-25 22:43:56 +0000

    Add MetricPrefix
    This will be used to say what prefix type we want returned

---
+++ src/Humanizer/MetricPrefix.cs
@@ -0,0 +1,13 @@
+namespace Humanizer
+{
+    /// <summary>
+    /// Contains the options for use of Short and Long scales with Metric conversions
+    /// </summary>
+    public enum MetricPrefix
+    {   
+        Symbol = 0,
+        Word = 1,
+        Short = 2,
+        Long = 3
+    }
+}
