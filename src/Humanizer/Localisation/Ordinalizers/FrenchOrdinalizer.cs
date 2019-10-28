becee46ccfd62e7da38efbc1a48d4a12834b0d99
Lionel Dimitrijevic <lionel.dimitrijevic@gmail.com>
2019-10-28 20:45:31 +0100

    French ordinal registry

---
+++ src/Humanizer/Localisation/Ordinalizers/FrenchOrdinalizer.cs
@@ -0,0 +1,24 @@
+﻿namespace Humanizer.Localisation.Ordinalizers
+{
+    internal class FrenchOrdinalizer : DefaultOrdinalizer
+    {
+        public override string Convert(int number, string numberString)
+        {
+            return Convert(number, numberString, GrammaticalGender.Masculine);
+        }
+
+        public override string Convert(int number, string numberString, GrammaticalGender gender)
+        {
+            if (number == 1)
+            {
+                if (gender == GrammaticalGender.Feminine)
+                {
+                    return numberString + "ère";
+                }
+                return numberString + "er";
+            }
+
+            return numberString + "ème";
+        }
+    }
+}
