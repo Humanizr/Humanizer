c1837e30eb53b5cc8dafe94a990ec8e79cea5e78
Antonio Nakic Alfirevic <antonio.nakic@gmail.com>
2020-02-11 12:39:23 +0100

    Update SerbianNumberToWordsConverter.cs
    Fixing typo for 14 ("četrnaestt"->"četrnaest")

--- src/Humanizer/Localisation/NumberToWords/SerbianNumberToWordsConverter.cs
+++ src/Humanizer/Localisation/NumberToWords/SerbianNumberToWordsConverter.cs
@@ -6,7 +6,7 @@ namespace Humanizer.Localisation.NumberToWords
 {
     internal class SerbianNumberToWordsConverter : GenderlessNumberToWordsConverter
     {
-        private static readonly string[] UnitsMap = { "nula", "jedan", "dva", "tri", "četiri", "pet", "šest", "sedam", "osam", "devet", "deset", "jedanaest", "dvanaest", "trinaest", "četrnaestt", "petnaest", "šestnaest", "sedemnaest", "osemnaest", "devetnaest" };
+        private static readonly string[] UnitsMap = { "nula", "jedan", "dva", "tri", "četiri", "pet", "šest", "sedam", "osam", "devet", "deset", "jedanaest", "dvanaest", "trinaest", "četrnaest", "petnaest", "šestnaest", "sedemnaest", "osemnaest", "devetnaest" };
         private static readonly string[] TensMap = { "nula", "deset", "dvadeset", "trideset", "četrdeset", "petdeset", "šestdeset", "sedamdeset", "osamdeset", "devetdeset" };
 
         private readonly CultureInfo _culture;
