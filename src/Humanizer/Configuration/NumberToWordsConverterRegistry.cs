32b1cfd14f184b1f29d9716452c9e89fa79223d2
Nattakit Sriburanapitak <czbas23@gmail.com>
2021-02-03 22:31:04 +0700

    Support Thai formatter and number to word

--- src/Humanizer/Configuration/NumberToWordsConverterRegistry.cs
+++ src/Humanizer/Configuration/NumberToWordsConverterRegistry.cs
@@ -47,6 +47,7 @@ namespace Humanizer.Configuration
             Register("hy", new ArmenianNumberToWordsConverter());
             Register("az", new AzerbaijaniNumberToWordsConverter());
             Register("ja", new JapaneseNumberToWordsConverter());
+            Register("th-TH", new ThaiNumberToWordsConverter());
         }
     }
 }
