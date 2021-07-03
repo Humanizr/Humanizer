689802b55f94ccb8e4e8d33b121f36b85d9e7108
hangy <hangy@hangy.de>
2021-07-03 13:52:04 +0200

    Add TimeUnitToSymbolTests that should be fulfilled

---
+++ src/Humanizer.Tests.Shared/Localisation/de/TimeUnitToSymbolTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Humanizer.Localisation;
+
+using Xunit;
+
+namespace Humanizer.Tests.Localisation.de
+{
+    [UseCulture("de-DE")]
+    public class TimeUnitToSymbolTests
+    {
+        [Theory]
+        [Trait("Translation", "Native speaker")]
+        [InlineData(TimeUnit.Millisecond, "ms")]
+        [InlineData(TimeUnit.Second, "s")]
+        [InlineData(TimeUnit.Minute, "min")]
+        [InlineData(TimeUnit.Hour, "h")]
+        [InlineData(TimeUnit.Day, "d")]
+        [InlineData(TimeUnit.Week, "Woche")]
+        [InlineData(TimeUnit.Month, "M")]
+        [InlineData(TimeUnit.Year, "a")]
+        public void ToSymbol(TimeUnit unit, string expected)
+        {
+            Assert.Equal(expected, unit.ToSymbol());
+        }
+    }
+}
