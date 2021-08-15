30eb3c40a05b696d1b34d752cdced11418c8e2cb
kristinn.stefansson <kristinn.stefansson@gmail.com>
2021-08-15 12:06:35 +0000

    Add resources for Icelandic localisation
    Make basic tests for accessing resources pass.
    Overload DefaultFormatter in IcelandicFormatter, having to trim
    plural endings that do not apply to Icelandic while anticipating
    gender rules for Icelandic localisation of time based units with month
    and day being masculine, year being neuter and others, eg. seconds
    and hours taking the feminine gender. This called for access to
    CultureInfo as base class does not expose this (improvement
    suggestion?).
    Register the Icelandic Formatter in Formatter Registry.
    Some code branches cannot be unit tested in formatter as e.g.
    Argument Exception can only be thrown if missing resource key.
    Dual and TrialQuadral resource strings not included as they do not
    apply to the Icelandic implementation.

---
+++ src/Humanizer/Localisation/Formatters/IcelandicFormatter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Humanizer.Localisation.Formatters
+{
+    internal class IcelandicFormatter : DefaultFormatter
+    {
+        private const string LocaleCode = "is";
+        private readonly CultureInfo _localCulture;
+        public IcelandicFormatter()
+            : base(LocaleCode)
+        {
+            _localCulture = new CultureInfo(LocaleCode);
+        }
+        public override string DataUnitHumanize(DataUnit dataUnit, double count, bool toSymbol = true)
+        {
+            return base.DataUnitHumanize(dataUnit, count, toSymbol)?.TrimEnd('s');
+        }
+        protected override string Format(string resourceKey, int number, bool toWords = false)
+        {
+            var resourceString = Resources.GetResource(GetResourceKey(resourceKey, number), _localCulture);
+
+            if (string.IsNullOrEmpty(resourceString))
+            {
+                throw new ArgumentException($@"The resource object with key '{resourceKey}' was not found", nameof(resourceKey));
+            }
+            var words = resourceString.Split(' ');
+
+            var unitGender = words.Last() switch
+            {
+                var x when x.StartsWith("mán") => GrammaticalGender.Masculine,
+                var x when x.StartsWith("dag") => GrammaticalGender.Masculine,
+                var x when x.StartsWith("ár") => GrammaticalGender.Neuter,
+                _ => GrammaticalGender.Feminine
+            };
+
+            return toWords ? 
+                resourceString.FormatWith(number.ToWords(unitGender, _localCulture)) :
+                resourceString.FormatWith(number);
+        }
+    }
+}
