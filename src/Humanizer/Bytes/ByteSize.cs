359fd8733cb33b1463112dc2695df10a5ed1842a
Yoh Deadfall <yoh.deadfall@hotmail.com>
2021-10-05 21:38:37 +0300

    Removed ToString call ignoring the format

--- src/Humanizer/Bytes/ByteSize.cs
+++ src/Humanizer/Bytes/ByteSize.cs
@@ -220,7 +220,7 @@ namespace Humanizer.Bytes
         /// </summary>
         public override string ToString()
         {
-            return ToString(NumberFormatInfo.CurrentInfo);
+            return ToString(provider: null);
         }
 
         public string ToString(IFormatProvider provider)
@@ -228,7 +228,7 @@ namespace Humanizer.Bytes
             if (provider == null)
                 provider = CultureInfo.CurrentCulture;
 
-            return string.Format("{0:0.##} {1}", LargestWholeNumberValue.ToString(provider), GetLargestWholeNumberSymbol(provider));
+            return string.Format("{0:0.##} {1}", LargestWholeNumberValue, GetLargestWholeNumberSymbol(provider));
         }
 
         public string ToString(string format)
