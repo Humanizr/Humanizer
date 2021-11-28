f6ad841b4879bc1a86eabbad0bd4bb4e61d1544f
Louis Zanella <louis.zanella@gmail.com>
2021-11-28 09:26:17 -0500

    Fix DateOnly, TimeOnly & DateTime issues in French
    - Add DateToOrdinalWordsTests, TimeOnlyHumanizeTests & TimeToClockNotationTests for French culture
    - Adapt code so that tests pass

---
+++ src/Humanizer.Tests.Shared/Localisation/fr/DateToOrdinalWordsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+namespace Humanizer.Tests.Localisation.fr
+{
+    [UseCulture("fr")]
+    public class DateToOrdinalWordsTests
+    {
+        [Fact]
+        public void OrdinalizeString()
+        {
+            Assert.Equal("1er janvier 2015", new DateTime(2015, 1, 1).ToOrdinalWords());
+            Assert.Equal("2 mars 2020", new DateTime(2020, 3, 2).ToOrdinalWords());
+            Assert.Equal("31 octobre 2021", new DateTime(2021, 10, 31).ToOrdinalWords());
+        }
+
+#if NET6_0_OR_GREATER
+        [Fact]
+        public void OrdinalizeDateOnlyString()
+        {
+            Assert.Equal("1er janvier 2015", new DateOnly(2015, 1, 1).ToOrdinalWords());
+            Assert.Equal("2 mars 2020", new DateOnly(2020, 3, 2).ToOrdinalWords());
+            Assert.Equal("31 octobre 2021", new DateOnly(2021, 10, 31).ToOrdinalWords());
+        }
+#endif
+    }
+}
---
+++ src/Humanizer/Localisation/TimeToClockNotation/FrTimeOnlyToClockNotationConverter.cs
@@ -0,0 +1,43 @@
+﻿#if NET6_0_OR_GREATER
+
+using System;
+using Humanizer;
+
+namespace Humanizer.Localisation.TimeToClockNotation
+{
+    internal class FrTimeOnlyToClockNotationConverter : ITimeOnlyToClockNotationConverter
+    {
+        public virtual string Convert(TimeOnly time, ClockNotationRounding roundToNearestFive)
+        {
+            switch (time)
+            {
+                case { Hour: 0, Minute: 0 }:
+                    return "minuit";
+                case { Hour: 12, Minute: 0 }:
+                    return "midi";
+            }
+
+            var normalizedMinutes = (int)(roundToNearestFive == ClockNotationRounding.NearestFiveMinutes
+                ? 5 * Math.Round(time.Minute / 5.0)
+                : time.Minute);
+
+            return normalizedMinutes switch
+            {
+                00 => GetHourExpression(time.Hour),
+                15 => $"{GetHourExpression(time.Hour)} et quart",
+                30 => $"{GetHourExpression(time.Hour)} et demie",
+                40 => $"{GetHourExpression(time.Hour + 1)} moins vingt",
+                45 => $"{GetHourExpression(time.Hour + 1)} moins le quart",
+                60 => $"{GetHourExpression(time.Hour + 1)}",
+                _ => $"{GetHourExpression(time.Hour)} {normalizedMinutes.ToWords(GrammaticalGender.Feminine)}"
+            };
+
+            static string GetHourExpression(int hour)
+            {
+                return hour.ToWords(GrammaticalGender.Feminine) + (hour > 1 ? " heures" : " heure");
+            }
+        }
+    }
+}
+
+#endif
