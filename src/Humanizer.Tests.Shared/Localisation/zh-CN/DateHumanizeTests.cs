469a4ee5b6c03f47633754e683ffdd70bd1582e1
Dameng <313880747@qq.com>
2022-11-14 15:07:34 +0000

    fix tests related to `now` chinese translation

--- src/Humanizer.Tests.Shared/Localisation/zh-CN/DateHumanizeTests.cs
+++ src/Humanizer.Tests.Shared/Localisation/zh-CN/DateHumanizeTests.cs
@@ -10,7 +10,7 @@ namespace Humanizer.Tests.Localisation.zhCN
         [Theory]
         [InlineData(2, "2 天前")]
         [InlineData(1, "昨天")]
-        [InlineData(0, "今天")]
+        [InlineData(0, "现在")]
         public void DaysAgo(int days, string expected)
         {
             DateHumanize.Verify(expected, days, TimeUnit.Day, Tense.Past);
@@ -19,7 +19,7 @@ namespace Humanizer.Tests.Localisation.zhCN
         [Theory]
         [InlineData(2, "2 天后")]
         [InlineData(1, "明天")]
-        [InlineData(0, "今天")]
+        [InlineData(0, "现在")]
         public void DaysFromNow(int days, string expected)
         {
             DateHumanize.Verify(expected, days, TimeUnit.Day, Tense.Future);
