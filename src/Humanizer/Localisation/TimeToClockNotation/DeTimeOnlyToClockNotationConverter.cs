7b6f44b6ba0c2f5db10398492392faadf3aa8426
Spoc Web <10584012+SpocWeb@users.noreply.github.com>
2022-12-27 12:51:03 +0100

    removed Curlies around single-Line

--- src/Humanizer/Localisation/TimeToClockNotation/DeTimeOnlyToClockNotationConverter.cs
+++ src/Humanizer/Localisation/TimeToClockNotation/DeTimeOnlyToClockNotationConverter.cs
@@ -20,13 +20,12 @@ public class DeTimeOnlyToClockNotationConverter : ITimeOnlyToClockNotationConver
         if (!PrePendQuarter.HasValue
             || ret == German.MidNight
             || ret == German.MidDay)
-        {
             return ret;
-        }
-        return PrePendQuarter switch
+
+        return PrePendQuarter.Value switch
         {
             false => ret + ' ' + time.GetDayQuarterGermanGenitive(),
-            true => time.GetDayQuarterGermanGenitive() + ' ' + ret, _ => ret
+            true => time.GetDayQuarterGermanGenitive() + ' ' + ret
         };
     }
 }
