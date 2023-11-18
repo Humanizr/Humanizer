db841ce8a62f4ebcb24b48ff9d60c664d1a32e99
Olegas <olegas@warranty.expert>
2023-11-18 18:29:03 +0200

    add LtDateOnlyToOrdinalWordsConverter and LtDateToOrdinalWordsConverter

---
+++ src/Humanizer/Localisation/DateToOrdinalWords/LtDateOnlyToOrdinalWordsConverter.cs
@@ -0,0 +1,19 @@
+﻿#if NET6_0_OR_GREATER
+using System;
+
+namespace Humanizer.Localisation.DateToOrdinalWords
+{
+    internal class LtDateOnlyToOrdinalWordsConverter : IDateOnlyToOrdinalWordConverter
+    {
+        public string Convert(DateOnly date)
+        {
+            return date.ToString("yyyy 'm.' MMMM d 'd.'");
+        }
+
+        public string Convert(DateOnly date, GrammaticalCase grammaticalCase)
+        {
+            return Convert(date);
+        }
+    }
+}
+#endif
