a2fbf63a554591e971842d2daea6ff0c147f2dac
Simon Cropp <simon.cropp@gmail.com>
2024-02-11 23:15:47 +1100

    .

---
+++ src/Humanizer.Tests/NewFile1.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using VerifyTests;
+
+[ModuleInitializer]
+public static void ModuleInitializer()
+{
+    VerifierSettings.InitializePlugins();
+}
+#if NET48
+
+namespace System.Runtime.CompilerServices;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = false)]
+sealed class ModuleInitializerAttribute : Attribute
+{
+}
+#endif
