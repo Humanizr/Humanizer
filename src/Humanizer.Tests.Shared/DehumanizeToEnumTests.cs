85c1ad1262fe673d60d4318a347ff60405675d35
Simon Cropp <simon.cropp@gmail.com>
2024-02-17 00:07:20 +1100

    remove redundant DummyStructWithEnumInterfaces  members

--- src/Humanizer.Tests.Shared/DehumanizeToEnumTests.cs
+++ src/Humanizer.Tests.Shared/DehumanizeToEnumTests.cs
@@ -87,57 +87,6 @@
 
             public string ToString(string format, IFormatProvider formatProvider) =>
                 throw new NotImplementedException();
-
-            public TypeCode GetTypeCode() =>
-                throw new NotImplementedException();
-
-            public bool ToBoolean(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public char ToChar(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public sbyte ToSByte(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public byte ToByte(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public short ToInt16(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public ushort ToUInt16(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public int ToInt32(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public uint ToUInt32(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public long ToInt64(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public ulong ToUInt64(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public float ToSingle(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public double ToDouble(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public decimal ToDecimal(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public DateTime ToDateTime(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public string ToString(IFormatProvider provider) =>
-                throw new NotImplementedException();
-
-            public object ToType(Type conversionType, IFormatProvider provider) =>
-                throw new NotImplementedException();
         }
 
         enum DummyEnum
