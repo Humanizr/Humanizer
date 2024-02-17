26efa9153421d456047f33f5c790fcb80a3ab3f8
Simon Cropp <simon.cropp@gmail.com>
2024-02-17 23:18:17 +1100

    .

---
+++ src/Benchmarks/EnumBenchmarks.cs
@@ -0,0 +1,16 @@
+﻿[MemoryDiagnoser(false)]
+public class EnumBenchmarks
+{
+    public enum EnumUnderTest
+    {
+        [Description("The description")]
+        MemberWithDescriptionAttribute,
+        MemberWithoutDescriptionAttribute,
+
+        [Display(Description = "Display value")]
+        MemberWithDisplayAttribute,
+    }
+
+    [Benchmark]
+    public string Humanize() => EnumUnderTest.MemberWithDisplayAttribute.Humanize();
+}
