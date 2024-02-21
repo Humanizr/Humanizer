eb184754ca190e7700dbc5112228a71de72207e5
Simon Cropp <simon.cropp@gmail.com>
2024-02-21 18:39:49 +1100

    PrependArticleSuffix benchmark

--- src/Benchmarks/EnglishArticleBenchmarks.cs
+++ src/Benchmarks/EnglishArticleBenchmarks.cs
@@ -4,4 +4,8 @@ public class EnglishArticleBenchmarks
     [Benchmark]
     public string[] AppendArticlePrefix() =>
         EnglishArticle.AppendArticlePrefix([ "Ant", "The Theater", "The apple", "Fox", "Bear"]);
+
+    [Benchmark]
+    public string[] PrependArticleSuffix() =>
+        EnglishArticle.PrependArticleSuffix([ "Ant", "apple The", "Bear", "Fox", "Theater The"]);
 }
