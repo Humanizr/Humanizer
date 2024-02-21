b13ef0a046f7642a4c2ade6fb8220fe2aad13d9d
Alex Zaytsev <hazzik@gmail.com>
2024-02-21 14:17:32 +1000

    Fix article sorting for words containing a suffix in a middle

--- src/Humanizer.Tests.Shared/ArticlePrefixSortTests.cs
+++ src/Humanizer.Tests.Shared/ArticlePrefixSortTests.cs
@@ -7,6 +7,7 @@
         [InlineData(new[] { "An Ant", "The Theater", "the apple", "a Fox", "Bear" }, new[] { "An Ant", "the apple", "Bear", "a Fox", "The Theater" })]
         [InlineData(new[] { "Ant", "A Theater", "an apple", "Fox", "Bear" }, new[] { "Ant", "an apple", "Bear", "Fox", "A Theater" })]
         [InlineData(new[] { " Ant ", " A  Theater ", "  an apple  ", " Fox", "Bear " }, new[] { "A  Theater", "an apple", "Ant", "Bear", "Fox" })]
+        [InlineData(new[] { "The General Theory of Relativity" }, new[] { "The General Theory of Relativity" })]
         public void SortStringArrayIgnoringArticlePrefixes(string[] input, string[] expectedOutput) =>
             Assert.Equal(expectedOutput, EnglishArticle.PrependArticleSuffix(EnglishArticle.AppendArticlePrefix(input)));
 
