bfd828e985392218072f7fa8184ea93697fbfc91
Simon Cropp <simon.cropp@gmail.com>
2024-02-24 19:36:48 +1100

    Update InDateTests.cs

--- src/Humanizer.Tests/FluentDate/InDateTests.cs
+++ src/Humanizer.Tests/FluentDate/InDateTests.cs
@@ -1,33 +1,31 @@
 ﻿#if NET6_0_OR_GREATER
 
-namespace Humanizer.Tests.FluentDate
+namespace Humanizer.Tests.FluentDate;
+
+public class InDateTests
 {
-    public class InDateTests
+    [Fact]
+    public void InJanuary() =>
+        Assert.Equal(new(DateTime.Now.Year, 1, 1), InDate.January);
+
+    [Fact]
+    public void InJanuaryOf2009() =>
+        Assert.Equal(new(2009, 1, 1), InDate.JanuaryOf(2009));
+
+    [Fact]
+    public void InFebruary() =>
+        Assert.Equal(new(DateTime.Now.Year, 2, 1), InDate.February);
+
+    [Fact]
+    public void InTheYear() =>
+        Assert.Equal(new(2009, 1, 1), InDate.TheYear(2009));
+
+    [Fact]
+    public void InFiveDays()
     {
-        [Fact]
-        public void InJanuary() =>
-            Assert.Equal(new(DateTime.Now.Year, 1, 1), InDate.January);
-
-        [Fact]
-        public void InJanuaryOf2009() =>
-            Assert.Equal(new(2009, 1, 1), InDate.JanuaryOf(2009));
-
-        [Fact]
-        public void InFebruary() =>
-            Assert.Equal(new(DateTime.Now.Year, 2, 1), InDate.February);
-
-        [Fact]
-        public void InTheYear() =>
-            Assert.Equal(new(2009, 1, 1), InDate.TheYear(2009));
-
-        [Fact]
-        public void InFiveDays()
-        {
-            var baseDate = OnDate.January.The21st;
-            var date = InDate.Five.DaysFrom(baseDate);
-            Assert.Equal(baseDate.AddDays(5), date);
-        }
+        var baseDate = OnDate.January.The21st;
+        var date = InDate.Five.DaysFrom(baseDate);
+        Assert.Equal(baseDate.AddDays(5), date);
     }
 }
 #endif
-
