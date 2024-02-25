a115f621e89691450db758086a095dc61c7718dd
Simon Cropp <simon.cropp@gmail.com>
2024-02-25 21:22:22 +1100

    Update LbTimeOnlyToClockNotationConverter.cs

--- src/Humanizer/Localisation/TimeToClockNotation/LbTimeOnlyToClockNotationConverter.cs
+++ src/Humanizer/Localisation/TimeToClockNotation/LbTimeOnlyToClockNotationConverter.cs
@@ -7,45 +7,86 @@ class LbTimeOnlyToClockNotationConverter : ITimeOnlyToClockNotationConverter
     static string tenMinutes = 10.ToWords();
     static        string oneMinute = 1.ToWords(GrammaticalGender.Feminine);
     static        string fiveMinutes = 5.ToWords();
+    string twentyMinutes = 20.ToWords();
 
     public string Convert(TimeOnly time, ClockNotationRounding roundToNearestFive)
     {
-        var rounded = roundToNearestFive is ClockNotationRounding.NearestFiveMinutes
-            ? GetRoundedTime(time)
-            : time;
-
-        if (rounded is {Hour: 0, Minute: 0})
+        if (roundToNearestFive is ClockNotationRounding.NearestFiveMinutes)
         {
-            return "Mëtternuecht";
+            return HandlerRounded(time);
         }
 
-        if (rounded is {Hour: 12, Minute: 0})
+        if (HandleOnHour(time, out var value))
         {
-            return "Mëtteg";
+            return value;
+        }
+
+        return time.Minute switch
+        {
+            00 => Hours(time.Hour, "Auer"),
+            01 => $"{oneMinute} Minutt op {Hours(time.Hour)}",
+            05 => $"{fiveMinutes} op {Hours(time.Hour)}",
+            10 => $"{tenMinutes} op {Hours(time.Hour)}",
+            15 => $"Véirel op {Hours(time.Hour)}",
+            20 => $"{twentyMinutes} op {Hours(time.Hour)}",
+            25 => $"{fiveMinutes} vir hallwer {Hours(time.Hour + 1)}",
+            30 => $"hallwer {Hours(time.Hour + 1)}",
+            35 => $"{fiveMinutes} op hallwer {Hours(time.Hour + 1)}",
+            40 => $"{twentyMinutes} vir {Hours(time.Hour + 1)}",
+            45 => $"Véirel vir {Hours(time.Hour + 1)}",
+            50 => $"{tenMinutes} vir {Hours(time.Hour + 1)}",
+            55 => $"{fiveMinutes} vir {Hours(time.Hour + 1)}",
+            59 => $"{oneMinute} Minutt vir {Hours(time.Hour + 1)}",
+            60 => Hours(time.Hour + 1, "Auer"),
+            > 00 and < 25 => $"{Minutes(time.Minute, "Minutten")} op {Hours(time.Hour)}",
+            > 25 and < 30 => $"{Minutes(30 - time.Minute, "Minutten")} vir hallwer {Hours(time.Hour + 1)}",
+            > 30 and < 35 => $"{Minutes(time.Minute - 30, "Minutten")} op hallwer {Hours(time.Hour + 1)}",
+            _ =>  $"{Minutes(60 - time.Minute, "Minutten")} vir {Hours(time.Hour + 1)}",
+        };
+    }
+
+    static bool HandleOnHour(TimeOnly time, [NotNullWhen(true)] out string? result)
+    {
+        if (time is {Hour: 0, Minute: 0})
+        {
+            result = "Mëtternuecht";
+            return true;
+        }
+
+        if (time is {Hour: 12, Minute: 0})
+        {
+            result = "Mëtteg";
+            return true;
+        }
+
+        result = null;
+        return false;
+    }
+
+    string HandlerRounded(TimeOnly time)
+    {
+        var rounded = GetRoundedTime(time);
+
+        if (HandleOnHour(time, out var value))
+        {
+            return value;
         }
 
         return rounded.Minute switch
         {
             00 => Hours(rounded.Hour, "Auer"),
-            15 => $"Véirel op {Hours(rounded.Hour)}",
-            25 => $"{Minutes(5, "vir")} hallwer {Hours(rounded.Hour + 1)}",
-            30 => $"hallwer {Hours(rounded.Hour + 1)}",
-            35 => $"{Minutes(5, "op")} hallwer {Hours(rounded.Hour + 1)}",
-            45 => $"Véirel vir {Hours(rounded.Hour + 1)}",
-            60 => Hours(rounded.Hour + 1, "Auer"),
-            01 => $"{oneMinute} Minutt op {Hours(rounded.Hour)}",
-            59 => $"{oneMinute} Minutt vir {Hours(rounded.Hour + 1)}",
-            05 => $"{Minutes(5, "op")} {Hours(rounded.Hour)}",
+            05 => $"{fiveMinutes} op {Hours(rounded.Hour)}",
             10 => $"{tenMinutes} op {Hours(rounded.Hour)}",
-            20 => $"{Minutes(20, "op")} {Hours(rounded.Hour)}",
-            40 => $"{Minutes(20, "vir")} {Hours(rounded.Hour + 1)}",
+            15 => $"Véirel op {Hours(rounded.Hour)}",
+            20 => $"{twentyMinutes} op {Hours(rounded.Hour)}",
+            25 => $"{fiveMinutes} vir hallwer {Hours(rounded.Hour + 1)}",
+            30 => $"hallwer {Hours(rounded.Hour + 1)}",
+            35 => $"{fiveMinutes} op hallwer {Hours(rounded.Hour + 1)}",
+            40 => $"{twentyMinutes} vir {Hours(rounded.Hour + 1)}",
+            45 => $"Véirel vir {Hours(rounded.Hour + 1)}",
             50 => $"{tenMinutes} vir {Hours(rounded.Hour + 1)}",
-            55 => $"{Minutes(5, "vir")} {Hours(rounded.Hour + 1)}",
-            > 00 and < 25 => $"{Minutes(rounded.Minute, "Minutten")} op {Hours(rounded.Hour)}",
-            > 25 and < 30 => $"{Minutes(30 - rounded.Minute, "Minutten")} vir hallwer {Hours(rounded.Hour + 1)}",
-            > 30 and < 35 => $"{Minutes(rounded.Minute - 30, "Minutten")} op hallwer {Hours(rounded.Hour + 1)}",
-            > 35 and < 60 => $"{Minutes(60 - rounded.Minute, "Minutten")} vir {Hours(rounded.Hour + 1)}",
-            _ => $"{Hours(time.Hour, "Auer")} {Minutes(time.Minute)}"
+            55 => $"{fiveMinutes} vir {Hours(rounded.Hour + 1)}",
+            _ => Hours(rounded.Hour + 1, "Auer"),
         };
     }
 
@@ -62,11 +103,6 @@ class LbTimeOnlyToClockNotationConverter : ITimeOnlyToClockNotationConverter
 
     static string Minutes(int minute, string nextWord)
         => GetFormattedExpression(minute, nextWord);
-
-    static string FiveMinutes(string nextWord)
-    {
-        return $"{fiveMinutes} {nextWord}";
-    }
 
     static string Hours(int hour)
     {
