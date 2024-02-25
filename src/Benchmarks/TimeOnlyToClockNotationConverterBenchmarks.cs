96abd101cb7bb78da19f38877f3093ac959ae7a0
Simon Cropp <simon.cropp@gmail.com>
2024-02-25 17:09:22 +1100

    Update TimeOnlyToClockNotationConverterBenchmarks.cs

--- src/Benchmarks/TimeOnlyToClockNotationConverterBenchmarks.cs
+++ src/Benchmarks/TimeOnlyToClockNotationConverterBenchmarks.cs
@@ -5,26 +5,41 @@ public class TimeOnlyToClockNotationConverterBenchmarks
 
     static EsTimeOnlyToClockNotationConverter esConverter = new();
     [Benchmark]
-    public string EsClockNotationConverter() =>
+    public void EsClockNotationConverter()
+    {
         esConverter.Convert(time, ClockNotationRounding.None);
+        esConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes);
+    }
 
     static BrazilianPortugueseTimeOnlyToClockNotationConverter brazilianConverter = new();
     [Benchmark]
-    public string BrazilianPortugueseClockNotationConverter() =>
+    public void BrazilianPortugueseClockNotationConverter()
+    {
         brazilianConverter.Convert(time, ClockNotationRounding.None);
+        brazilianConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes);
+    }
 
     static FrTimeOnlyToClockNotationConverter frConverter = new();
     [Benchmark]
-    public string FrClockNotationConverter() =>
+    public void FrClockNotationConverter()
+    {
         frConverter.Convert(time, ClockNotationRounding.None);
+        frConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes);
+    }
 
     static LbTimeOnlyToClockNotationConverter lbConverter = new();
     [Benchmark]
-    public string LbClockNotationConverter() =>
+    public void LbClockNotationConverter()
+    {
         lbConverter.Convert(time, ClockNotationRounding.None);
+        lbConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes);
+    }
 
     static DefaultTimeOnlyToClockNotationConverter defaultConverter = new();
     [Benchmark]
-    public string DefaultClockNotationConverter() =>
+    public void DefaultClockNotationConverter()
+    {
         defaultConverter.Convert(time, ClockNotationRounding.None);
+        defaultConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes);
+    }
 }
