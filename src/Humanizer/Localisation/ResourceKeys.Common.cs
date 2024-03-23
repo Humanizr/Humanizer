c698311826e800112e06379ee4a07c9667c84e60
Simon Cropp <simon.cropp@gmail.com>
2024-03-23 21:46:13 +1100

    .

--- src/Humanizer/Localisation/ResourceKeys.Common.cs
+++ src/Humanizer/Localisation/ResourceKeys.Common.cs
@@ -2,11 +2,4 @@
 
 public partial class ResourceKeys
 {
-    static void ValidateRange(int count)
-    {
-        if (count < 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(count));
-        }
-    }
 }
