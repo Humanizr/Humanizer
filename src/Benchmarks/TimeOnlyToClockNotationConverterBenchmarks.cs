590d25b9bd62a03fae98393f21d40b0285ff36f1
Julian Katzwinkel <j.katzwinkel@posteo.de>
2024-08-24 17:42:05 +0200

    Fix Benchmark

--- src/Benchmarks/TimeOnlyToClockNotationConverterBenchmarks.cs
+++ src/Benchmarks/TimeOnlyToClockNotationConverterBenchmarks.cs
@@ -1,4 +1,4 @@
-﻿[MemoryDiagnoser(false)]
+[MemoryDiagnoser(false)]
 public class TimeOnlyToClockNotationConverterBenchmarks
 {
     static TimeOnly time = new(13, 6, 2, 88, 987);
@@ -7,39 +7,39 @@ public class TimeOnlyToClockNotationConverterBenchmarks
     [Benchmark]
     public void EsClockNotationConverter()
     {
-        esConverter.Convert(time, ClockNotationRounding.None);
-        esConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes);
+        esConverter.Convert(time, ClockNotationRounding.None, null);
+        esConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes, null);
     }
 
     static BrazilianPortugueseTimeOnlyToClockNotationConverter brazilianConverter = new();
     [Benchmark]
     public void BrazilianPortugueseClockNotationConverter()
     {
-        brazilianConverter.Convert(time, ClockNotationRounding.None);
-        brazilianConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes);
+        brazilianConverter.Convert(time, ClockNotationRounding.None, null);
+        brazilianConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes, null);
     }
 
     static FrTimeOnlyToClockNotationConverter frConverter = new();
     [Benchmark]
     public void FrClockNotationConverter()
     {
-        frConverter.Convert(time, ClockNotationRounding.None);
-        frConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes);
+        frConverter.Convert(time, ClockNotationRounding.None, null);
+        frConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes, null);
     }
 
     static LbTimeOnlyToClockNotationConverter lbConverter = new();
     [Benchmark]
     public void LbClockNotationConverter()
     {
-        lbConverter.Convert(time, ClockNotationRounding.None);
-        lbConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes);
+        lbConverter.Convert(time, ClockNotationRounding.None, null);
+        lbConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes, null);
     }
 
     static DefaultTimeOnlyToClockNotationConverter defaultConverter = new();
     [Benchmark]
     public void DefaultClockNotationConverter()
     {
-        defaultConverter.Convert(time, ClockNotationRounding.None);
-        defaultConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes);
+        defaultConverter.Convert(time, ClockNotationRounding.None, null);
+        defaultConverter.Convert(time, ClockNotationRounding.NearestFiveMinutes, null);
     }
 }
