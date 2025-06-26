1f1164ed05da3602ebfe72518b73d17c0853f6db
Hasoun <omierhasoun@gmail.com>
2025-06-26 19:14:08 +0200

    renamed TryToNumber to TryConvertToNumber

--- src/Humanizer/Localisation/WordsToNumber/IWordsToNumberConverter.cs
+++ src/Humanizer/Localisation/WordsToNumber/IWordsToNumberConverter.cs
@@ -2,5 +2,8 @@ namespace Humanizer;
 
 public interface IWordsToNumberConverter
 {
+    bool TryConvert(string words, out int parsedValue);
+    bool TryConvert(string words, out int parsedValue, out string? unrecognizedNumber);
+
     int Convert(string words);
 }
--- src/Humanizer/WordsToNumberExtension.cs
+++ src/Humanizer/WordsToNumberExtension.cs
@@ -8,5 +8,9 @@ namespace Humanizer
     public static class WordsToNumberExtension
     {
         public static int ToNumber(this string words, CultureInfo culture) => Configurator.GetWordsToNumberConverter(culture).Convert(words);
+        public static bool TryConvertToNumber(this string words, out int parsedNumber, CultureInfo culture) => Configurator.GetWordsToNumberConverter(culture).TryConvert(words, out parsedNumber);
+        public static bool TryConvertToNumber(this string words, out int parsedNumber, CultureInfo culture, out string? unrecognizedWord) => Configurator.GetWordsToNumberConverter(culture).TryConvert(words, out parsedNumber, out unrecognizedWord);
+
+
     }
 }
