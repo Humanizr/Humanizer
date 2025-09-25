6e56b082b8025836811f7fab43559aaa145c962e
Omier <omierhasoun@gmail.com>
2025-09-26 01:50:20 +0200

    Introduce TryToNumber methods for safe word-to-number conversion (#1582)
    Co-authored-by: Claire Novotny <1427284+clairernovotny@users.noreply.github.com>

--- src/Humanizer/Localisation/WordsToNumber/GenderlessWordsToNumberConverter.cs
+++ src/Humanizer/Localisation/WordsToNumber/GenderlessWordsToNumberConverter.cs
@@ -3,4 +3,6 @@ namespace Humanizer;
 internal abstract class GenderlessWordsToNumberConverter : IWordsToNumberConverter
 {
     public abstract int Convert(string words);
+    public abstract bool TryConvert(string words, out int parsedValue);
+    public abstract bool TryConvert(string words, out int parsedValue, out string? unrecognizedNumber);
 }
