81eef81190463f139910a9540b2ad6d7747981f0
Claire Novotny <1427284+clairernovotny@users.noreply.github.com>
2025-10-08 23:20:10 -0400

    Update Romanian translation test for multiple years ago

--- src/Humanizer.Tests/Localisation/ResourcesTests.cs
+++ src/Humanizer.Tests/Localisation/ResourcesTests.cs
@@ -7,13 +7,13 @@ public class ResourcesTests
     public void CanGetCultureSpecificTranslationsWithImplicitCulture()
     {
         var format = Resources.GetResource("DateHumanize_MultipleYearsAgo");
-        Assert.Equal("acum {0} ani", format);
+        Assert.Equal("acum {0}{1} ani", format);
     }
 
     [Fact]
     public void CanGetCultureSpecificTranslationsWithExplicitCulture()
     {
         var format = Resources.GetResource("DateHumanize_MultipleYearsAgo", new("ro"));
-        Assert.Equal("acum {0} ani", format);
+        Assert.Equal("acum {0}{1} ani", format);
     }
 }
