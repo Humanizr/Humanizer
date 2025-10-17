34610d40f8e49747a64d989e481edde30511a532
Claire Novotny <1427284+clairernovotny@users.noreply.github.com>
2025-10-17 17:17:53 -0400

    fix

--- src/Benchmarks/Program.cs
+++ src/Benchmarks/Program.cs
@@ -1,9 +1,1 @@
-﻿using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Exporters;
-using BenchmarkDotNet.Exporters.Json;
-
-var config = DefaultConfig.Instance
-    .AddExporter(MarkdownExporter.GitHub)
-    .AddExporter(JsonExporter.Full);
-
-BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
+﻿BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
