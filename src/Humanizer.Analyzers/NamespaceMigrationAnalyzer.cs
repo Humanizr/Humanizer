230a8dc6d0760c45f34474aa3ff6c786df2de2a1
Claire Novotny <1427284+clairernovotny@users.noreply.github.com>
2025-10-17 18:28:37 -0400

    make it an error

--- src/Humanizer.Analyzers/NamespaceMigrationAnalyzer.cs
+++ src/Humanizer.Analyzers/NamespaceMigrationAnalyzer.cs
@@ -22,7 +22,7 @@ public class NamespaceMigrationAnalyzer : DiagnosticAnalyzer
         Title,
         MessageFormat,
         Category,
-        DiagnosticSeverity.Warning,
+        DiagnosticSeverity.Error,
         isEnabledByDefault: true,
         description: Description);
 
