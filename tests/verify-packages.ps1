6b047ad02cd283fd6dad0b574f62628d52b4632d
Claire Novotny <1427284+clairernovotny@users.noreply.github.com>
2026-03-12 18:02:55 -0400

    chore: refresh dependencies and move repo config to root (#1685)

--- tests/verify-packages.ps1
+++ tests/verify-packages.ps1
@@ -198,6 +198,7 @@ $projectContent = @"
   <PropertyGroup>
     <TargetFramework>$TargetFramework</TargetFramework>
     <TreatWarningsAsErrors>true</TreatWarningsAsErrors>
+    <MSBuildTreatWarningsAsErrors>true</MSBuildTreatWarningsAsErrors>
   </PropertyGroup>
   <ItemGroup>
     <PackageReference Include="Humanizer" Version="$PackageVersion" />
