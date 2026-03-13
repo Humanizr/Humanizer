24696173d252672ab8dfeada35e19da792ae4349
Claire Novotny <claire@novotny.org>
2026-03-13 13:12:53 -0400

    fix: complete filipino locale parity

--- tests/Humanizer.Tests/Localisation/fil-PH/TimeSpanHumanizeTests.cs
+++ tests/Humanizer.Tests/Localisation/fil-PH/TimeSpanHumanizeTests.cs
@@ -72,4 +72,20 @@ public class TimeSpanHumanizeTests
     [Fact]
     public void NoTimeToWords() =>
         Assert.Equal("walang oras", TimeSpan.Zero.Humanize(toWords: true));
+
+    [Theory]
+    [Trait("Translation", "Google Translate")]
+    [InlineData(4, false, "4 araw gulang")]
+    [InlineData(367, true, "isang taon gulang")]
+    public void Age(int days, bool toWords, string expected) =>
+        Assert.Equal(expected, TimeSpan.FromDays(days).ToAge(toWords: toWords));
+
+    [Theory]
+    [Trait("Translation", "Google Translate")]
+    [InlineData(24 * 60 * 60 * 1000, "isang araw")]
+    [InlineData(60 * 60 * 1000, "isang oras")]
+    [InlineData(60 * 1000, "isang minuto")]
+    [InlineData(1000, "isang segundo")]
+    public void TimeSpanWithNumbersConvertedToWords(int milliseconds, string expected) =>
+        Assert.Equal(expected, TimeSpan.FromMilliseconds(milliseconds).Humanize(toWords: true));
 }
