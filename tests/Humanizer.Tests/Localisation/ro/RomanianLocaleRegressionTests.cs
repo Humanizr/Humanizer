a2b72cdca36c08464b22adce8a364dfa8d2981cf
Claire Novotny <claire@novotny.org>
2026-07-28 04:13:33 -0400

    test: align Romanian regressions across targets (#1834)

--- tests/Humanizer.Tests/Localisation/ro/RomanianLocaleRegressionTests.cs
+++ tests/Humanizer.Tests/Localisation/ro/RomanianLocaleRegressionTests.cs
@@ -1,5 +1,6 @@
 namespace Humanizer.Tests.Localisation.ro;
 
+[UseCulture("ro-RO")]
 public class RomanianLocaleRegressionTests
 {
     static readonly CultureInfo Romanian = new("ro-RO");
@@ -20,9 +21,11 @@ public class RomanianLocaleRegressionTests
         Assert.Equal(expected, formatter.TimeSpanHumanize(unit, 1, toWords: true));
     }
 
+#if NET6_0_OR_GREATER
     [Fact]
     public void ExactHourUsesMin0Template() =>
         Assert.Equal(
             "ora unu",
             new TimeOnly(13, 0).ToClockNotation(ClockNotationRounding.None, Romanian));
+#endif
 }
