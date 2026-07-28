2d01c9f221a0c7d694fb1cb93568a9e0a72ef97e
Claire Novotny <claire@novotny.org>
2026-07-28 04:03:19 -0400

    test: cover Romanian singular durations and exact hours

---
+++ tests/Humanizer.Tests/Localisation/ro/RomanianLocaleRegressionTests.cs
@@ -0,0 +1,28 @@
+namespace Humanizer.Tests.Localisation.ro;
+
+public class RomanianLocaleRegressionTests
+{
+    static readonly CultureInfo Romanian = new("ro-RO");
+
+    [Theory]
+    [InlineData(TimeUnit.Millisecond, "o milisecundă")]
+    [InlineData(TimeUnit.Second, "o secundă")]
+    [InlineData(TimeUnit.Minute, "un minut")]
+    [InlineData(TimeUnit.Hour, "o oră")]
+    [InlineData(TimeUnit.Day, "o zi")]
+    [InlineData(TimeUnit.Week, "o săptămână")]
+    [InlineData(TimeUnit.Month, "o lună")]
+    [InlineData(TimeUnit.Year, "un an")]
+    public void ToWordsUsesSingularFormForEverySupportedUnit(TimeUnit unit, string expected)
+    {
+        var formatter = Configurator.Formatters.ResolveForCulture(Romanian);
+
+        Assert.Equal(expected, formatter.TimeSpanHumanize(unit, 1, toWords: true));
+    }
+
+    [Fact]
+    public void ExactHourUsesMin0Template() =>
+        Assert.Equal(
+            "ora unu",
+            new TimeOnly(13, 0).ToClockNotation(ClockNotationRounding.None, Romanian));
+}
