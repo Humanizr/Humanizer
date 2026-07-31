fd3cd85bbc11c45521b3a5c10d15de511c99375d
Claire Novotny <claire@novotny.org>
2026-07-31 19:28:21 -0400

    fix(generator): simplify locale resolution filters

--- tests/Humanizer.SourceGenerators.Tests/SourceGenerators/CanonicalLocaleSchemaTests.cs
+++ tests/Humanizer.SourceGenerators.Tests/SourceGenerators/CanonicalLocaleSchemaTests.cs
@@ -1213,6 +1213,12 @@ surfaces:
   calendar:
     eraNames: {}
 """, "unsupported property 'eraNames'")]
+    [InlineData("""
+locale: 'zz'
+surfaces:
+  compass:
+    unexpected: {}
+""", "unsupported property 'unexpected'")]
     public void CanonicalSchemaRejectsUnsupportedCanonicalProperties(string yaml, string expectedMessage)
     {
         var catalog = CreateCatalog(("zz", yaml));
